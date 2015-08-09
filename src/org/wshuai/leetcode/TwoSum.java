28b847d5b9ca1a762789233a32bafae8425c8754
relentlesscoder <relentless.code@gmail.com>
2015-08-09 17:57:50 -0400

    implemented Longest Substring Without Repeating Characters

--- src/org/wshuai/leetcode/TwoSum.java
+++ src/org/wshuai/leetcode/TwoSum.java
@@ -7,33 +7,31 @@ import java.util.Map;
  * Created by Wei on 8/9/15.
  */
 public class TwoSum {
-    public static int[] twoSum(int[] nums, int target){
-        int[] result = new int[2];
-        if(nums.length > 1){
-            Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-            for(int i = 0; i < nums.length; i++){
-                map.put(nums[i], i);
-            }
-            for(int i = 0; i < nums.length; i++){
-                int diff = target - nums[i];
-                Integer index = map.get(diff);
-                if(index != null && (index != i)){
-                    if(index < i){
-                        result[0] = index + 1;
-                        result[1] = i + 1;
-                    }
-                    else{
-                        result[0] = i + 1;
-                        result[1] = index + 1;
-                    }
-                    break;
-                }
-                else{
-                    continue;
-                }
-            }
+  public static int[] twoSum(int[] nums, int target) {
+    int[] result = new int[2];
+    if (nums.length > 1) {
+      Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+      for (int i = 0; i < nums.length; i++) {
+        map.put(nums[i], i);
+      }
+      for (int i = 0; i < nums.length; i++) {
+        int diff = target - nums[i];
+        Integer index = map.get(diff);
+        if (index != null && (index != i)) {
+          if (index < i) {
+            result[0] = index + 1;
+            result[1] = i + 1;
+          } else {
+            result[0] = i + 1;
+            result[1] = index + 1;
+          }
+          break;
+        } else {
+          continue;
         }
-        return result;
+      }
     }
+    return result;
+  }
 }
 
--- src/org/wshuai/leetcode/test/TwoSumTest.java
+++ src/org/wshuai/leetcode/test/TwoSumTest.java
@@ -9,13 +9,13 @@ import static org.junit.Assert.assertEquals;
  * Created by Wei on 8/9/15.
  */
 public class TwoSumTest {
-    @Test
-    public void twoSumShouldReturnCorrectIndexes(){
-        int[] array1 = {3, 2, 4};
-        int target1 = 6;
+  @Test
+  public void twoSumShouldReturnCorrectIndexes() {
+    int[] array1 = {3, 2, 4};
+    int target1 = 6;
 
-        int[] result1 = TwoSum.twoSum(array1, target1);
-        assertEquals(result1[0], 2);
-        assertEquals(result1[1], 3);
-    }
+    int[] result1 = TwoSum.twoSum(array1, target1);
+    assertEquals(2, result1[0]);
+    assertEquals(3, result1[1]);
+  }
 }
