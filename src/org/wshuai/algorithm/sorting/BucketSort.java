3eb353704f8bddfa79db65050b9cae1a046a070f
relentlesscoder <relentless.code@gmail.com>
2015-08-22 09:55:40 -0400

    implemented bucket sort

--- src/org/wshuai/algorithm/sorting/BucketSort.java
+++ src/org/wshuai/algorithm/sorting/BucketSort.java
@@ -1,10 +1,34 @@
 package org.wshuai.algorithm.sorting;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * Created by Wei on 8/11/2015.
  */
 public class BucketSort {
   public static void sort(Comparable[] array){
-    
+    if(array == null || array.length <= 0){
+      return;
+    }
+
+    Map<Comparable, Integer> map = new HashMap<Comparable, Integer>();
+    for(int i=0; i < array.length; i++){
+      Integer count = map.get(array[i]);
+      map.put(array[i], count == null ? 1 : ++count);
+    }
+
+    Iterator it = map.keySet().iterator();
+    int index = 0;
+    while (it.hasNext()){
+      Comparable key = (Comparable)it.next();
+      Integer count = map.get(key);
+      while (count > 0){
+        array[index] = key;
+        index++;
+        count--;
+      }
+    }
   }
 }
