17fd0ac47cf344f51e417bacde62e9cccaa4c14e
relentlesscoder <relentless.code@gmail.com>
2015-08-23 12:18:25 -0400

    implemented merge sort

--- src/org/wshuai/algorithm/sorting/InsertionSort.java
+++ src/org/wshuai/algorithm/sorting/InsertionSort.java
@@ -1,10 +1,24 @@
 package org.wshuai.algorithm.sorting;
 
+import org.wshuai.utility.ArrayUtil;
+
 /**
  * Created by Wei on 8/23/15.
  */
 public class InsertionSort {
   public static void sort(Comparable[] array){
-    
+    if(array == null || array.length <= 1){
+      return;
+    }
+
+    for(int i = 1; i < array.length; i++){
+      int j = i;
+      Comparable value = array[j];
+      while (j > 0 && ArrayUtil.less(value, array[j-1])){
+        ArrayUtil.exchange(array, j, j-1);
+        j--;
+      }
+      array[j] = value;
+    }
   }
 }
---
+++ src/org/wshuai/algorithm/sorting/MergeSort.java
@@ -0,0 +1,75 @@
+package org.wshuai.algorithm.sorting;
+
+import org.wshuai.utility.ArrayUtil;
+
+/**
+ * Created by Wei on 8/23/2015.
+ */
+public class MergeSort {
+  /**
+   * Global auxiliary array for merging two sorted sub-arrays
+   */
+  private static Comparable[] auxiliary;
+
+  /**
+   * Merge sort the input array.
+   * @param array the input array
+   */
+  public static void sort(Comparable[] array){
+    if(array == null || array.length <= 1){
+      return;
+    }
+
+    auxiliary = new Comparable[array.length];
+    MergeSort.sort(array, 0, array.length - 1);
+  }
+
+  /**
+   * Divide the input into two sub-arrays, sort both sub-arrays and then merge back
+   * into one sorted array.
+   * @param array the input array
+   * @param low the lower index
+   * @param high the higher index
+   */
+  private static void sort(Comparable[] array, int low, int high){
+    if(high <= low){
+      return;
+    }
+
+    int middle = low + (high - low)/2;
+    sort(array, low, middle);
+    sort(array, middle+1, high);
+    merge(array, low, middle, high);
+  }
+
+  /**
+   * Merge two sorted sub-arrays back into one sorted array.
+   * @param array the input array
+   * @param low the lower index
+   * @param middle the middle index
+   * @param high the higher index
+   */
+  private static void merge(Comparable[] array, int low, int middle, int high){
+    for(int i = low; i <= high; i++){
+      auxiliary[i] = array[i];
+    }
+
+    int left = low;
+    int right = middle;
+
+    for(int i = low; i <= high; i++){
+      if(left > middle){
+        array[i] = auxiliary[right++];
+      }
+      else if(right > high){
+        array[i] = auxiliary[left++];
+      }
+      else if(ArrayUtil.less(auxiliary[left], auxiliary[right])){
+        array[i] = auxiliary[left++];
+      }
+      else{
+        array[i] = auxiliary[right++];
+      }
+    }
+  }
+}
