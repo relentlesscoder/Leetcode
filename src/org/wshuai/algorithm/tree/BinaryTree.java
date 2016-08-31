990c95b162e19f753c235e4ab1823578a1893786
Wei Shuai <relentless.code@gmail.com>
2016-08-30 23:36:07 -0400

    jia you shua ti

--- src/org/wshuai/algorithm/tree/BinaryTree.java
+++
@@ -1,7 +0,0 @@
-package org.wshuai.algorithm.tree;
-
-/**
- * Created by Wei on 8/11/2015.
- */
-public class BinaryTree {
-}
---
+++ src/org/wshuai/algorithm/tree/test/BinaryTreeLevelOrderTraversalRecursiveTest.java
@@ -0,0 +1,24 @@
+package org.wshuai.algorithm.tree.test;
+
+import org.junit.Test;
+import org.wshuai.algorithm.tree.BinaryTreeLevelOrderTraversalRecursive;
+import org.wshuai.algorithm.tree.BinaryTreeNode;
+
+import java.util.List;
+
+/**
+ * Created by Wei on 8/30/16.
+ */
+public class BinaryTreeLevelOrderTraversalRecursiveTest {
+  @Test
+  public void testcase(){
+    BinaryTreeNode root = new BinaryTreeNode(3);
+    root.left = new BinaryTreeNode(5);
+    root.right = new BinaryTreeNode(1);
+    root.left.left = new BinaryTreeNode(7);
+    root.left.right = new BinaryTreeNode(6);
+    root.right.left = new BinaryTreeNode(9);
+    root.right.right = new BinaryTreeNode(4);
+    List<List<Integer>> lst = BinaryTreeLevelOrderTraversalRecursive.traversal(root);
+  }
+}
