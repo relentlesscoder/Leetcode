3e3b5d24f901537d225bc85ce1ac47e48a70e561
Wei Shuai <relentless.code@gmail.com>
2016-10-02 16:11:46 -0400

    193

---
+++ src/org/wshuai/leetcode/ShortestWordDistanceIII.java
@@ -0,0 +1,35 @@
+package org.wshuai.leetcode;
+
+/**
+ * Created by Wei on 10/2/16.
+ */
+public class ShortestWordDistanceIII {
+  public int shortestWordDistance(String[] words, String word1, String word2) {
+    int min = Integer.MAX_VALUE;
+    int w1 = -1;
+    int w2 = -1;
+    int len = words.length;
+    boolean last = false;
+    boolean same = word1.equals(word2);
+    for(int i = 0; i < len; i++){
+      String val = words[i];
+      if(val.equals(word1) && (!same || !last)){
+        w1 = i;
+        if(w2 != -1){
+          int dis = w1 - w2;
+          min = dis < min ? dis : min;
+        }
+        last = true;
+      }
+      else if(val.equals(word2) && (!same || last)){
+        w2 = i;
+        if(w1 != -1){
+          int dis = w2 - w1;
+          min = dis < min ? dis : min;
+        }
+        last = false;
+      }
+    }
+    return min;
+  }
+}
