84558c4fac6615ee1f700bf6e8bef15fd8e44962
Wei Shuai <relentless.code@gmail.com>
2016-11-11 20:04:39 -0500

    315

---
+++ src/org/wshuai/leetcode/test/PalindromePermutationIITest.java
@@ -0,0 +1,17 @@
+package org.wshuai.leetcode.test;
+
+import org.junit.Test;
+import org.wshuai.leetcode.PalindromePermutationII;
+
+import java.util.List;
+
+/**
+ * Created by Wei on 11/11/16.
+ */
+public class PalindromePermutationIITest {
+  @Test
+  public void testcase(){
+    PalindromePermutationII pp = new PalindromePermutationII();
+    List<String> s =  pp.generatePalindromes("aabb");
+  }
+}
