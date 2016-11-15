96089b1328e84f7a5de43e678e63b761cbaa18ab
Wei Shuai <relentless.code@gmail.com>
2016-11-14 23:59:06 -0500

    324

---
+++ src/org/wshuai/leetcode/FlattenNestedListIterator.java
@@ -0,0 +1,49 @@
+package org.wshuai.leetcode;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by Wei on 11/14/16.
+ * #341 https://leetcode.com/problems/flatten-nested-list-iterator/
+ */
+public class FlattenNestedListIterator implements Iterator<Integer> {
+  private int pos;
+  private List<Integer> list;
+
+  public FlattenNestedListIterator(List<NestedInteger> nestedList) {
+    pos = 0;
+    list = new ArrayList<Integer>();
+    if(nestedList != null){
+      flattenList(nestedList);
+    }
+  }
+
+  private void flattenList(List<NestedInteger> nestedList){
+    for(NestedInteger ni: nestedList){
+      if(ni.isInteger()){
+        list.add(ni.getInteger());
+      }else{
+        flattenList(ni.getList());
+      }
+    }
+  }
+
+  @Override
+  public Integer next() {
+    return list.get(pos++);
+  }
+
+  @Override
+  public boolean hasNext() {
+    return pos < list.size();
+  }
+
+  @Override
+  public void remove(){
+    throw new NotImplementedException();
+  }
+}
