bd253772c98cd08520233a46780fa73e57067d3a
relentlesscoder <relentless.code@gmail.com>
2017-01-24 20:39:34 -0500

    362

---
+++ src/org/wshuai/leetcode/FindPermutation.java
@@ -0,0 +1,41 @@
+package org.wshuai.leetcode;
+
+/**
+ * Created by Wei on 1/24/2017.
+ * #484 https://leetcode.com/problems/find-permutation/
+ */
+public class FindPermutation {
+  public int[] findPermutation(String s) {
+    int len = s.length();
+    int nLen = len+1;
+    int[] res = new int[nLen];
+    for(int i = 0; i < nLen; i++){
+      res[i] = i+1;
+    }
+    int cLen = 0;
+    for(int i = 0; i < len; i++){
+      char c = s.charAt(i);
+      if(c == 'D'){
+        int j = i;
+        while(j < len && s.charAt(j) == 'D'){
+          cLen++;
+          j++;
+        }
+        reverse(res, i, i+cLen);
+        i = j;
+      }
+      cLen = 0;
+    }
+    return res;
+  }
+
+  private void reverse(int[] res, int i, int j){
+    while(j > i){
+      int temp = res[i];
+      res[i] = res[j];
+      res[j] = temp;
+      i++;
+      j--;
+    }
+  }
+}
---
+++ src/org/wshuai/leetcode/test/FindPermutationTest.java
@@ -0,0 +1,15 @@
+package org.wshuai.leetcode.test;
+
+import org.junit.Test;
+import org.wshuai.leetcode.FindPermutation;
+
+/**
+ * Created by Wei on 1/24/2017.
+ */
+public class FindPermutationTest {
+  @Test
+  public void testcase(){
+    FindPermutation fp = new FindPermutation();
+    int[] r = fp.findPermutation("DDIIDI");
+  }
+}
