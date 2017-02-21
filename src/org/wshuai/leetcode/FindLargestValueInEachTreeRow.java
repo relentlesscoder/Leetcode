3fa3ae1b27006faf13a8cbbc2c819bd59a440fd2
Wei Shuai <relentless.code@gmail.com>
2017-02-20 22:03:34 -0500

    380

---
+++ src/org/wshuai/leetcode/FindLargestValueInEachTreeRow.java
@@ -0,0 +1,41 @@
+package org.wshuai.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by Wei on 2/20/17.
+ * #515 https://leetcode.com/problems/find-largest-value-in-each-tree-row/
+ */
+public class FindLargestValueInEachTreeRow {
+  //O(n), binary tree level order traversal
+  public List<Integer> largestValues(TreeNode root) {
+    List<Integer> res = new ArrayList<Integer>();
+    if(root == null){
+      return res;
+    }
+    Queue<TreeNode> curr = new LinkedList<TreeNode>();
+    Queue<TreeNode> nxt = new LinkedList<TreeNode>();
+    curr.offer(root);
+    int max = Integer.MIN_VALUE;
+    while(!curr.isEmpty()){
+      TreeNode node = curr.poll();
+      max = max < node.val ? node.val : max;
+      if(node.left != null){
+        nxt.offer(node.left);
+      }
+      if(node.right != null){
+        nxt.offer(node.right);
+      }
+      if(curr.isEmpty()){
+        res.add(max);
+        max = Integer.MIN_VALUE;
+        curr = nxt;
+        nxt = new LinkedList<TreeNode>();
+      }
+    }
+    return res;
+  }
+}
---
+++ src/org/wshuai/leetcode/Heaters.java
@@ -0,0 +1,33 @@
+package org.wshuai.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Created by Wei on 2/20/17.
+ * #475 https://leetcode.com/problems/heaters/
+ */
+public class Heaters {
+  public int findRadius(int[] houses, int[] heaters) {
+    Arrays.sort(heaters);
+    int len = houses.length;
+    int res = Integer.MIN_VALUE;
+    for(int i = 0; i < len; i++){
+      int house = houses[i];
+      int idx = Arrays.binarySearch(heaters, house);
+      //cannot find house in heaters
+      if(idx < 0){
+        //get insert position
+        idx = -(idx+1);
+        //corner case 1 - house located at the left of all heaters
+        int left = idx-1 >= 0 ? house-heaters[idx-1] : heaters[0]-house;
+        //corner case 2 - house located at the right of all heaters
+        int right = idx < heaters.length ? heaters[idx]-house : house-heaters[heaters.length-1];
+        res = Math.max(res, Math.min(left, right));
+      }else{
+        //find house in heaters, which means the required min radius is 0
+        res = Math.max(res, 0);
+      }
+    }
+    return res;
+  }
+}
