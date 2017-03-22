be9a67aa317528f6b8c16c3a13ad14c3eb65c9a2
Wei Shuai <relentless.code@gmail.com>
2017-03-21 21:45:59 -0400

    409

---
+++ src/org/wshuai/leetcode/HIndexII.java
@@ -0,0 +1,27 @@
+package org.wshuai.leetcode;
+
+/**
+ * Created by Wei on 3/14/17.
+ * #275 https://leetcode.com/problems/h-index-ii/
+ */
+public class HIndexII {
+  public int hIndex(int[] citations) {
+    if(citations == null || citations.length == 0){
+      return 0;
+    }
+    int len = citations.length;
+    int i = 0;
+    int j = len;
+    while(i < j){
+      int mid = i+(j-i)/2;
+      if(citations[mid] == len-mid){
+        return (len-mid);
+      }else if(citations[mid] < len-mid){
+        i = mid+1;
+      }else{
+        j = mid;
+      }
+    }
+    return len-i;
+  }
+}
