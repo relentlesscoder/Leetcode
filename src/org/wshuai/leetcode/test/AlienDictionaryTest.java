126e2446a639f0913994c38858dc0b9b2ba6a613
Wei Shuai <relentless.code@gmail.com>
2017-07-06 09:17:59 -0400

    435

---
+++ src/org/wshuai/leetcode/test/AlienDictionaryTest.java
@@ -0,0 +1,16 @@
+package org.wshuai.leetcode.test;
+
+import org.junit.Test;
+import org.wshuai.leetcode.AlienDictionary;
+
+/**
+ * Created by Wei on 7/4/17.
+ */
+public class AlienDictionaryTest {
+  @Test
+  public void testcase(){
+    AlienDictionary ad = new AlienDictionary();
+    String x = ad.alienOrder(new String[]{
+      "wrt","wrf","er","ett","rftt"});
+  }
+}
