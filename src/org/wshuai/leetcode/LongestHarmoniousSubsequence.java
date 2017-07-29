eb75d9bff1095dd73d5c55d6e63feed8937342ae
Wei Shuai <relentless.code@gmail.com>
2017-07-29 12:24:30 -0400

    98

---
+++ src/org/wshuai/leetcode/LongestHarmoniousSubsequence.java
@@ -0,0 +1,27 @@
+package org.wshuai.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Wei on 7/29/2017.
+ * #594 https://leetcode.com/problems/longest-harmonious-subsequence/
+ */
+public class LongestHarmoniousSubsequence {
+  public int findLHS(int[] nums) {
+    if(nums == null || nums.length == 0){
+      return 0;
+    }
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for(int num: nums){
+      map.put(num, map.getOrDefault(num, 0) + 1);
+    }
+    int result = 0;
+    for(int key : map.keySet()){
+      if(map.containsKey(key + 1)){
+        result = Math.max(result, map.get(key + 1) + map.get(key));
+      }
+    }
+    return result;
+  }
+}
