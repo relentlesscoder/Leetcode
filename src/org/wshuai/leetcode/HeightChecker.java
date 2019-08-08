905de4b49c483c87487d096fc8fcaa85c4c54544
relentlesscoder <relentless.code@gmail.com>
2019-08-08 15:05:33 -0400

    922

---
+++ src/org/wshuai/leetcode/HeightChecker.java
@@ -0,0 +1,21 @@
+package org.wshuai.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Created by Wei on 8/8/19.
+ * #1051 https://leetcode.com/problems/height-checker/
+ */
+public class HeightChecker {
+    public int heightChecker(int[] heights) {
+        int[] aux = heights.clone();
+        Arrays.sort(aux);
+        int res = 0;
+        for(int i = 0; i < heights.length; i++){
+            if(heights[i] != aux[i]){
+                res++;
+            }
+        }
+        return res;
+    }
+}
---
+++ src/org/wshuai/leetcode/SortArrayByParityII.java
@@ -0,0 +1,23 @@
+package org.wshuai.leetcode;
+
+/**
+ * Created by Wei on 8/8/19.
+ * #922 https://leetcode.com/problems/sort-array-by-parity-ii/
+ */
+public class SortArrayByParityII {
+    public int[] sortArrayByParityII(int[] A) {
+        int odd = 1;
+        for(int even = 0; even < A.length; even += 2){
+            if(A[even] % 2 == 1){
+                while(A[odd] % 2 == 1){
+                    odd += 2;
+                }
+
+                int tmp = A[even];
+                A[even] = A[odd];
+                A[odd] = tmp;
+            }
+        }
+        return A;
+    }
+}
