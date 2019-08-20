2ce8c70367d412bc4a98be242f6f5c8d95be1ea5
relentlesscoder <relentless.code@gmail.com>
2019-08-19 21:52:12 -0400

    local

--- src/org/wshuai/leetcode/BuddyStrings.java
+++ src/org/wshuai/leetcode/BuddyStrings.java
@@ -1,4 +1,43 @@
 package org.wshuai.leetcode;
 
+/**
+ * Created by Wei on 8/19/19.
+ * #859 https://leetcode.com/problems/buddy-strings/
+ */
 public class BuddyStrings {
+    public boolean buddyStrings(String A, String B) {
+        int len1 = A.length();
+        int len2 = B.length();
+        if(len1 != len2 || len1 < 2){
+            return false;
+        }
+        // handles the case that two strings are equal
+        if(A.equals(B)){
+            int[] count = new int[26];
+            for (int i = 0; i < A.length(); i++){
+                count[A.charAt(i) - 'a']++;
+            }
+            // only if there is any repetitive character
+            for (int c: count){
+                if (c > 1) return true;
+            }
+            return false;
+        }else{
+            int count = 0;
+            int[] diff = new int[2];
+            for(int i = 0; i < len1; i++){
+                if(A.charAt(i) == B.charAt(i)){
+                    continue;
+                }
+                if(count >= 2){
+                    return false;
+                }
+                diff[count] = i;
+                count++;
+            }
+            return (count == 2)
+                    && (A.charAt(diff[0]) == B.charAt(diff[1]))
+                    && (A.charAt(diff[1]) == B.charAt(diff[0]));
+        }
+    }
 }
--- src/org/wshuai/leetcode/DayOfTheYear.java
+++ src/org/wshuai/leetcode/DayOfTheYear.java
@@ -1,4 +1,20 @@
 package org.wshuai.leetcode;
 
+/**
+ * Created by Wei on 8/19/19.
+ * #1154 https://leetcode.com/problems/day-of-the-year/
+ */
 public class DayOfTheYear {
+    public int dayOfYear(String date) {
+        NumberOfDaysInAMonth nod = new NumberOfDaysInAMonth();
+        String[] nums = date.split("-");
+        int year = Integer.parseInt(nums[0]);
+        int month = Integer.parseInt(nums[1]);
+        int day = Integer.parseInt(nums[2]);
+        int res = 0;
+        for(int i = 1; i < month; i++){
+            res += nod.numberOfDays(year, i);
+        }
+        return res + day;
+    }
 }
