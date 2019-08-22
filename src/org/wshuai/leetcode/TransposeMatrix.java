999e31069c0f5a761c2bcdcc03a04efe0e0d9638
relentlesscoder <relentless.code@gmail.com>
2019-08-22 11:37:52 -0400

    582

---
+++ src/org/wshuai/leetcode/TransposeMatrix.java
@@ -0,0 +1,19 @@
+package org.wshuai.leetcode;
+
+/**
+ * Created by Wei on 8/22/19.
+ * #867 https://leetcode.com/problems/transpose-matrix/
+ */
+public class TransposeMatrix {
+    public int[][] transpose(int[][] A) {
+        int r = A.length;
+        int c = A[0].length;
+        int[][] res = new int[c][r];
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                res[j][i] = A[i][j];
+            }
+        }
+        return res;
+    }
+}
