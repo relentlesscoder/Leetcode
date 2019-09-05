4c1a8931a2e2937404e5f3d92ec96d843f969135
relentlesscoder <relentless.code@gmail.com>
2019-09-04 21:08:41 -0400

    622

---
+++ src/org/wshuai/leetcode/SubarrayProductLessThanK.java
@@ -0,0 +1,27 @@
+package org.wshuai.leetcode;
+
+/**
+ * Created by Wei on 9/4/19.
+ * #713 https://leetcode.com/problems/subarray-product-less-than-k/
+ */
+public class SubarrayProductLessThanK {
+
+    // sliding window
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if(k <= 1){
+            return 0;
+        }
+        int prod = 1;
+        int ans = 0;
+        int left = 0;
+        for(int right = 0; right < nums.length; right++){
+            prod *= nums[right];
+            while(prod >= k){
+                prod /= nums[left++];
+            }
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+
+}
---
+++ src/org/wshuai/leetcode/test/SubarrayProductLessThanKTest.java
@@ -0,0 +1,12 @@
+package org.wshuai.leetcode.test;
+
+import org.junit.Test;
+import org.wshuai.leetcode.SubarrayProductLessThanK;
+
+public class SubarrayProductLessThanKTest {
+    @Test
+    public void testcase(){
+        SubarrayProductLessThanK sp = new SubarrayProductLessThanK();
+        int count = sp.numSubarrayProductLessThanK(new int[]{10,5,2,6}, 100);
+    }
+}
