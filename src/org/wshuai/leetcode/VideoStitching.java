f00f31dd1d9081a843271a24ee698dcbe4ef9a55
relentlesscoder <relentless.code@gmail.com>
2019-09-26 20:52:30 -0400

    727

---
+++ src/org/wshuai/leetcode/VideoStitching.java
@@ -0,0 +1,34 @@
+package org.wshuai.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Created by Wei on 9/26/19.
+ * #1024 https://leetcode.com/problems/video-stitching/
+ */
+public class VideoStitching {
+
+	public int videoStitching(int[][] clips, int T) {
+		Arrays.sort(clips, (a, b) -> a[0] - b[0]);
+		int count = 0;
+		int curend = 0;
+
+		for(int i = 0; i < clips.length; ) {
+			if(clips[i][0] > curend) {
+				return -1;
+			}
+			int maxend = curend;
+			while(i < clips.length && clips[i][0] <= curend) { // while one clip's start is before or equal to current end
+				maxend = Math.max(maxend, clips[i][1]); // find out the one with the max possible end
+				i++;
+			}
+			count++;
+			curend = maxend;
+			if(curend >= T) {
+				return count;
+			}
+		}
+		return -1;
+	}
+
+}
