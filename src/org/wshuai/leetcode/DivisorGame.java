808489f8cea232ddf4bc4489976a52bbcbcafbea
Wei Shuai <relentless.code@gmail.com>
2019-10-01 21:24:07 -0400

    763

---
+++ src/org/wshuai/leetcode/DivisorGame.java
@@ -0,0 +1,13 @@
+package org.wshuai.leetcode;
+
+/**
+ * Created by Wei on 10/1/19.
+ * #1025 https://leetcode.com/problems/divisor-game/
+ */
+public class DivisorGame {
+
+  // pointless :(
+  public boolean divisorGame(int N) {
+    return N % 2 == 0;
+  }
+}
