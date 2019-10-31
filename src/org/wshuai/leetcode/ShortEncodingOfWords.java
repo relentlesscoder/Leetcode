a984d7837c2c80f7318276e6b8ac8e0934f76e3f
Wei Shuai <relentless.code@gmail.com>
2019-10-31 19:07:27 -0400

    910

--- src/org/wshuai/leetcode/ShortEncodingOfWords.java
+++ src/org/wshuai/leetcode/ShortEncodingOfWords.java
@@ -1,12 +1,44 @@
 package org.wshuai.leetcode;
 
+import java.util.Arrays;
+
 /**
  * Created by Wei on 10/31/2019.
  * #820 https://leetcode.com/problems/short-encoding-of-words/
  */
 public class ShortEncodingOfWords {
 
-	// slow
+	// 43 ms trie
+	public int minimumLengthEncoding(String[] words) {
+		Arrays.sort(words, (a, b) -> b.length() - a.length());
+		int res = 0;
+		TrieNode root = new TrieNode();
+		for(String s : words){
+			if(tryInsert(s, root)){
+				res += s.length() + 1;
+			}
+		}
+		return res;
+	}
+
+	private boolean tryInsert(String s, TrieNode root){
+		boolean res = false;
+		char[] arr = s.toCharArray();
+		TrieNode curr = root;
+		for(int i = arr.length - 1; i >= 0; i--){
+			if(!curr.containsKey(arr[i])){
+				res = true;
+				curr.put(arr[i], new TrieNode());
+			}
+			curr = curr.get(arr[i]);
+		}
+		if(res){
+			curr.setEnd();
+		}
+		return res;
+	}
+
+	// 406 ms slow
 	public int minimumLengthEncodingUnionFind(String[] words) {
 		int N = words.length;
 		int[] root = new int[N];
