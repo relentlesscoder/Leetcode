545a165f02031aa3ae380acfd0a2d06d57e295bf
relentlesscoder <relentless.code@gmail.com>
2019-11-26 15:50:33 -0500

    1060

---
+++ src/org/wshuai/leetcode/CoinPath.java
@@ -0,0 +1,46 @@
+package org.wshuai.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Wei on 11/26/19.
+ * #656 https://leetcode.com/problems/coin-path/
+ */
+public class CoinPath {
+	// see proof at https://leetcode.com/problems/coin-path/discuss/106291/Java-22-lines-solution-with-proof
+	public List<Integer> cheapestJump(int[] A, int B) {
+		if(A[0] == -1){
+			return new ArrayList<>();
+		}
+		int N = A.length;
+		int[] dp = new int[N];
+		int[] path = new int[N]; // previous index
+		int[] len = new int[N]; // length
+		for(int i = 0; i < N; i++){
+			dp[i] = Integer.MAX_VALUE;
+			path[i] = -1;
+		}
+		dp[0] = 0;
+		for(int i = 0; i < N; i++){
+			if(A[i] == -1){
+				continue;
+			}
+			for(int j = Math.max(0, i - B); j < i; j++){
+				if(A[j] == -1){
+					continue;
+				}
+				if(dp[j] + A[i] < dp[i] || (dp[j] + A[i] == dp[i] && len[i] < len[j] + 1)){
+					dp[i] = dp[j] + A[i];
+					path[i] = j;
+					len[i] = len[j] + 1;
+				}
+			}
+		}
+		List<Integer> res = new ArrayList<>();
+		for (int cur = N - 1; cur >= 0; cur = path[cur]){
+			res.add(0, cur + 1);
+		}
+		return res.get(0) != 1 ? new ArrayList<>() : res;
+	}
+}
---
+++ src/org/wshuai/leetcode/KInversePairsArray.java
@@ -0,0 +1,35 @@
+package org.wshuai.leetcode;
+
+/**
+ * Created by Wei on 11/26/19.
+ * #629 https://leetcode.com/problems/k-inverse-pairs-array/
+ */
+public class KInversePairsArray {
+	// https://leetcode.com/problems/k-inverse-pairs-array/discuss/104815/Java-DP-O(nk)-solution
+	public int kInversePairs(int n, int k) {
+		int mod = 1_000_000_007;
+		//max possible inverse pairs (array is in descending order)
+		int max = n*(n - 1)/2;
+		if(k > max || k < 0){
+			return 0;
+		}
+		if(k == 0 || k == max){
+			return 1;
+		}
+		long[][] dp = new long[n + 1][k + 1];
+		dp[2][0] = 1;
+		dp[2][1] = 1;
+		for(int i = 3; i <= n; i++){
+			dp[i][0] = 1;
+			int cur = i*(i-1)/2;
+			for(int j = 1; j <= Math.min(k, cur); j++){
+				dp[i][j] = dp[i][j-1] + dp[i-1][j];
+				if(j >= i){
+					dp[i][j] -= dp[i-1][j-i];
+				}
+				dp[i][j] = (dp[i][j] + mod) % mod;
+			}
+		}
+		return (int)dp[n][k];
+	}
+}
