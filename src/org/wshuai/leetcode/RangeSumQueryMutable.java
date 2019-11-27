ca0187aca9740dbd26b82af5a7209f2e50cc36b1
Wei Shuai <relentless.code@gmail.com>
2019-11-27 10:04:01 -0500

    segment tree

--- src/org/wshuai/leetcode/RangeSumQueryMutable.java
+++ src/org/wshuai/leetcode/RangeSumQueryMutable.java
@@ -1,55 +1,67 @@
 package org.wshuai.leetcode;
 
+import org.wshuai.algorithm.segmentTree.SegmentTreeNode;
+
 /**
  * Created by Wei on 9/25/2016.
  * #307 https://leetcode.com/problems/range-sum-query-mutable/
  */
 public class RangeSumQueryMutable {
-	private int[] btree;
-	private int[] arr;
+	private int N;
+	private SegmentTreeNode root;
 
 	public RangeSumQueryMutable(int[] nums) {
-		if (nums == null) {
-			throw new IllegalArgumentException("Invalid input.");
-		}
-
-		btree = new int[nums.length + 1];
-		arr = nums;
-
-		for (int i = 0; i < nums.length; i++) {
-			add(i + 1, nums[i]);
-		}
-	}
-
-	private void add(int i, int val) {
-		for (int j = i; j < btree.length; j = j + (j & (-j))) {
-			btree[j] += val;
-		}
+		N = nums.length;
+		root = buildSegmentTree(nums, 0, N - 1);
 	}
 
 	public void update(int i, int val) {
-		if (validateIdx(i)) {
-			add(i + 1, val - arr[i]);
-			arr[i] = val;
+		updateUtil(root, i, val);
+	}
+
+	private void updateUtil(SegmentTreeNode root, int i, int val){
+		if(root == null){
+			return;
 		}
+		int mid = root.start + (root.end - root.start) / 2;
+		if(i > mid){
+			updateUtil(root.right, i, val);
+		}else{
+			updateUtil(root.left, i, val);
+		}
+		if(root.start == i && root.start == root.end){
+			root.sum = val;
+			return;
+		}
+		root.sum = root.left.sum + root.right.sum;
 	}
 
 	public int sumRange(int i, int j) {
-		if (validateIdx(i) && validateIdx(j)) {
-			return sumHelper(j + 1) - sumHelper(i);
-		}
-		return 0;
+		return sumRangeUtil(root, i, j);
 	}
 
-	public int sumHelper(int i) {
-		int sum = 0;
-		for (int j = i; j >= 1; j = j - (j & (-j))) {
-			sum += btree[j];
+	private int sumRangeUtil(SegmentTreeNode root, int i, int j){
+		if(root == null || i > root.end || j < root.start || i > j){
+			return 0;
 		}
-		return sum;
+		if(i <= root.start && j >= root.end){
+			return root.sum;
+		}
+		int mid = root.start + (root.end - root.start) / 2;
+		return sumRangeUtil(root.left, i, Math.min(mid, j)) +
+			sumRangeUtil(root.right, Math.max(mid + 1, i), j);
 	}
 
-	private boolean validateIdx(int i) {
-		return i >= 0 && i < arr.length;
+	private SegmentTreeNode buildSegmentTree(int[] nums, int i, int j){
+		if(i > j){
+			return null;
+		}
+		if(i == j){
+			return new SegmentTreeNode(i, j, nums[i], null, null);
+		}
+		int mid = i + (j - i) / 2;
+		SegmentTreeNode left = buildSegmentTree(nums, i, mid);
+		SegmentTreeNode right = buildSegmentTree(nums, mid + 1, j);
+		return new SegmentTreeNode(i, j, left.sum + right.sum, left, right);
 	}
 }
