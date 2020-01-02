f764f9c90085a1efafeb8f8030fe61c5d5190069
relentlesscoder <relentless.code@gmail.com>
2020-01-02 18:15:44 -0500

    1200-1

--- src/org/wshuai/leetcode/MinimumUniqueWordAbbreviation.java
+++ src/org/wshuai/leetcode/MinimumUniqueWordAbbreviation.java
@@ -1,102 +1,114 @@
 package org.wshuai.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Wei on 12/31/2019.
  * #411 https://leetcode.com/problems/minimum-unique-word-abbreviation/
  */
 public class MinimumUniqueWordAbbreviation {
-	//N represents the length of the target string
-	//Cand is the bitwise OR of all the bit conversion of each of the strings in dictionary
-	//bn = (1 << n)
-	//minab = One possible solution of a bit subset/ abbreviation of min lnegth
-	//minLen = the length of a word abbreviation of min length
-	int n, cand, bn, minLen, minab;
-	//List of all the words in their binary converted form
-	List<Integer> dict = new ArrayList<>();
-
-	/*Logic of this method:
-	 Whenever you have a sequence of k consecutive 0's you can replace them with a number.
-	 You gain k characters, but then you lose 1 from adding the number, therefore the actual length
-	 is k-1. We do this count locally however by viewing adjacent bits in the binary number*/
-	public int abbrLen(int mask){
-		int count = n; //All of the characters are turned on
-		//3 = 0b11
-		for (int b = 3; b < bn; b <<= 1){
-			if ((mask & b) == 0) count--;
-		}
-
-		return count;
-	}
-
-	//Here usually for dfs we would track the position in the binary number we want to set.
-	//In this implementation they just set the 1 already for us in advance
-
-	void dfs(int bit, int mask){
-		// System.out.println("dfs(" + Integer.toBinaryString(mask) + ")");
-		//Compute the actual length of the abbreviation
-		int len = abbrLen(mask);
-		//Some smart pruning. (Don't even bother looking at possible abbreviations that are suboptimal)
-		if (len >= minLen) return;
-		//Assume that this bitmask(subset of target) that we are testing out will work
-		boolean match = true;
-		for (Integer d : dict){
-			//Remember we are trying to check if for this mask we have at least 1 conflict
-			//If we have at least 1 conflict with each string then this means this abbreviation can
-			//be considered to be unique
-			if ((mask & d) == 0){
-				match = false;
-				break; //We can break out here because don't have to find all possible abbreviations
-			}
-		}
-		//New candidate (update)
-		if (match){
-			minLen = len; //If we have found a proper cover then update the min value
-			minab = mask;
-		}else {
-			//Bit is a binary number with 1 bit set
-			for (int b = bit; b < bn; b <<= 1){
-				if ((cand & b) != 0) dfs(b << 1, mask + b);
-			}
-		}
-	}
+	private int minLen;
+	private int result;
 
 	public String minAbbreviation(String target, String[] dictionary) {
-
-		n = target.length();
-		bn = 1 << n;
-		cand = 0;
-		minLen = Integer.MAX_VALUE;
-		StringBuilder res = new StringBuilder();
-
-		//Preprocessing where we convert each string to a "match" binary number
-		for (String s : dictionary){
-			int word = 0; //This will be our integer match binary number
-			//The only strings that can have conflicting abbr are ones of the same length
-			if (s.length() != n) continue;
-			for (int i = 0; i < n; i++)
-				if (target.charAt(i) != s.charAt(i)) word |= 1 << i;
-			//Add to converted integer dictionary list.
-			dict.add(word);
-			//Add to bit set of bitwise OR'ed candidate 1's list. (Use for optimization in dfs)
-			cand |= word;
-		}
-
-		dfs(1, 0); //Now perform the bfs. Initial state (0b1, 0) (msb, mask)
-
-		for(int i = 0; i < n;){
-			if ((minab & (1 << i)) != 0){
-				//If we have a 1 set, that means to keep this character
-				res.append(target.charAt(i++));
-			}else{//Otherwise we potentially have consecutive 0's so count them and replace it with the numebr
-				int j = i;
-				while (i < n && (minab & (1 << i)) == 0) i++;
-				res.append(i-j);
+		// only keep words whose length == target in new dict, then compute their bit masks
+		Set<Integer> maskSet = new HashSet<>();
+		for(String s: dictionary){
+			if(target.length() == s.length()){
+				maskSet.add(getBitMask(s,target));
 			}
 		}
 
-		return res.toString();
+		// dfs with pruning
+		minLen = target.length()+1;
+		result = -1;
+
+		dfs(target,maskSet,0,0,0);
+
+		if(minLen > target.length()){
+			return "";
+		}
+
+		// convert result to word
+		int zeroCnt = 0;
+		String res = "";
+		for (int i = target.length()-1; i>=0; i--) {
+			//遇到0要累加连续零个数,遇到1填原char
+			int digit = (result & 1);
+			if(digit == 0){
+				++zeroCnt;
+			} else {
+				if(zeroCnt > 0){
+					res = zeroCnt + res;
+					zeroCnt =0;
+				}
+				res = target.charAt(i) + res;
+			}
+			result >>= 1;
+		}
+		if(zeroCnt > 0) res = zeroCnt + res;
+		return res;
+	}
+
+	/**
+	 *
+	 * @param target
+	 * @param maskSet masks of words in dict
+	 * @param start idx at target
+	 * @param curLen current abbr's length
+	 */
+	private void dfs(String target,Set<Integer> maskSet,int start,int curLen,int curResult){
+		// pruning, no need to continue, already not min length
+		if(curLen >= minLen) return;
+
+		if(start == target.length()){
+			// check whether curResult mask conflicts with words in dict
+			for(int mask:maskSet){
+				/**
+				 * 单词manipulation的缩写m2ip6n可以转化为100110000001
+				 *  m a n i p u l a t i o n
+				 m  2  i p      6      n
+				 1 0 0 1 1 0 0 0 0 0 0 1
+				 * 0代表随意不care,如果这个mask和dict中某个mask的所有1重合代表在意的位置完全相同,
+				 * 说明这个mask和dict中那个词冲突
+				 * 我们要找的是不冲突的mask
+				 */
+				if((curResult & mask) == curResult){
+					return; // conflict
+				}
+			}
+			// no conflict happens, can use
+			if(minLen > curLen){
+				minLen = curLen;
+				result = curResult;
+			}
+			return;
+		}
+
+		// case 1: replace chars from start in target with number
+		for (int i = start; i < target.length(); i++) {
+			//被replace掉的char位置由0代替所以是curResult<<(i+1-start),没replace掉的这里不管,我们只管到i,之后的由backtrack内决定
+			//注意:不允许word => w11d这种用数字代替但含义不同
+			if(curLen == 0 || (curResult &1) == 1){
+				//后者即上一次是保留了字母
+				dfs(target,maskSet,i+1,curLen+1,curResult<<(i+1-start));
+			}
+		}
+
+		// case 2: no replace from start (curResult << 1)+1代表新的这位保留了char,所以是加一
+		dfs(target,maskSet,start+1,curLen+1,(curResult << 1)+1);
+	}
+
+	// 比如apple 和 amper 字母相同设1,不同设0,所以得到10100
+	private int getBitMask(String s1,String s2){
+		int mask = 0;
+		for (int i = 0; i < s1.length(); i++) {
+			mask <<= 1;
+			if(s1.charAt(i) == s2.charAt(i)){
+				mask += 1;
+			}
+		}
+		return mask;
 	}
 }
