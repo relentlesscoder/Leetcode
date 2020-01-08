530232f9a27db29f2ac818e964c22c106f5085f7
Wei Shuai <relentless.code@gmail.com>
2020-01-08 09:13:57 -0500

    r1-37

--- src/org/wshuai/leetcode/SudokuSolver.java
+++ src/org/wshuai/leetcode/SudokuSolver.java
@@ -1,58 +1,58 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 11/9/16.
- * #37 https://leetcode.com/problems/sudoku-solver/
+ * Created by Wei on 11/09/2016.
+ * #0037 https://leetcode.com/problems/sudoku-solver/
  */
 public class SudokuSolver {
 	public void solveSudoku(char[][] board) {
-		if (board == null || board.length != 9 || board[0].length != 9) {
-			return;
-		}
-		solveSudokuUtil(board, 0, 0);
+		dfs(0, 0, board);
 	}
 
-	private boolean solveSudokuUtil(char[][] board, int i, int j) {
-		if (i == 9) {
+	private boolean dfs(int row, int col, char[][] board){
+		if(row == 9){
 			return true;
 		}
-		if (j >= 9) {
-			return solveSudokuUtil(board, i + 1, 0);
+		if(col == 9){
+			return dfs(row + 1, 0, board);
 		}
-		if (board[i][j] == '.') {
-			for (int k = 1; k <= 9; k++) {
-				board[i][j] = (char) (k + '0');
-				if (isValid(board, i, j)) {
-					if (solveSudokuUtil(board, i, j + 1)) {
+		if(board[row][col] == '.'){
+			for(char i = 1; i <= 9; i++){
+				board[row][col] = (char)(i + '0');
+				if(isValid(board, row, col)){
+					if(dfs(row, col + 1, board)){
 						return true;
 					}
 				}
-				board[i][j] = '.';
+				board[row][col] = '.';
 			}
-		} else {
-			return solveSudokuUtil(board, i, j + 1);
+		}else{
+			return dfs(row, col + 1, board);
 		}
 		return false;
 	}
 
-	private boolean isValid(char[][] board, int i, int j) {
-		for (int k = 0; k < 9; k++) {
-			if (k != i && board[k][j] == board[i][j]) {
+	private boolean isValid(char[][] board, int i, int j){
+		// check columns affected by board[i][j]
+		for(int k = 0; k < 9; k++){
+			if(k != i && board[k][j] == board[i][j]){
 				return false;
 			}
 		}
 
-		for (int k = 0; k < 9; k++) {
-			if (k != j && board[i][j] == board[i][k]) {
+		// check the current row
+		for(int k = 0; k < 9; k++){
+			if(k != j && board[i][j] == board[i][k]){
 				return false;
 			}
 		}
 
-		int r = i / 3 * 3;
-		int c = j / 3 * 3;
-		for (int x = r; x < r + 3; x++) {
-			for (int y = c; y < c + 3; y++) {
-				if (x != i && y != j && board[x][y] == board[i][j]) {
+		// check the 3x3 sub-box
+		int r = i/3*3;
+		int c = j/3*3;
+		for(int x = r; x < r + 3; x++){
+			for(int y = c; y < c + 3; y++){
+				if(x != i && y != j && board[x][y] == board[i][j]){
 					return false;
 				}
 			}
--- src/org/wshuai/leetcode/ValidSudoku.java
+++ src/org/wshuai/leetcode/ValidSudoku.java
@@ -1,71 +1,59 @@
 package org.wshuai.leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 /**
- * Created by Wei on 11/8/16.
- * #36 https://leetcode.com/problems/valid-sudoku/
+ * Created by Wei on 11/08/2016.
+ * #0036 https://leetcode.com/problems/valid-sudoku/
  */
 public class ValidSudoku {
+	// time O(n^2)
 	public boolean isValidSudoku(char[][] board) {
-		if (board == null) {
-			return false;
-		}
+		int[] used = new int[9];
 
-		int rLen = board.length;
-		if (rLen != 9) {
-			return false;
-		}
-		int cLen = board[0].length;
-		if (cLen != 9) {
-			return false;
-		}
-
-		Set<Character> set = new HashSet<Character>();
-		for (int i = 0; i < rLen; i++) {
-			set.clear();
-			for (int j = 0; j < cLen; j++) {
-				char x = board[i][j];
-				if (x == '.') {
+		// check rows
+		for(int i = 0; i < 9; i++){
+			Arrays.fill(used, 0);
+			for(int j = 0; j < 9; j++){
+				char val = board[i][j];
+				if(val == '.'){
 					continue;
 				}
-				if (x >= '1' && x <= '9' && !set.contains(x)) {
-					set.add(x);
-				} else {
+				if(used[val - '1'] > 0){
 					return false;
 				}
+				used[val - '1'] = 1;
 			}
 		}
 
-		for (int i = 0; i < cLen; i++) {
-			set.clear();
-			for (int j = 0; j < rLen; j++) {
-				char x = board[j][i];
-				if (x == '.') {
+		// check columns
+		for(int i = 0; i < 9; i++){
+			Arrays.fill(used, 0);
+			for(int j = 0; j < 9; j++){
+				char val = board[j][i];
+				if(val == '.'){
 					continue;
 				}
-				if (x >= '1' && x <= '9' && !set.contains(x)) {
-					set.add(x);
-				} else {
+				if(used[val - '1'] > 0){
 					return false;
 				}
+				used[val - '1'] = 1;
 			}
 		}
 
-		for (int b = 0; b < 9; b++) {
-			set.clear();
-			for (int i = b / 3 * 3; i < b / 3 * 3 + 3; i++) {
-				for (int j = b % 3 * 3; j < b % 3 * 3 + 3; j++) {
-					char x = board[i][j];
-					if (x == '.') {
+		// check 3x3 sub-boxes
+		for(int k = 0; k < 9; k++){
+			Arrays.fill(used, 0);
+			for(int i = k/3*3; i < k/3*3 + 3; i++){
+				for(int j = k%3*3; j < k%3*3 + 3; j++){
+					char val = board[j][i];
+					if(val == '.'){
 						continue;
 					}
-					if (x >= '1' && x <= '9' && !set.contains(x)) {
-						set.add(x);
-					} else {
+					if(used[val - '1'] > 0){
 						return false;
 					}
+					used[val - '1'] = 1;
 				}
 			}
 		}
