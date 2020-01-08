278048bfdeead11bf5ef719e83eb3c1636b32f73
Wei Shuai <relentless.code@gmail.com>
2020-01-07 23:02:46 -0500

    minor change

--- src/org/wshuai/leetcode/MedianOfTwoSortedArrays.java
+++ src/org/wshuai/leetcode/MedianOfTwoSortedArrays.java
@@ -22,8 +22,8 @@ public class MedianOfTwoSortedArrays {
 			0 1 2 3 4 5 6 7 8 9 10
 			0 1 2 3 4 5 6 7 8 9
 
-			(10 + 1) / 2 = 6
-			10 / 2 = 5
+			(11 + 1) / 2 = 6
+			(10 + 1) / 2 = 5
 
 			(len + 1) / 2 is the left of two median numbers
 			*/
