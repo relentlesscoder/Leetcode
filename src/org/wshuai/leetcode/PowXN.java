714d0142d55a2c6f5f8eeb1b6f9b861e3b686350
Wei Shuai <relentless.code@gmail.com>
2020-01-09 09:09:22 -0500

    r1-50

--- src/org/wshuai/leetcode/PowXN.java
+++ src/org/wshuai/leetcode/PowXN.java
@@ -2,26 +2,24 @@ package org.wshuai.leetcode;
 
 /**
  * Created by Wei on 10/12/2016.
- * #50 https://leetcode.com/problems/powx-n/
+ * #0050 https://leetcode.com/problems/powx-n/
  */
 public class PowXN {
+	// time log(n)
 	public double myPow(double x, int n) {
-		double r = myPowUtil(x, Math.abs(n));
-		return n < 0 ? 1 / r : r;
+		double res = getPow(x, Math.abs(n));
+		return n < 0 ? 1 / res : res;
 	}
 
-	private double myPowUtil(double x, int n) {
-		if (n == 0) {
+	private double getPow(double x, int n){
+		if(n == 0){
 			return 1.0;
 		}
-		if (n == 1) {
+		if(n == 1){
 			return x;
 		}
-		double t = myPowUtil(x, n / 2);
-		if (n % 2 == 0) {
-			return t * t;
-		} else {
-			return t * t * x;
-		}
+		double pow = getPow(x, n / 2);
+		double prod = pow * pow;
+		return n % 2 == 0 ? prod : prod * x;
 	}
 }
