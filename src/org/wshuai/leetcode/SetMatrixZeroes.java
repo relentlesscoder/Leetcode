f63d36e533920d7aef11eafd7434c43b8b23bddb
relentlesscoder <relentless.code@gmail.com>
2020-01-10 11:20:05 -0500

    r1-75

--- src/org/wshuai/leetcode/SetMatrixZeroes.java
+++ src/org/wshuai/leetcode/SetMatrixZeroes.java
@@ -1,76 +1,40 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 10/8/16.
- * #73 https://leetcode.com/problems/set-matrix-zeroes/
+ * Created by Wei on 10/08/2016.
+ * #0073 https://leetcode.com/problems/set-matrix-zeroes/
  */
 public class SetMatrixZeroes {
+	// time O(m*n), space O(1)
 	public void setZeroes(int[][] matrix) {
-		if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+		if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
 			return;
 		}
-
-		int rows = matrix.length;
-		int cols = matrix[0].length;
-
-		//Using first row and column to record if
-		// the rest rows and columns need to be set to zeros
-		boolean setFirstRow = false;
-		boolean setFirstCol = false;
-
-		for (int i = 0; i < cols; i++) {
-			if (matrix[0][i] == 0) {
-				setFirstRow = true;
-				break;
+		int r = matrix.length, c = matrix[0].length, col0 = 1;
+		// use left(top) of each row(column) to store the reset status
+		for(int i = 0; i < r; i++){
+			// only one exception: use col0 flag to record status for column 1
+			if(matrix[i][0] == 0){
+				col0 = 0;
 			}
-		}
-
-		for (int i = 0; i < rows; i++) {
-			if (matrix[i][0] == 0) {
-				setFirstCol = true;
-				break;
-			}
-		}
-
-		for (int i = 1; i < rows; i++) {
-			for (int j = 1; j < cols; j++) {
-				if (matrix[i][j] == 0) {
-					matrix[0][j] = 0;
-					matrix[i][0] = 0;
+			for(int j = 1; j < c; j++){
+				if(matrix[i][j] == 0){
+					matrix[i][0] = matrix[0][j] = 0;
 				}
 			}
 		}
 
-		for (int i = 1; i < cols; i++) {
-			if (matrix[0][i] == 0) {
-				setColZeros(i, matrix, rows);
+		// reset cells from bottom right since we need the column reset flags in the first row
+		for(int i = r - 1; i >= 0; i--){
+			for(int j = c - 1; j >= 1; j--){
+				if(matrix[i][0] == 0 || matrix[0][j] == 0){
+					matrix[i][j] = 0;
+				}
 			}
-		}
-
-		for (int i = 1; i < rows; i++) {
-			if (matrix[i][0] == 0) {
-				setRowZeros(i, matrix, cols);
+			// finally reset the first in the row only if the first column reset flag is on
+			if(col0 == 0){
+				matrix[i][0] = 0;
 			}
-		}
-
-		if (setFirstRow) {
-			setRowZeros(0, matrix, cols);
-		}
-
-		if (setFirstCol) {
-			setColZeros(0, matrix, rows);
-		}
-	}
-
-	private void setRowZeros(int row, int[][] matrix, int cols) {
-		for (int i = 0; i < cols; i++) {
-			matrix[row][i] = 0;
-		}
-	}
-
-	private void setColZeros(int col, int[][] matrix, int rows) {
-		for (int i = 0; i < rows; i++) {
-			matrix[i][col] = 0;
 		}
 	}
 }
