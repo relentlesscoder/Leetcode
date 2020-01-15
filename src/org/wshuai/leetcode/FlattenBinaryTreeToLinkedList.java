91dc4a4c0555bddf9deb699047f49f7979cb2d48
relentlesscoder <relentless.code@gmail.com>
2020-01-15 17:16:17 -0500

    r1-115

--- src/org/wshuai/leetcode/FlattenBinaryTreeToLinkedList.java
+++ src/org/wshuai/leetcode/FlattenBinaryTreeToLinkedList.java
@@ -2,28 +2,24 @@ package org.wshuai.leetcode;
 
 /**
  * Created by Wei on 11/10/2016.
- * #114 https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+ * #0114 https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
  */
 public class FlattenBinaryTreeToLinkedList {
+	// time O(n)
 	public void flatten(TreeNode root) {
-		if (root == null) {
-			return;
-		}
-		if (root.left == null && root.right == null) {
+		if(root == null){
 			return;
 		}
 		flatten(root.left);
 		flatten(root.right);
-		TreeNode node = root.right;
-		if (root.left != null) {
+		TreeNode temp = root.right;
+		if(root.left != null){
 			root.right = root.left;
 			root.left = null;
-			while (root.right != null) {
+			while(root.right != null){
 				root = root.right;
 			}
 		}
-		if (node != null) {
-			root.right = node;
-		}
+		root.right = temp;
 	}
 }
--- src/org/wshuai/leetcode/PathSum.java
+++ src/org/wshuai/leetcode/PathSum.java
@@ -1,43 +1,21 @@
 package org.wshuai.leetcode;
 
-import java.util.Stack;
-
 /**
  * Created by Wei on 10/21/2016.
- * #112 https://leetcode.com/problems/path-sum/
+ * #0112 https://leetcode.com/problems/path-sum/
  */
 public class PathSum {
+	// time O(n)
 	public boolean hasPathSum(TreeNode root, int sum) {
-		if (root == null) {
+		if(root == null){
 			return false;
 		}
-
-		Stack<TreeNode> stk = new Stack<TreeNode>();
-		stk.push(root);
-		TreeNode curr = root;
-		int path = root.val;
-		while (!stk.empty()) {
-			if (curr != null && curr.left != null) {
-				path += curr.left.val;
-				stk.push(curr.left);
-				curr = curr.left;
-			} else {
-				TreeNode n = stk.peek();
-				if (n.right != null) {
-					path += n.right.val;
-					stk.push(n.right);
-					curr = n.right;
-					n.right = null;
-				} else {
-					if (curr != null && path == sum) {
-						return true;
-					}
-					stk.pop();
-					path -= n.val;
-					curr = null;
-				}
-			}
+		if(root.left == null
+				&& root.right == null
+				&& sum == root.val){
+			return true;
 		}
-		return false;
+		return hasPathSum(root.left, sum - root.val)
+				|| hasPathSum(root.right, sum - root.val);
 	}
 }
