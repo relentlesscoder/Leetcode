1f083a4839c0e83507f32dfc378f8b9ddee3c252
Wei Shuai <relentless.code@gmail.com>
2020-01-16 22:20:22 -0500

    r1-127

--- src/org/wshuai/leetcode/BinaryTreeMaximumPathSum.java
+++ src/org/wshuai/leetcode/BinaryTreeMaximumPathSum.java
@@ -2,40 +2,29 @@ package org.wshuai.leetcode;
 
 /**
  * Created by Wei on 10/21/2016.
- * #124 https://leetcode.com/problems/binary-tree-maximum-path-sum/
+ * #0124 https://leetcode.com/problems/binary-tree-maximum-path-sum/
  */
 public class BinaryTreeMaximumPathSum {
+	private int res;
+
+	// time O(n)
 	public int maxPathSum(TreeNode root) {
-		MaxVal maxVal = new MaxVal();
-		maxVal.val = Integer.MIN_VALUE;
-		int maxCross = findMaxPathSum(root, maxVal);
-		int mv = maxVal.val;
-		return maxCross > mv ? maxCross : mv;
+		res = Integer.MIN_VALUE;
+		dfs(root);
+		return res;
 	}
 
-	private int findMaxPathSum(TreeNode node, MaxVal maxVal) {
-		if (node == null) {
+	private int dfs(TreeNode root){
+		if(root == null){
 			return 0;
 		}
-		int nval = node.val;
-		int left = findMaxPathSum(node.left, maxVal);
-		int right = findMaxPathSum(node.right, maxVal);
-
-		//For each node if the val path go through it, there are 3 scenarios:
-		//1. left + node
-		//2. right + node
-		//3. cross
-		int gc = left > right ? left : right;
-		int maxChild = gc > 0 ? gc + nval : nval;
-		int crs = left + right + nval;
-		int maxTop = crs > maxChild ? crs : maxChild;
-		int mv = maxVal.val;
-		maxVal.val = mv > maxTop ? mv : maxTop;
-		//return val child path for the parent node calculation
-		return maxChild;
-	}
-
-	class MaxVal {
-		public int val;
+		int left = dfs(root.left);
+		int right = dfs(root.right);
+		left = left > 0 ? left : 0;
+		right = right > 0 ? right : 0;
+		// cross sum
+		int max = root.val + left + right;
+		res = Math.max(max, res);
+		return Math.max(left + root.val, right + root.val);
 	}
 }
