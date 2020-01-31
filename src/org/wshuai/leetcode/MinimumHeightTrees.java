c91cf24fc432702438e705bf30c613b068703bf0
Wei Shuai <relentless.code@gmail.com>
2020-01-30 21:29:28 -0500

    local

--- src/org/wshuai/leetcode/MinimumHeightTrees.java
+++ src/org/wshuai/leetcode/MinimumHeightTrees.java
@@ -3,100 +3,49 @@ package org.wshuai.leetcode;
 import java.util.*;
 
 /**
- * Created by Wei on 3/20/17.
- * #310 https://leetcode.com/problems/minimum-height-trees/
+ * Created by Wei on 03/20/2017.
+ * #0310 https://leetcode.com/problems/minimum-height-trees/
  */
 public class MinimumHeightTrees {
 
-	//O(n), see https://discuss.leetcode.com/topic/30572/share-some-thoughts/2
+	// time O(V + E), space O(V + E)
+	// https://discuss.leetcode.com/topic/30572/share-some-thoughts/2
 	public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-		List<Integer> lst = new ArrayList<Integer>();
-		if (n <= 0) {
-			return lst;
+		List<Integer> res = new ArrayList<>();
+		if(n == 0){
+			return res;
 		}
-		if (n == 1) {
-			lst.add(0);
-			return lst;
+		// only one vertex and no edges
+		if(edges == null || edges.length == 0){
+			res.add(0);
+			return res;
 		}
-		List<Set<Integer>> adj = new ArrayList<Set<Integer>>();
-		for (int i = 0; i < n; i++) {
-			adj.add(new HashSet<Integer>());
+		Set<Integer>[] adj = new HashSet[n];
+		for(int i = 0; i < n; i++){
+			adj[i] = new HashSet<>();
 		}
-		for (int i = 0; i < edges.length; i++) {
-			adj.get(edges[i][0]).add(edges[i][1]);
-			adj.get(edges[i][1]).add(edges[i][0]);
+		for(int[] e : edges){
+			adj[e[0]].add(e[1]);
+			adj[e[1]].add(e[0]);
 		}
-		for (int i = 0; i < n; i++) {
-			if (adj.get(i).size() == 1) {
-				lst.add(i);
+		List<Integer> leaves = new ArrayList<>();
+		for(int i = 0; i < n; i++){
+			if(adj[i].size() == 1){
+				leaves.add(i);
 			}
 		}
-		while (n > 2) {
-			n -= lst.size();
-			List<Integer> nLst = new ArrayList<Integer>();
-			for (int i : lst) {
-				int j = adj.get(i).iterator().next();
-				adj.get(j).remove(i);
-				if (adj.get(j).size() == 1) {
-					nLst.add(j);
+		while(n > 2){
+			n -= leaves.size();
+			List<Integer> newLeaves = new ArrayList<>();
+			for(int cur : leaves){
+				int next = adj[cur].iterator().next();
+				adj[next].remove(cur);
+				if(adj[next].size() == 1){
+					newLeaves.add(next);
 				}
 			}
-			lst = nLst;
+			leaves = newLeaves;
 		}
-		return lst;
-	}
-
-	//TLE
-	public List<Integer> findMinHeightTreesNaive(int n, int[][] edges) {
-		if (n <= 0) {
-			return new ArrayList<Integer>();
-		}
-		int min = Integer.MAX_VALUE;
-		List<List<Integer>> adj = new ArrayList<List<Integer>>();
-		for (int i = 0; i < n; i++) {
-			adj.add(new ArrayList<Integer>());
-		}
-		for (int i = 0; i < edges.length; i++) {
-			adj.get(edges[i][0]).add(edges[i][1]);
-			adj.get(edges[i][1]).add(edges[i][0]);
-		}
-		List<Integer> lst = new ArrayList<Integer>();
-		for (int i = 0; i < n; i++) {
-			int height = getTreeHight(i, adj);
-			if (height == min) {
-				lst.add(i);
-			} else if (height < min) {
-				lst = new ArrayList<Integer>();
-				lst.add(i);
-				min = height;
-			}
-		}
-		return lst;
-	}
-
-	private int getTreeHight(int i, List<List<Integer>> adj) {
-		int res = 0;
-		Set<Integer> visited = new HashSet<Integer>();
-		Queue<Integer> queue = new LinkedList<Integer>();
-		Queue<Integer> next = new LinkedList<Integer>();
-		queue.offer(i);
-		visited.add(i);
-		while (!queue.isEmpty()) {
-			int node = queue.poll();
-			List<Integer> lst = adj.get(node);
-			for (int j = 0; j < lst.size(); j++) {
-				int nnode = lst.get(j);
-				if (!visited.contains(nnode)) {
-					visited.add(nnode);
-					next.offer(nnode);
-				}
-			}
-			if (queue.isEmpty()) {
-				res++;
-				queue = next;
-				next = new LinkedList<Integer>();
-			}
-		}
-		return res - 1;
+		return leaves;
 	}
 }
