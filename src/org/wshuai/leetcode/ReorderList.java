5505b9a41f43835ce12a0510bad5f40530b051e0
Wei Shuai <relentless.code@gmail.com>
2020-02-01 00:34:03 -0500

    r1-310

--- src/org/wshuai/leetcode/ReorderList.java
+++ src/org/wshuai/leetcode/ReorderList.java
@@ -1,7 +1,5 @@
 package org.wshuai.leetcode;
 
-import java.util.Stack;
-
 /**
  * Created by Wei on 10/21/2016.
  * #0143 https://leetcode.com/problems/reorder-list/
@@ -9,74 +7,44 @@ import java.util.Stack;
 public class ReorderList {
 	// time O(n), space O(1)
 	public void reorderList(LinkedListNode head) {
-		if (head == null) {
-			return;
-		}
-
-		// Find the middle
-		LinkedListNode fast = head;
-		LinkedListNode slow = head;
-		while (fast != null) {
-			slow = slow.next;
-			fast = fast.next;
-			if (fast != null) {
-				fast = fast.next;
-			}
-		}
-
-		// Split the list
-		LinkedListNode it = head;
-		while (it.next != slow) {
-			it = it.next;
-		}
-		it.next = null;
-
-		// Reverse the 2nd half
-		LinkedListNode prev = null;
-		LinkedListNode curr = slow;
-		while (curr != null) {
-			LinkedListNode nxt = curr.next;
-			curr.next = prev;
-			prev = curr;
-			curr = nxt;
-		}
-
-		// Reorder
-		LinkedListNode left = head;
-		LinkedListNode right = prev;
-		while (right != null) {
-			LinkedListNode nxtLeft = left.next;
-			LinkedListNode nxtRight = right.next;
-			left.next = right;
-			right.next = nxtLeft;
-			left = nxtLeft;
-			right = nxtRight;
-		}
-	}
-
-	// time O(n), space O(n)
-	public void reorderListStack(LinkedListNode head) {
 		if(head == null){
 			return;
 		}
-		Stack<LinkedListNode> stack = new Stack<>();
-		LinkedListNode cur = head;
-		while(cur != null){
-			stack.push(cur);
-			cur = cur.next;
+		// split the list into two lists
+		// for list that has odd number of nodes,
+		// the seond list has one less node
+		LinkedListNode slow = head, fast = head, prev = null;
+		while(fast != null && fast.next != null){
+			prev = slow;
+			fast = fast.next.next;
+			slow = slow.next;
 		}
-		cur = head;
-		while(!stack.isEmpty() && cur != stack.peek()){
-			LinkedListNode tail = stack.peek();
-			if(cur.next == tail){
-				break;
-			}
-			stack.pop();
+		// handle the odd list
+		if(fast != null){
+			prev = slow;
+			slow = slow.next;
+		}
+		// split the node
+		prev.next = null;
+
+		// reverse the second list
+		prev = null;
+		LinkedListNode cur = slow;
+		while(cur != null){
 			LinkedListNode next = cur.next;
-			cur.next = tail;
-			tail.next = next;
+			cur.next = prev;
+			prev = cur;
 			cur = next;
 		}
-		stack.peek().next = null;
+
+		// merge the two lists
+		LinkedListNode left = head, right = prev;
+		while(right != null){
+			LinkedListNode next = right.next;
+			right.next = left.next;
+			left.next = right;
+			left = right.next;
+			right = next;
+		}
 	}
 }
