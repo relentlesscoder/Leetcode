be219da0e715562984cf73c50bca180679ce4d09
relentlesscoder <relentless.code@gmail.com>
2020-02-04 16:12:33 -0500

    r1-330

--- src/org/wshuai/leetcode/CountOfRangeSum.java
+++ src/org/wshuai/leetcode/CountOfRangeSum.java
@@ -1,51 +1,110 @@
 package org.wshuai.leetcode;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 /**
- * Created by Wei on 8/3/17.
- * #327 https://leetcode.com/problems/count-of-range-sum/
+ * Created by Wei on 08/03/2017.
+ * #0327 https://leetcode.com/problems/count-of-range-sum/
  */
 public class CountOfRangeSum {
+
+	// time O(n*log(n))
 	public int countRangeSum(int[] nums, int lower, int upper) {
-		List<Long> cand = new ArrayList<Long>();
-		cand.add(Long.MIN_VALUE);
-		cand.add(0L);
-		long[] sum = new long[nums.length + 1];
-		for (int i = 1; i < sum.length; i++) {
+		if(nums == null || nums.length == 0 || lower > upper){
+			return 0;
+		}
+		int index = 0, count = 0;
+		long[] sum = new long[nums.length + 1], cand = new long[3 * sum.length + 1];
+		cand[index++] = sum[0];
+		cand[index++] = lower + sum[0] - 1;
+		cand[index++] = upper + sum[0];
+
+		for(int i = 1; i < sum.length; i++){
 			sum[i] = sum[i - 1] + nums[i - 1];
-			cand.add(sum[i]);
-			cand.add(lower + sum[i - 1] - 1);
-			cand.add(upper + sum[i - 1]);
+			cand[index++] = sum[i];
+			cand[index++] = lower + sum[i] - 1;
+			cand[index++] = upper + sum[i];
 		}
-		Collections.sort(cand);
 
-		int[] bit = new int[cand.size()];
-		for (int i = 0; i < sum.length; i++) {
-			add(bit, Collections.binarySearch(cand, sum[i]), 1);
+		cand[index] = Long.MIN_VALUE;
+		Arrays.sort(cand);
+
+		int[] bit = new int[cand.length];
+		for(int i = 0; i < sum.length; i++){
+			add(bit, Arrays.binarySearch(cand, sum[i]), 1);
 		}
-		int ans = 0;
-		for (int i = 1; i < sum.length; i++) {
-			add(bit, Collections.binarySearch(cand, sum[i - 1]), -1);
-			ans += find(bit, Collections.binarySearch(cand, upper + sum[i - 1]));
-			ans -= find(bit, Collections.binarySearch(cand, lower + sum[i - 1] - 1));
+		for(int i = 0; i < sum.length; i++){
+			add(bit, Arrays.binarySearch(cand, sum[i]), -1);
+
+			count += query(bit, Arrays.binarySearch(cand, upper + sum[i]));
+
+			count -= query(bit, Arrays.binarySearch(cand, lower + sum[i] - 1));
 		}
-		return ans;
+
+		return count;
 	}
 
-	private void add(int[] bit, int idx, int delta) {
-		for (int i = idx; i < bit.length; i += i & (-i)) {
-			bit[i] += delta;
+	private void add(int[] bit, int index, int value){
+		while(index < bit.length){
+			bit[index] += value;
+			index += (index & -index);
 		}
 	}
 
-	private int find(int[] bit, int idx) {
+	private int query(int[] bit, int index){
 		int sum = 0;
-		for (int i = idx; i > 0; i -= i & (-i)) {
-			sum += bit[i];
+		while(index > 0){
+			sum += bit[index];
+			index -= (index & -index);
 		}
 		return sum;
+	}
+
+	// time O(n*(log(n))^2)
+	public int countRangeSumMergeSort(int[] nums, int lower, int upper) {
+		if(nums == null || nums.length == 0 || lower > upper){
+			return 0;
+		}
+		return mergeSort(nums, 0, nums.length - 1, lower, upper);
+	}
+
+	private int mergeSort(int[] nums, int l, int r, int lower, int upper){
+		if(l == r){
+			return nums[l] >= lower && nums[r] <= upper ? 1 : 0;
+		}
+		int m = l + (r - l) / 2, count = 0;
+		long[] arr = new long[r - m];
+		long sum = 0;
+
+		// find the prefix sum of the right part
+		for(int i = m + 1; i <= r; i++){
+			sum += nums[i];
+			arr[i - (m + 1)] = sum;
+		}
+		// sort the prefix sum so that we can do binary search on it later
+		Arrays.sort(arr);
+		sum = 0;
+		// time O(n*log(n))
+		for(int i = m; i >= l; i--){
+			sum += nums[i];
+			// go from back to front on the left part
+			// find the insertion point for upper - (left) sum and lower - (left) sum
+			// the difference is the number of all right prefix sums that satisfy the criterion
+			count += findIndex(arr, upper - sum + 0.5) - findIndex(arr, lower - sum - 0.5);
+		}
+		return mergeSort(nums, l, m, lower, upper) + mergeSort(nums, m + 1, r, lower, upper) + count;
+	}
+
+	private int findIndex(long[] arr, double val){
+		int l = 0, r = arr.length, m = 0;
+		while(l < r){
+			m = l + (r - l) / 2;
+			if(arr[m] < val){
+				l = m + 1;
+			}else{
+				r = m;
+			}
+		}
+		return l;
 	}
 }
--- src/org/wshuai/leetcode/CountOfSmallerNumbersAfterSelf.java
+++ src/org/wshuai/leetcode/CountOfSmallerNumbersAfterSelf.java
@@ -11,6 +11,7 @@ public class CountOfSmallerNumbersAfterSelf {
 	private int n;
 
 	// time O(n*log(n)), space O(n)
+	// https://www.youtube.com/watch?v=2SVLYsq5W8M
 	// http://community.topcoder.com/i/education/binaryIndexedTrees/BITimg.gif
 	public List<Integer> countSmaller(int[] nums) {
 		List<Integer> res = new ArrayList<>();
@@ -33,7 +34,10 @@ public class CountOfSmallerNumbersAfterSelf {
 		for(int i = nums.length - 1; i >= 0; i--){
 			// use rank as the index of binary indexed tree
 			rank = map.get(nums[i]);
+			// from tail to head find out how many nodes
+			// in the BIT that are less than the current rank
 			res.add(getSum(rank - 1));
+			// insert the current rank
 			update(rank, 1);
 		}
 		Collections.reverse(res);
@@ -55,6 +59,67 @@ public class CountOfSmallerNumbersAfterSelf {
 		while(index <= n){
 			bit[index] += delta;
 			index += (index & -index);
+		}
+	}
+
+
+	// time O(n*log(n)), space O(n)
+	// https://leetcode.com/problems/count-of-smaller-numbers-after-self/discuss/76583/11ms-JAVA-solution-using-merge-sort-with-explanation
+	public List<Integer> countSmallerMergeSort(int[] nums) {
+		List<Integer> res = new ArrayList<>();
+		int n = nums.length;
+		int[] count = new int[n], indexes = new int[n];
+		for(int i = 0; i < n; i++){
+			indexes[i] = i;
+		}
+		mergeSort(nums, indexes, count, 0, n - 1);
+		for(int i = 0; i < n; i++){
+			res.add(count[i]);
+		}
+		return res;
+	}
+
+	private void mergeSort(int[] nums, int[] indexes, int[] count, int start, int end){
+		if(end <= start){
+			return;
+		}
+		int mid = start + (end - start) / 2;
+		mergeSort(nums, indexes, count, start, mid);
+		mergeSort(nums, indexes, count, mid + 1, end);
+		merge(nums, indexes, count, start, end);
+	}
+
+	private void merge(int[] nums, int[] indexes, int[] count, int start, int end){
+		int mid = start + (end - start) / 2, leftIndex = start, rightIndex = mid + 1, rightCount = 0, sortIndex = 0;
+		int[] newIndexes = new int[end - start + 1];
+		while(leftIndex <= mid && rightIndex <= end){
+			// if the value on the right side is smaller, increase the count
+			if(nums[indexes[rightIndex]] < nums[indexes[leftIndex]]){
+				newIndexes[sortIndex] = indexes[rightIndex];
+				rightCount++;
+				rightIndex++;
+			// if the value on the right side is not smaller, add the count to the index
+			// note that in the current merge, the right count is accumulative
+			// example: left [4 7 9], right [2 3 5]
+			// the right count for 4 is 2 and for 7 is 2 + 1 = 3
+			}else{
+				newIndexes[sortIndex] = indexes[leftIndex];
+				count[indexes[leftIndex]] += rightCount;
+				leftIndex++;
+			}
+			sortIndex++;
+		}
+		while(leftIndex <= mid){
+			newIndexes[sortIndex] = indexes[leftIndex];
+			count[indexes[leftIndex]] += rightCount;
+			leftIndex++;
+			sortIndex++;
+		}
+		while(rightIndex <= end){
+			newIndexes[sortIndex++] = indexes[rightIndex++];
+		}
+		for(int i = start; i <= end; i++){
+			indexes[i] = newIndexes[i - start];
 		}
 	}
 }
