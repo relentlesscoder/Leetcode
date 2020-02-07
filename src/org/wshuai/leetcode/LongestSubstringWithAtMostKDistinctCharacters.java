381cbb0de6155297621da1a8abd963523490d22b
Wei Shuai <relentless.code@gmail.com>
2020-02-06 21:16:47 -0500

    r1-340

--- src/org/wshuai/leetcode/LongestSubstringWithAtMostKDistinctCharacters.java
+++ src/org/wshuai/leetcode/LongestSubstringWithAtMostKDistinctCharacters.java
@@ -1,43 +1,25 @@
 package org.wshuai.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Created by Wei on 11/18/2016.
- * #340 https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
+ * #0340 https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
  */
 public class LongestSubstringWithAtMostKDistinctCharacters {
-	//O(n), sliding window
+	// time O(n)
 	public int lengthOfLongestSubstringKDistinct(String s, int k) {
-		if (s == null || s.isEmpty() || k <= 0) {
-			return 0;
-		}
-		int left = 0;
-		int max = 0;
-		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		int len = s.length();
-		for (int i = 0; i < len; i++) {
-			char val = s.charAt(i);
-			if (map.containsKey(val)) {
-				int cnt = map.get(val);
-				map.put(val, cnt + 1);
-			} else {
-				map.put(val, 1);
+		int res = 0, i = 0, j = 0, nums = 0;
+		int[] count = new int[256];
+		for(; i < s.length(); i++){
+			if(count[s.charAt(i)]++ == 0){
+				nums++;
 			}
-			while (map.size() > k) {
-				char lval = s.charAt(left);
-				int cnt = map.get(lval);
-				if (cnt == 1) {
-					map.remove(lval);
-				} else {
-					map.put(lval, cnt - 1);
+			if(nums > k){
+				while(--count[s.charAt(j++)] > 0){
 				}
-				left++;
+				nums--;
 			}
-			int cLen = i - left + 1;
-			max = cLen > max ? cLen : max;
+			res = Math.max(res, i - j + 1);
 		}
-		return max;
+		return res;
 	}
 }
