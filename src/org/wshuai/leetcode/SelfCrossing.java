f7e43eeb94f085f09e4b12a17a4070a5484a7f6b
relentlesscoder <relentless.code@gmail.com>
2020-02-07 14:12:18 -0500

    r1-350

--- src/org/wshuai/leetcode/SelfCrossing.java
+++ src/org/wshuai/leetcode/SelfCrossing.java
@@ -1,28 +1,34 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 8/3/17.
- * #335 https://leetcode.com/problems/self-crossing/
+ * Created by Wei on 08/03/2017.
+ * #0335 https://leetcode.com/problems/self-crossing/
  */
 public class SelfCrossing {
-	// Draw all the possible scenarios of all crosses
+	// time O(n)
+	// draw all the possible scenarios of all crosses
 	public boolean isSelfCrossing(int[] x) {
-		if (x == null || x.length <= 3) {
+		int n = x.length;
+		if(n <= 3){
 			return false;
 		}
-		for (int i = 3; i < x.length; i++) {
-			if (x[i - 1] <= x[i - 3] && x[i] >= x[i - 2]) {
+		for(int i = 3; i < n; i++){
+			// 4th line cross the 1st line
+			if(x[i] >= x[i - 2]
+					&& x[i - 1] <= x[i - 3]){
 				return true;
 			}
-			if (i >= 4) {
-				if (x[i - 1] == x[i - 3] && x[i] + x[i - 4] >= x[i - 2]) {
-					return true;
-				}
+			// 5th line meets first line
+			if(i >= 4 && x[i - 1] == x[i - 3]
+					&& x[i] + x[i - 4] >= x[i - 2]){
+				return true;
 			}
-			if (i >= 5) {
-				if (x[i - 2] >= x[i - 4] && x[i - 1] + x[i - 5] >= x[i - 3] && x[i] >= x[i - 2] - x[i - 4] && x[i - 1] <= x[i - 3]) {
-					return true;
-				}
+			// 6th line cross first line
+			if(i >= 5 && x[i - 2] >= x[i - 4]
+					&& x[i - 1] <= x[i - 3]
+					&& x[i - 1] + x[i - 5] >= x[i - 3]
+					&& x[i] + x[i - 4] >= x[i - 2]){
+				return true;
 			}
 		}
 		return false;
