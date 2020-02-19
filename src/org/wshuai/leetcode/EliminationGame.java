bb49d5351c9c08ea18ae3c057c6cc3aaa221a771
relentlesscoder <relentless.code@gmail.com>
2020-02-19 12:49:24 -0500

    r1-390

--- src/org/wshuai/leetcode/EliminationGame.java
+++ src/org/wshuai/leetcode/EliminationGame.java
@@ -1,12 +1,40 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 4/3/17.
- * #390 https://leetcode.com/problems/elimination-game/
+ * Created by Wei on 04/03/2017.
+ * #0390 https://leetcode.com/problems/elimination-game/
  */
 public class EliminationGame {
-	//See http://blog.csdn.net/mebiuw/article/details/52713918
+	// time O(log(n))
+	// https://leetcode.com/problems/elimination-game/discuss/87121/O(logN)-solution.-clear-break-down
 	public int lastRemaining(int n) {
-		return n == 1 ? 1 : 2 * (n / 2 + 1 - lastRemaining(n / 2));
+		return leftToRight(n);
+	}
+
+	// eliminate [1...n] first from left to right, then alternate
+	private int leftToRight(int n){
+		if(n == 1){
+			return 1;
+		}
+		// scan from left to right is simple, the length of array doesn't matter
+		// [1, 2, 3, 4] -> 2 * [1, 2]
+		// [1, 2, 3, 4, 5] -> 2 * [1, 2]
+		return 2 * rightToLeft(n / 2);
+	}
+
+	// eliminate [1...n] first from right to left, then alternate
+	private int rightToLeft(int n){
+		if(n == 1){
+			return 1;
+		}
+		// if the length of array is even, we will get only odd number
+		// [1, 2, 3, 4] -> [1, 3] = 2 * [1, 2] - 1
+		if(n % 2 == 0){
+			return 2 * leftToRight(n / 2) - 1;
+			// else if the length of array is odd, we will get only even number
+			// [1, 2, 3, 4, 5] -> [2, 4] = 2 * [1, 2]
+		}else{
+			return 2 * leftToRight(n / 2);
+		}
 	}
 }
--- src/org/wshuai/leetcode/RansomNote.java
+++ src/org/wshuai/leetcode/RansomNote.java
@@ -1,41 +1,22 @@
 package org.wshuai.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
- * Created by Wei on 9/19/2016.
+ * Created by Wei on 09/19/2016.
+ * #0383 https://leetcode.com/problems/ransom-note/
  */
 public class RansomNote {
+	// time O(n)
 	public boolean canConstruct(String ransomNote, String magazine) {
-		if (ransomNote == null || magazine == null) {
-			throw new IllegalArgumentException("Invalid input.");
+		int[] count = new int[26];
+		for(char c : magazine.toCharArray()){
+			count[c - 'a']++;
 		}
-		char[] arr1 = ransomNote.toCharArray();
-		char[] arr2 = magazine.toCharArray();
-		int len1 = arr1.length;
-		int len2 = arr2.length;
-		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		for (int i = 0; i < len2; i++) {
-			char k = arr2[i];
-			if (map.containsKey(k)) {
-				int c = map.get(k);
-				map.put(k, c + 1);
-			} else {
-				map.put(k, 1);
-			}
-		}
-		for (int i = 0; i < len1; i++) {
-			char k = arr1[i];
-			Integer c = map.get(k);
-			if (c == null) {
+		for(char c : ransomNote.toCharArray()){
+			int key = c - 'a';
+			count[key]--;
+			if(count[key] < 0){
 				return false;
 			}
-			int x = (int) c;
-			if (x == 0) {
-				return false;
-			}
-			map.put(k, x - 1);
 		}
 		return true;
 	}
