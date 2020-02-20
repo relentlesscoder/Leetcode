4855fffc08fdff42a32081164c596e96a879e6aa
Wei Shuai <relentless.code@gmail.com>
2020-02-20 09:12:50 -0500

    local

--- src/org/wshuai/leetcode/IsSubsequence.java
+++ src/org/wshuai/leetcode/IsSubsequence.java
@@ -1,28 +1,19 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 10/31/16.
- * #392 https://leetcode.com/problems/is-subsequence/
+ * Created by Wei on 10/31/2016.
+ * #0392 https://leetcode.com/problems/is-subsequence/
  */
 public class IsSubsequence {
+	// time O(n)
 	public boolean isSubsequence(String s, String t) {
-		if (s == null || t == null) {
-			return false;
-		}
-
-		int sLen = s.length();
-		int tLen = t.length();
-		int i = 0;
-		int j = 0;
-		while (i < tLen && j < sLen) {
-			if (t.charAt(i) == s.charAt(j)) {
-				i++;
-				j++;
-			} else {
+		int i = 0, j = 0, m = s.length(), n = t.length();
+		while(i < m && j < n){
+			if(s.charAt(i) == t.charAt(j)){
 				i++;
 			}
+			j++;
 		}
-
-		return j == sLen;
+		return i == m;
 	}
 }
