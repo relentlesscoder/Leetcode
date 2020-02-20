c7a527f84dfb2809382f266adb3147686a46d930
relentlesscoder <relentless.code@gmail.com>
2020-02-20 18:50:59 -0500

    r1-405

--- src/org/wshuai/leetcode/ConvertANumberToHexadecimal.java
+++ src/org/wshuai/leetcode/ConvertANumberToHexadecimal.java
@@ -1,21 +1,23 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 11/2/2016.
- * #405 https://leetcode.com/problems/convert-a-number-to-hexadecimal/
+ * Created by Wei on 11/02/2016.
+ * #0405 https://leetcode.com/problems/convert-a-number-to-hexadecimal/
  */
 public class ConvertANumberToHexadecimal {
 	public String toHex(int num) {
-		if (num == 0) {
-			return "0";
-		}
+		char[] map = new char[]{
+				'0','1','2','3','4','5',
+				'6','7','8','9','a','b',
+				'c','d','e','f'
+		};
 		StringBuilder sb = new StringBuilder();
-		char[] map = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-		while (num != 0) {
-			//0x0000000F
-			sb.append(map[num & 15]);
-			num = (num >>> 4);
+		int mask = (1 << 4) - 1;
+		while(num != 0){
+			sb.append(map[num & mask]);
+			// unsigned right shift
+			num = num >>> 4;
 		}
-		return sb.reverse().toString();
+		return sb.length() > 0 ? sb.reverse().toString() : "0";
 	}
 }
