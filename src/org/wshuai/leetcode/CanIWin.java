c1016a20f802335c1bb44812e2052adac85b889a
relentlesscoder <relentless.code@gmail.com>
2020-02-28 12:21:51 -0500

    r1-470

--- src/org/wshuai/leetcode/CanIWin.java
+++ src/org/wshuai/leetcode/CanIWin.java
@@ -2,40 +2,38 @@ package org.wshuai.leetcode;
 
 /**
  * Created by Wei on 10/12/2019.
- * #464 https://leetcode.com/problems/can-i-win/
+ * #0464 https://leetcode.com/problems/can-i-win/
  */
 public class CanIWin {
-	private byte[] map;
-
+	// time O(2^n)
 	public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
-		int sum = maxChoosableInteger * (maxChoosableInteger + 1) / 2;
-		if(sum < desiredTotal){
+		if((maxChoosableInteger * (maxChoosableInteger + 1) >> 1) < desiredTotal){
 			return false;
 		}
 		if(desiredTotal <= 0){
 			return true;
 		}
-		map = new byte[1 << maxChoosableInteger];
-		return canIWin(maxChoosableInteger, desiredTotal, 0);
+		byte[] states = new byte[1 << maxChoosableInteger];
+		return dfs(desiredTotal, 0, maxChoosableInteger, states);
 	}
 
-	private boolean canIWin(int max, int target, int state){
+	private boolean dfs(int target, int cur, int range, byte[] states){
 		if(target <= 0){
 			return false;
 		}
-		if(map[state] != 0){
-			return map[state] == 1;
+		if(states[cur] != 0){
+			return states[cur] == 1;
 		}
-		for(int i = 0; i < max; i++){
-			if((state & (1 << i)) > 0){
+		for(int i = 0; i < range; i++){
+			if(((cur >>> i) & 1) == 1){
 				continue;
 			}
-			if(!canIWin(max, target - (i + 1), state | (1 << i))){
-				map[state] = 1;
+			if(!dfs(target - (i + 1), cur | (1 << i), range, states)){
+				states[cur] = 1;
 				return true;
 			}
 		}
-		map[state] = -1;
+		states[cur] = -1;
 		return false;
 	}
 }
