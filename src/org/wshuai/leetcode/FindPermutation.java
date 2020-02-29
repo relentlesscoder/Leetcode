ef549e11016c0b2229a189a343d04e72a5d3dee3
Wei Shuai <relentless.code@gmail.com>
2020-02-29 10:44:25 -0500

    r1-485

--- src/org/wshuai/leetcode/FindPermutation.java
+++ src/org/wshuai/leetcode/FindPermutation.java
@@ -1,41 +1,40 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 1/24/2017.
- * #484 https://leetcode.com/problems/find-permutation/
+ * Created by Wei on 01/24/2017.
+ * #0484 https://leetcode.com/problems/find-permutation/
  */
 public class FindPermutation {
+	// time O(n)
 	public int[] findPermutation(String s) {
-		int len = s.length();
-		int nLen = len + 1;
-		int[] res = new int[nLen];
-		for (int i = 0; i < nLen; i++) {
+		if(s == null || s.isEmpty()){
+			return new int[0];
+		}
+		int n = s.length();
+		char[] chars = s.toCharArray();
+		int[] res = new int[n + 1];
+		for(int i = 0; i <= n; i++){
 			res[i] = i + 1;
 		}
-		int cLen = 0;
-		for (int i = 0; i < len; i++) {
-			char c = s.charAt(i);
-			if (c == 'D') {
-				int j = i;
-				while (j < len && s.charAt(j) == 'D') {
-					cLen++;
-					j++;
+		for(int i = 0; i < n; i++){
+			if(chars[i] == 'D'){
+				int start = i, end = i;
+				while(i < n && chars[i] == 'D'){
+					end++;
+					i++;
 				}
-				reverse(res, i, i + cLen);
-				i = j;
+				reverse(res, start, end);
+				i--;
 			}
-			cLen = 0;
 		}
 		return res;
 	}
 
-	private void reverse(int[] res, int i, int j) {
-		while (j > i) {
-			int temp = res[i];
-			res[i] = res[j];
-			res[j] = temp;
-			i++;
-			j--;
+	private void reverse(int[] arr, int i, int j){
+		while(i < j){
+			int temp = arr[i];
+			arr[i++] = arr[j];
+			arr[j--] = temp;
 		}
 	}
 }
