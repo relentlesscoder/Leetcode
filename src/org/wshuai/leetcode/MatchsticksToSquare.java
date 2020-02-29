2b104d0afa3281c15ff3a0f325b47d86c4bd2a6a
Wei Shuai <relentless.code@gmail.com>
2020-02-28 19:59:37 -0500

    r1-480

--- src/org/wshuai/leetcode/MatchsticksToSquare.java
+++ src/org/wshuai/leetcode/MatchsticksToSquare.java
@@ -1,44 +1,47 @@
 package org.wshuai.leetcode;
 
+import java.util.Arrays;
+
 /**
- * Created by Wei on 10/27/19.
- * #473 https://leetcode.com/problems/matchsticks-to-square/
+ * Created by Wei on 10/27/2019.
+ * #0473 https://leetcode.com/problems/matchsticks-to-square/
  */
 public class MatchsticksToSquare {
-	private int[] visited;
-
-	// same as #698 Partition to K Equal Sum Subsets
+	// time O(3^n)
 	public boolean makesquare(int[] nums) {
-		if(nums.length < 4){
+		if(nums == null || nums.length < 4){
 			return false;
 		}
-		int sum = 0;
-		for(int v: nums){
-			sum += v;
+		int sum = 0, target = 0;
+		for(int num : nums){
+			sum += num;
 		}
 		if(sum % 4 != 0){
 			return false;
 		}
-		visited = new int[nums.length];
-		return dfs(nums, 0, 0, 4, sum/4);
+		target = sum / 4;
+		Arrays.sort(nums);
+		return dfs(0, 0, 0, 4, target, nums);
 	}
 
-	private boolean dfs(int[] nums, int start, int curr, int k, int target){
-		if(k == 1){
+	private boolean dfs(int start, int sum, int used, int count, int target, int[] nums){
+		if(count == 1){
 			return true;
 		}
-		if(curr == target){
-			return dfs(nums, 0, 0, k-1, target);
+		if(sum == target){
+			return dfs(0, 0, used, count - 1, target, nums);
 		}
-		for(int j = start; j < nums.length; j++){
-			if(visited[j] == 1 || curr + nums[j] > target){
+		for(int i = start; i < nums.length; i++){
+			if(((1 << i) & used) > 0){
 				continue;
 			}
-			visited[j] = 1;
-			if(dfs(nums, j, curr + nums[j], k, target)){
+			// recursion tree pruning
+			if(sum + nums[i] > target){
+				return false;
+			}
+			if(dfs(i, sum + nums[i], (1 << i) | used, count, target, nums)){
 				return true;
 			}
-			visited[j] = 0;
 		}
 		return false;
 	}
