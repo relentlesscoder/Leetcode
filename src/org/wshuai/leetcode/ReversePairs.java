2df3c436a4ea0f252b32712747edb434985eb908
Wei Shuai <relentless.code@gmail.com>
2020-03-03 21:31:53 -0500

    r1-500

--- src/org/wshuai/leetcode/ReversePairs.java
+++ src/org/wshuai/leetcode/ReversePairs.java
@@ -22,17 +22,27 @@ public class ReversePairs {
 		int m = l + (r - l) / 2, i = l, j = m + 1, k = 0, p = m + 1;
 		int[] merge = new int[r - l + 1];
 		int res = mergeSort(nums, l, m) + mergeSort(nums, m + 1, r);
+		// since both left and right part are sorted, the reverse pair
+		// can be carried over when we iterate the left part.
+		// for example, we have left: [10, 12, 15], right[3, 4, 5]
+		// for 10, it can combine with 3 and 4 to form a reverse pair
+		// for 12, since it is greater than or equal to 10, it can also
+		// combine with 3 and 4 to form a reverse pair. and plus the 5
+		// there will be total 3 reverse pairs. Then for the same reason,
+		// 15 will also have 3 reverse pairs.
 		while(i <= m){
 			while(p <= r && nums[i] > 2L * nums[p]){
 				p++;
 			}
 			res += p - m - 1;
+			// sorting
 			while(j <= r && nums[j] < nums[i]){
 				merge[k++] = nums[j++];
 			}
 			merge[k++] = nums[i++];
 		}
 		while(j <= r){
+			// if right part still has elements left
 			merge[k++] = nums[j++];
 		}
 		System.arraycopy(merge, 0, nums, l, r - l + 1);
@@ -44,12 +54,23 @@ public class ReversePairs {
 		if(nums == null || nums.length == 0){
 			return 0;
 		}
+		// general idea:
+		// for each of the element, find it's index using binary search then
+		// save the frequency to the bit. Then iterate the array, for each of
+		// the number nums[i], find the frequency of element that are already
+		// in the bit that are less or equal to 2*nums[i] and add it to the
+		// answer. Add the index of the nums[i] to the bit for following
+		// calculations.
 		int n = nums.length, res = 0;
 		int[] sorted = new int[n], bit = new int[n + 1];
 		System.arraycopy(nums, 0, sorted, 0, n);
 		Arrays.sort(sorted);
 		for(int i = 0; i < n; i++){
+			// use i here because i is not yet added to the bit
+			// so when we are at i = 4, there is only 3 elements in
+			// bit.
 			res += i - getSum(bit, searchIndex(sorted, 2L*nums[i]));
+			// add i to the bit
 			add(bit, searchIndex(sorted, nums[i]));
 		}
 		return res;
