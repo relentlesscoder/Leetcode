f6e8e2759ac4fb2a317a8030d2b61e793d740884
Wei Shuai <relentless.code@gmail.com>
2020-03-07 08:18:48 -0500

    r1-540

--- src/org/wshuai/leetcode/ConvertBSTToGreaterTree.java
+++ src/org/wshuai/leetcode/ConvertBSTToGreaterTree.java
@@ -1,31 +1,29 @@
 package org.wshuai.leetcode;
 
-import java.util.Stack;
-
 /**
- * Created by Wei on 7/20/2017.
- * #538 https://leetcode.com/problems/convert-bst-to-greater-tree/
+ * Created by Wei on 07/20/2017.
+ * #0538 https://leetcode.com/problems/convert-bst-to-greater-tree/
  */
 public class ConvertBSTToGreaterTree {
-	//O(n), Right -> Root -> Left Tree Traversal
+	private int sum;
+
+	// time O(n)
 	public TreeNode convertBST(TreeNode root) {
-		if (root == null) {
-			return root;
+		if(root == null){
+			return null;
 		}
-		int total = 0;
-		Stack<TreeNode> stk = new Stack<TreeNode>();
-		TreeNode curr = root;
-		while (curr != null || !stk.isEmpty()) {
-			if (curr != null) {
-				stk.push(curr);
-				curr = curr.right;
-			} else {
-				curr = stk.pop();
-				curr.val += total;
-				total = curr.val;
-				curr = curr.left;
-			}
-		}
+		sum = 0;
+		dfs(root);
 		return root;
+	}
+
+	private void dfs(TreeNode root){
+		if(root == null){
+			return;
+		}
+		dfs(root.right);
+		root.val += sum;
+		sum = root.val;
+		dfs(root.left);
 	}
 }
