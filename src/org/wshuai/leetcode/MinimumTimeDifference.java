0e244766d8e138ae18af342d21bc34dbedbbb1e2
Wei Shuai <relentless.code@gmail.com>
2020-03-07 09:54:27 -0500

    r1-540

--- src/org/wshuai/leetcode/MinimumTimeDifference.java
+++ src/org/wshuai/leetcode/MinimumTimeDifference.java
@@ -4,25 +4,26 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Created by Wei on 9/15/19.
- * #539 https://leetcode.com/problems/minimum-time-difference/
+ * Created by Wei on 09/15/2019.
+ * #0539 https://leetcode.com/problems/minimum-time-difference/
  */
 public class MinimumTimeDifference {
+	// time O(n*log(n))
 	public int findMinDifference(List<String> timePoints) {
-		int[] arr = new int[timePoints.size()];
-		int i = 0;
-		for (String tp : timePoints) {
-			String[] vals = tp.split(":");
-			arr[i++] = Integer.parseInt(vals[0]) * 60 + Integer.parseInt(vals[1]);
+		int n = timePoints.size(), min = Integer.MAX_VALUE;
+		int[] times = new int[n];
+		for(int i = 0; i < timePoints.size(); i++){
+			String[] strs = timePoints.get(i).split(":");
+			times[i] = Integer.parseInt(strs[0]) * 60 + Integer.parseInt(strs[1]);
 		}
-		int min = Integer.MAX_VALUE;
-		Arrays.sort(arr);
-		for (int j = 0; j < arr.length; j++) {
-			// need to handle the 24 hour wrap case
-			int diff = j == 0 ? 1440 + arr[j] - arr[arr.length - 1] : arr[j] - arr[j - 1];
-			min = Math.min(min, diff);
+		Arrays.sort(times);
+		for(int i = 1; i < n; i++){
+			min = Math.min(times[i] - times[i - 1], min);
+			// handles 24 hours wrap up
+			if(i == n - 1){
+				min = Math.min(times[0] + 24 * 60 - times[i], min);
+			}
 		}
-
 		return min;
 	}
 }
