39bde2b6e542e8e8c2a3a3d06c130cf044058034
Wei Shuai <relentless.code@gmail.com>
2020-03-07 13:53:56 -0500

    r1-540

--- src/org/wshuai/leetcode/LonelyPixelII.java
+++ src/org/wshuai/leetcode/LonelyPixelII.java
@@ -1,55 +1,53 @@
 package org.wshuai.leetcode;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
- * Created by Wei on 9/27/19.
- * #533 https://leetcode.com/problems/lonely-pixel-ii/
+ * Created by Wei on 09/27/2019.
+ * #0533 https://leetcode.com/problems/lonely-pixel-ii/
  */
 public class LonelyPixelII {
+	// time O(m*n), space O(m+n)
 	public int findBlackPixel(char[][] picture, int N) {
-		int r = picture.length;
-		int c = picture[0].length;
-		Map<Integer, List<Integer>> rows = new HashMap<>();
-		Map<Integer, List<Integer>> cols = new HashMap<>();
-		List<int[]> bp = new ArrayList<>();
-		for(int i = 0; i < r; i++){
-			for(int j = 0; j < c; j++){
+		if(picture == null || picture.length == 0 || picture[0].length == 0){
+			return 0;
+		}
+		int res = 0, m = picture.length, n = picture[0].length;
+		Map<String, Integer> map = new HashMap<>();
+		int[] colCount = new int[n];
+		for(int i = 0; i < m; i++){
+			StringBuilder sb = new StringBuilder();
+			int rowCount = 0;
+			for(int j = 0; j < n; j++){
 				if(picture[i][j] == 'B'){
-					if(!rows.containsKey(i)){
-						rows.put(i, new ArrayList<>());
-					}
-					rows.get(i).add(j);
-					if(!cols.containsKey(j)){
-						cols.put(j, new ArrayList<>());
-					}
-					cols.get(j).add(i);
-					bp.add(new int[]{i, j});
+					rowCount++;
+					colCount[j]++;
 				}
+				sb.append(picture[i][j]);
+			}
+			// For each row, add the rwo to map only if
+			// row count of black pixels equals to N.
+			// Therefore map contains all potential rows that contains
+			// black pixels.
+			if(rowCount == N){
+				String key = sb.toString();
+				map.put(key, map.getOrDefault(key, 0) + 1);
 			}
 		}
-		int res = 0;
-		for(int[] b: bp){
-			if(rows.get(b[0]).size() != cols.get(b[1]).size() || rows.get(b[0]).size() != N){
-				continue;
-			}
-			boolean checkRule2 = true;
-			List<Integer> allRows = cols.get(b[1]);
-			List<Integer> firstRow = rows.get(allRows.get(0));
-			int i = 1;
-			while(i < allRows.size()){
-				List<Integer> currRow = rows.get(allRows.get(i));
-				if(!firstRow.equals(currRow)){
-					checkRule2 = false;
-					break;
+		for(String key : map.keySet()){
+			// For each of the potential row, check only if the number
+			// of same rows equals to N. If the count is not N, it
+			// breaks the rule #1.
+			if(map.get(key) == N){
+				// For each of the column of the valid rows, check if the
+				// column (with black pixel) has N black pixels since it's
+				// possible that the column has more than N black pixels.
+				for(int j = 0; j < n; j++){
+					if(key.charAt(j) == 'B' && colCount[j] == N){
+						res += N;
+					}
 				}
-				i++;
-			}
-			if(checkRule2){
-				res++;
 			}
 		}
 		return res;
