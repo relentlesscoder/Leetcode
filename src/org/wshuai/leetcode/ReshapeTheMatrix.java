d08ad38dfda9e079a63ef60f3be18e435173439b
relentlesscoder <relentless.code@gmail.com>
2020-03-12 16:05:04 -0400

    r1-570

--- src/org/wshuai/leetcode/ReshapeTheMatrix.java
+++ src/org/wshuai/leetcode/ReshapeTheMatrix.java
@@ -1,24 +1,24 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 7/18/17.
- * #566 https://leetcode.com/problems/reshape-the-matrix/
+ * Created by Wei on 07/18/2017.
+ * #0566 https://leetcode.com/problems/reshape-the-matrix/
  */
 public class ReshapeTheMatrix {
+	// time O(m*n)
 	public int[][] matrixReshape(int[][] nums, int r, int c) {
-		if (nums == null || nums.length == 0 || nums[0].length == 0) {
+		if(nums == null || nums.length == 0 || nums[0].length == 0){
 			return nums;
 		}
-		int rl = nums.length;
-		int cl = nums[0].length;
-		if (rl * cl < r * c) {
+		int m = nums.length, n = nums[0].length;
+		if(m * n != r * c){
 			return nums;
 		}
 		int[][] res = new int[r][c];
-		for (int i = 0; i < r; i++) {
-			for (int j = 0; j < c; j++) {
-				int idx = c * i + j;
-				res[i][j] = nums[idx / cl][idx % cl];
+		for(int i = 0; i < m; i++){
+			for(int j = 0; j < n; j++){
+				int cur = i * n + j;
+				res[cur / c][cur % c] = nums[i][j];
 			}
 		}
 		return res;
