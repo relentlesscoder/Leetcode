07361fb5c907db08d7207eba3c21fccc3a6b426e
relentlesscoder <relentless.code@gmail.com>
2020-03-12 17:22:29 -0400

    r1-570

--- src/org/wshuai/leetcode/ArrayNesting.java
+++ src/org/wshuai/leetcode/ArrayNesting.java
@@ -1,26 +1,28 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 10/9/2019.
- * #565 https://leetcode.com/problems/array-nesting/
+ * Created by Wei on 10/09/2019.
+ * #0565 https://leetcode.com/problems/array-nesting/
  */
 public class ArrayNesting {
+	// time O(n), space O(n)
 	public int arrayNesting(int[] nums) {
-		int res = 0;
-		boolean[] visited = new boolean[nums.length];
-		for(int i = 0; i < nums.length; i++){
-			if(visited[i]){
+		int res = 0, n = nums.length;
+		boolean[] visited = new boolean[n];
+		for (int i = 0; i < n; i++) {
+			if (visited[i]) {
 				continue;
 			}
-			int count = 0;
-			int j = i;
-			while(nums[j] != i){
-				visited[j] = true;
-				j = nums[j];
-				count++;
-			}
-			res = Math.max(res, count + 1);
+			res = Math.max(res, dfs(i, nums, visited));
 		}
 		return res;
+	}
+
+	private int dfs(int cur, int[] nums, boolean[] visited) {
+		if (visited[cur]) {
+			return 0;
+		}
+		visited[cur] = true;
+		return 1 + dfs(nums[cur], nums, visited);
 	}
 }
--- src/org/wshuai/leetcode/PermutationInString.java
+++ src/org/wshuai/leetcode/PermutationInString.java
@@ -1,51 +1,50 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 9/25/19.
- * #567 https://leetcode.com/problems/permutation-in-string/
+ * Created by Wei on 09/25/2019.
+ * #0567 https://leetcode.com/problems/permutation-in-string/
  */
 public class PermutationInString {
-
-	// sliding window
+	// time O(n)
 	public boolean checkInclusion(String s1, String s2) {
-		if(s2.length() < s1.length()){
+		if (s1.length() > s2.length()) {
 			return false;
 		}
-		int[] c1 = new int[26];
-		for(char c: s1.toCharArray()){
-			c1[c-'a']++;
+		int[] count = new int[26];
+		for (char c : s1.toCharArray()) {
+			count[c - 'a']++;
 		}
-		int i = 0;
-		char[] c2 = s2.toCharArray();
-		while(i < c2.length){
-			if(c1[c2[i]-'a'] == 0){
-				i++;
-				continue;
-			}
-			int[] tmp = new int[26];
-			for(int k = 0; k < 26; k++){
-				tmp[k] = c1[k];
-			}
-			int j = i;
-			while(j < c2.length && tmp[c2[j]-'a'] > 0){
-				tmp[c2[j]-'a']--;
-				j++;
-			}
-			if(j - i == s1.length()){
-				return true;
-			}
-			// example: s1 - abcdac s2 - exqybacaacbd
-			// when the first attempt to match (from index 4, character b) reaches index 8, character a
-			// update the starting point to index 6 try to match it again
-			if(j < c2.length && c1[c2[j]-'a'] > 0){
-				while(i < j && c2[i] != c2[j]){
-					i++;
+		int[] cur = count.clone();
+		char[] arr = s2.toCharArray();
+		for (int i = 0, j = 0; j < arr.length; j++) {
+			char c = arr[j];
+			int index = c - 'a';
+			// if s1 does not contains current character, then the current substring
+			// is invalid and we need restart the search from the next character
+			if (count[index] == 0) {
+				i = j + 1;
+				cur = count.clone();
+			} else if (cur[index] > 0) {
+				// if current s1 contains current character and all the characters in s1 are
+				// visited then we've found a match
+				if (--cur[index] == 0) {
+					int sum = 0;
+					for (int k = 0; k < 26; k++) {
+						sum += cur[k];
+					}
+					if (sum == 0) {
+						return true;
+					}
 				}
-				while(i < j && c2[i] == c2[j]){
-					i++;
+			} else {
+				// if current s1 contains current character c but we already matched all the c
+				// we need to set the head of current search to next character right after the
+				// first c from the current head
+				while (arr[i] != c) {
+					cur[arr[i++] - 'a']++;
 				}
-			}else{
-				i = j;
+				cur[arr[i++] - 'a']++;
+				j--;
 			}
 		}
 		return false;
