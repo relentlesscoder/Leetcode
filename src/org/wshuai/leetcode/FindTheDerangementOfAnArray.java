4ec67623562e9a9649e21e9936aa79e0a2d7b4d3
relentlesscoder <relentless.code@gmail.com>
2020-03-19 16:14:42 -0400

    r1-640

--- src/org/wshuai/leetcode/FindTheDerangementOfAnArray.java
+++ src/org/wshuai/leetcode/FindTheDerangementOfAnArray.java
@@ -1,38 +1,26 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 11/11/19.
- * #634 https://leetcode.com/problems/find-the-derangement-of-an-array/
+ * Created by Wei on 11/11/2019.
+ * #0634 https://leetcode.com/problems/find-the-derangement-of-an-array/
  */
 public class FindTheDerangementOfAnArray {
+	private static final long MOD = 1_000_000_007;
 
-	// optimize the space to O(1)
+	// time O(n)
+	// https://leetcode.com/problems/find-the-derangement-of-an-array/discuss/114008/Java-O(1)-space-details-about-how-to-getting-to-the-recurrence-relations
+	// https://leetcode.com/problems/find-the-derangement-of-an-array/discuss/104981/If-you-don't-understand
 	public int findDerangement(int n) {
 		if(n <= 1){
 			return 0;
 		}
-		long mod = 1_000_000_007;
 		long dp1 = 0;
 		long dp2 = 1;
 		for(int i = 3; i <= n; i++){
-			long cur = ((i - 1) * (dp1 + dp2)) % mod;
+			long cur = ((i - 1) * (dp1 + dp2)) % MOD;
 			dp1 = dp2;
 			dp2 = cur;
 		}
 		return (int)dp2;
-	}
-
-	// https://leetcode.com/problems/find-the-derangement-of-an-array/discuss/104981/If-you-don't-understand
-	public int findDerangementDP(int n) {
-		if(n <= 1){
-			return 0;
-		}
-		long[] dp = new long[n + 1];
-		long mod = 1_000_000_007;
-		dp[2] = 1;
-		for(int i = 3; i <= n; i++){
-			dp[i] = ((i - 1) * (dp[i - 1] + dp[i - 2])) % mod;
-		}
-		return (int)dp[n];
 	}
 }
