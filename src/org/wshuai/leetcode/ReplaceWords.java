934f02b833fcdf2840540fc1b449ded123af63c9
relentlesscoder <relentless.code@gmail.com>
2020-03-27 12:27:11 -0400

    r1-650

--- src/org/wshuai/leetcode/ReplaceWords.java
+++ src/org/wshuai/leetcode/ReplaceWords.java
@@ -3,21 +3,48 @@ package org.wshuai.leetcode;
 import java.util.List;
 
 /**
- * Created by Wei on 9/8/2019.
- * #648 https://leetcode.com/problems/replace-words/
+ * Created by Wei on 09/08/2019.
+ * #0648 https://leetcode.com/problems/replace-words/
  */
 public class ReplaceWords {
+	// time O(n*d), space O(26*d)
 	public String replaceWords(List<String> dict, String sentence) {
-		ImplementTrie trie = new ImplementTrie();
-		for (String word : dict) {
-			trie.insert(word);
+		TrieNode trie = new TrieNode();
+		for(String root : dict){
+			insert(root, trie);
 		}
-		String[] arr = sentence.split(" ");
-		StringBuilder sb = new StringBuilder();
-		for (String word : arr) {
-			sb.append(trie.searchRoot(word) + " ");
+		String[] words = sentence.split("\\s");
+		StringBuilder res = new StringBuilder();
+		for(String word : words){
+			res.append(searchRoot(word, trie)).append(" ");
 		}
-		String res = sb.toString();
 		return res.substring(0, res.length() - 1);
+	}
+
+	private void insert(String word, TrieNode root){
+		TrieNode cur = root;
+		for(int i = 0; i < word.length(); i++){
+			char key = word.charAt(i);
+			if(!cur.containsKey(key)){
+				cur.put(key, new TrieNode());
+			}
+			cur = cur.get(key);
+		}
+		cur.setEnd();
+	}
+
+	private String searchRoot(String word, TrieNode root){
+		TrieNode cur = root;
+		for(int i = 0; i < word.length(); i++){
+			char key = word.charAt(i);
+			if(!cur.containsKey(key)){
+				return word;
+			}
+			cur = cur.get(key);
+			if(cur.isEnd()){
+				return word.substring(0, i + 1);
+			}
+		}
+		return word;
 	}
 }
