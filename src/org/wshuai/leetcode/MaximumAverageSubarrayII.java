24e3af799f4b41cba4821694dd4b7b9b2b8bd99b
relentlesscoder <relentless.code@gmail.com>
2020-03-27 17:12:24 -0400

    r1-650

--- src/org/wshuai/leetcode/MaximumAverageSubarrayII.java
+++ src/org/wshuai/leetcode/MaximumAverageSubarrayII.java
@@ -2,14 +2,13 @@ package org.wshuai.leetcode;
 
 /**
  * Created by Wei on 12/12/2019.
- * #644 https://leetcode.com/problems/maximum-average-subarray-ii/
+ * #0644 https://leetcode.com/problems/maximum-average-subarray-ii/
  */
 public class MaximumAverageSubarrayII {
-	// explanation https://leetcode.com/problems/maximum-average-subarray-ii/discuss/105477/C%2B%2B-Clean-binary-search-solution-with-explanation
+	// time O(n*log(n))
+	// https://leetcode.com/problems/maximum-average-subarray-ii/discuss/105477/C%2B%2B-Clean-binary-search-solution-with-explanation
 	public double findMaxAverage(int[] nums, int k) {
-		double left  = Double.MAX_VALUE;
-		double right = Double.MIN_VALUE;
-		double mid;
+		double left  = Double.MAX_VALUE, right = Double.MIN_VALUE, mid;
 		for(int num : nums){
 			right = Math.max(right, (double)num);
 			left = Math.min(left, (double)num);
@@ -27,14 +26,16 @@ public class MaximumAverageSubarrayII {
 
 	// use prefix sum to find sum (with length k or larger) greater than 0
 	private boolean isTooBig(int[] nums, double mid, int k){
-		double sum = 0, prev_sum = 0, min_sum = 0;
+		double sum = 0, prefixSum = 0, minPrefixSum = 0;
 		for(int i = 0; i < nums.length; i++){
 			sum += nums[i] - mid;
 			if(i >= k){
-				prev_sum += nums[i - k] - mid;
-				min_sum = Math.min(prev_sum, min_sum);
+				prefixSum += nums[i - k] - mid;
+				// find the smallest prefix sum
+				minPrefixSum = Math.min(prefixSum, minPrefixSum);
 			}
-			if(i >= k - 1 && sum > min_sum){
+			// if there exists some subarray that have larger average value, return false
+			if(i >= k - 1 && sum > minPrefixSum){
 				return false;
 			}
 		}
