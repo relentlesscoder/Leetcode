a16d55cfa09b4952ace4c7e412d24af7ed1e640d
relentlesscoder <relentless.code@gmail.com>
2020-03-31 17:16:12 -0400

    r1-670

--- src/org/wshuai/leetcode/EqualTreePartition.java
+++ src/org/wshuai/leetcode/EqualTreePartition.java
@@ -6,33 +6,29 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by Wei on 11/6/19.
- * #663 https://leetcode.com/problems/equal-tree-partition/
+ * Created by Wei on 11/06/2019.
+ * #0663 https://leetcode.com/problems/equal-tree-partition/
  */
 public class EqualTreePartition {
-	Map<Integer, List<TreeNode>> map;
-
+	// time O(n), space O(n)
 	public boolean checkEqualTree(TreeNode root) {
-		map = new HashMap<>();
-		int sum = dfs(root);
-		if(sum % 2 == 0 && map.containsKey(sum / 2)
-				&& (map.get(sum / 2).size() > 1 || map.get(sum / 2).get(0) != root)){
-			return true;
+		Map<Integer, List<TreeNode>> map = new HashMap<>();
+		int sum = dfs(root, map);
+		if(sum % 2 != 0){
+			return false;
 		}
-		return false;
+		sum >>= 1;
+		return map.containsKey(sum)
+				&& (map.get(sum).size() > 1 || map.get(sum).get(0) != root);
 	}
 
-	private int dfs(TreeNode node){
-		if(node == null){
+	private int dfs(TreeNode root, Map<Integer, List<TreeNode>> map){
+		if(root == null){
 			return 0;
 		}
-		int left = dfs(node.left);
-		int right = dfs(node.right);
-		int sum = node.val + left + right;
-		if(!map.containsKey(sum)){
-			map.put(sum, new ArrayList<>());
-		}
-		map.get(sum).add(node);
+		int sum = root.val + dfs(root.left, map) + dfs(root.right, map);
+		map.putIfAbsent(sum, new ArrayList<>());
+		map.get(sum).add(root);
 		return sum;
 	}
 }
