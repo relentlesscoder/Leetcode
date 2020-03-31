305abf42660e1e5a052d388c3ab5e1eb7b1bfbf8
relentlesscoder <relentless.code@gmail.com>
2020-03-31 12:22:11 -0400

    r1-670

--- src/org/wshuai/leetcode/TrimABinarySearchTree.java
+++ src/org/wshuai/leetcode/TrimABinarySearchTree.java
@@ -1,22 +1,25 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 8/9/19.
- * #669 https://leetcode.com/problems/trim-a-binary-search-tree/
+ * Created by Wei on 08/09/2019.
+ * #0669 https://leetcode.com/problems/trim-a-binary-search-tree/
  */
 public class TrimABinarySearchTree {
+	// time O(n)
 	public TreeNode trimBST(TreeNode root, int L, int R) {
 		if (root == null) {
 			return null;
 		}
-		int val = root.val;
-		if (val < L) {
-			return trimBST(root.right, L, R);
-		} else if (val > R) {
-			return trimBST(root.left, L, R);
+		TreeNode left = trimBST(root.left, L, R);
+		TreeNode right = trimBST(root.right, L, R);
+		if (root.val < L) {
+			return right;
 		}
-		root.left = trimBST(root.left, L, R);
-		root.right = trimBST(root.right, L, R);
+		if (root.val > R) {
+			return left;
+		}
+		root.left = left;
+		root.right = right;
 		return root;
 	}
 }
