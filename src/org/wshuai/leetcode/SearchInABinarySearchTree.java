915291166edb57e5ac385c70e570568de36ed3aa
relentlesscoder <relentless.code@gmail.com>
2020-04-07 17:28:34 -0400

    R1-700

--- src/org/wshuai/leetcode/SearchInABinarySearchTree.java
+++ src/org/wshuai/leetcode/SearchInABinarySearchTree.java
@@ -1,20 +1,22 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 8/8/19.
- * #700 https://leetcode.com/problems/search-in-a-binary-search-tree/
+ * Created by Wei on 08/08/2019.
+ * #0700 https://leetcode.com/problems/search-in-a-binary-search-tree/
  */
 public class SearchInABinarySearchTree {
+	// time O(log(n))
 	public TreeNode searchBST(TreeNode root, int val) {
-		if (root == null) {
+		if(root == null){
 			return null;
 		}
-		if (root.val == val) {
+		if(root.val == val){
 			return root;
-		} else if (root.val < val) {
-			return searchBST(root.right, val);
-		} else {
+		}
+		if(root.val > val){
 			return searchBST(root.left, val);
+		}else{
+			return searchBST(root.right, val);
 		}
 	}
 }
