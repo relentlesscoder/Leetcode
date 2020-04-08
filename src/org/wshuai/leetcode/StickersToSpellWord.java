618ae7f0150dcc28eb24f2d792c2bd57eff1b91c
relentlesscoder <relentless.code@gmail.com>
2020-04-08 13:17:35 -0400

    R1-700

--- src/org/wshuai/leetcode/StickersToSpellWord.java
+++ src/org/wshuai/leetcode/StickersToSpellWord.java
@@ -5,58 +5,75 @@ import java.util.LinkedList;
 import java.util.Set;
 
 /**
- * Created by Wei on 12/2/19.
- * #691 https://leetcode.com/problems/stickers-to-spell-word/
+ * Created by Wei on 12/02/2019.
+ * #0691 https://leetcode.com/problems/stickers-to-spell-word/
  */
 public class StickersToSpellWord {
+	// time O((n*26)^d), space O(n^d)
+	// d is the length of target
 	public int minStickers(String[] stickers, String target) {
-		int N = stickers.length;
-		int T = target.length();
-		int[][] letters = new int[N][26];
+		int count = 0, n = stickers.length;
 		int[] targetLetters = new int[26];
-		for(int i = 0; i < N; i++){
-			for(char c : stickers[i].toCharArray()){
-				letters[i][c - 'a']++;
-			}
-		}
-		for(char c: target.toCharArray()){
+		int[][] stickerLetters = new int[n][26];
+		for (char c : target.toCharArray()) {
 			targetLetters[c - 'a']++;
 		}
-
-		int level = 0;
+		for (int i = 0; i < n; i++) {
+			for (char c : stickers[i].toCharArray()) {
+				stickerLetters[i][c - 'a']++;
+			}
+		}
+		String key = toKey(targetLetters);
 		LinkedList<int[]> queue = new LinkedList<>();
 		Set<String> visited = new HashSet<>();
 		queue.offerLast(targetLetters);
-		while(!queue.isEmpty()){
+		while (!queue.isEmpty()) {
 			int size = queue.size();
-			while(size-- > 0){
+			while (size-- > 0) {
 				int[] cur = queue.pollFirst();
-				for(int i = 0; i < N; i++){
-					int[] next = cur.clone();
-					for(int j = 0; j < 26; j++){
-						int left = next[j] - letters[i][j];
-						next[j] = left > 0 ? left : 0;
+				key = toKey(cur);
+				if (!visited.add(key)) {
+					continue;
+				}
+				for (int i = 0; i < n; i++) {
+					if (stickerLetters[i][key.charAt(0) - 'a'] == 0) {
+						continue;
 					}
-					String key = toKey(next);
-					if(key.length() == 0){
-						return level + 1;
+					int[] temp = cur.clone();
+					for (int j = 0; j < 26; j++) {
+						if (temp[j] > 0) {
+							temp[j] = Math.max(0, temp[j] - stickerLetters[i][j]);
+						}
 					}
-					if(visited.add(key)){
-						queue.offer(next);
+					if (targetSpelled(temp)) {
+						return count + 1;
 					}
+					queue.offerLast(temp);
 				}
 			}
-			level++;
+			count++;
 		}
-
 		return -1;
 	}
 
-	private String toKey(int[] arr){
+	private boolean targetSpelled(int[] arr) {
+		for (int i = 0; i < 26; i++) {
+			if (arr[i] > 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private String toKey(int[] arr) {
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < 26; i++){
-			for(int j = 0; j < arr[i]; j++){
-				sb.append((char)('a' + i));
+		for (int i = 0; i < 26; i++) {
+			if (arr[i] == 0) {
+				continue;
+			}
+			char c = (char) (i + 'a');
+			for (int j = 0; j < arr[i]; j++) {
+				sb.append(c);
 			}
 		}
 		return sb.toString();
