e3e236a8445e9c9d9e709c6982836048b5e64099
relentlesscoder <relentless.code@gmail.com>
2020-04-08 10:18:27 -0400

    R1-700

--- src/org/wshuai/leetcode/MaxAreaOfIsland.java
+++ src/org/wshuai/leetcode/MaxAreaOfIsland.java
@@ -1,43 +1,38 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 9/4/19.
- * #695 https://leetcode.com/problems/max-area-of-island/
+ * Created by Wei on 09/04/2019.
+ * #0695 https://leetcode.com/problems/max-area-of-island/
  */
 public class MaxAreaOfIsland {
+
+	private static final int[] dirs = new int[]{0, 1, 0, -1, 0};
+
+	// time O(m*n), space O(1)
 	public int maxAreaOfIsland(int[][] grid) {
-		if (grid.length == 0 || grid[0].length == 0) {
+		if(grid == null || grid.length == 0 || grid[0].length == 0){
 			return 0;
 		}
-		int[][] conn = new int[4][2];
-		conn[0] = new int[]{1, 0};
-		conn[1] = new int[]{-1, 0};
-		conn[2] = new int[]{0, 1};
-		conn[3] = new int[]{0, -1};
-		int max = 0;
-		int row = grid.length;
-		int col = grid[0].length;
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				if (grid[i][j] == 1) {
-					grid[i][j] = 0;
-					int area = bfs(i, j, grid, conn);
-					max = area > max ? area : max;
+		int res = 0, m = grid.length, n = grid[0].length;
+		for(int i = 0; i < m; i++){
+			for(int j = 0; j < n; j++){
+				if(grid[i][j] == 1){
+					res = Math.max(res, dfs(i, j, m, n, grid));
 				}
 			}
 		}
-		return max;
+		return res;
 	}
 
-	private int bfs(int i, int j, int[][] grid, int[][] conn) {
+	private int dfs(int i, int j, int m, int n, int[][] grid){
+		grid[i][j] = 0;
 		int res = 1;
-		for (int k = 0; k < 4; k++) {
-			int x = i + conn[k][0];
-			int y = j + conn[k][1];
-			if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == 1) {
-				grid[x][y] = 0;
-				res += bfs(x, y, grid, conn);
+		for(int k = 0; k < 4; k++){
+			int x = i + dirs[k], y = j + dirs[k + 1];
+			if(x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 0){
+				continue;
 			}
+			res += dfs(x, y, m, n, grid);
 		}
 		return res;
 	}
