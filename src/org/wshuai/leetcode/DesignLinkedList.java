a75a54edb42a53b0e93fa821810f459efbcd478b
relentlesscoder <relentless.code@gmail.com>
2020-04-09 11:43:07 -0400

    R1-710

--- src/org/wshuai/leetcode/DesignLinkedList.java
+++ src/org/wshuai/leetcode/DesignLinkedList.java
@@ -1,97 +1,103 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 8/20/19.
- * #707 https://leetcode.com/problems/design-linked-list/
+ * Created by Wei on 08/20/2019.
+ * #0707 https://leetcode.com/problems/design-linked-list/
  */
 public class DesignLinkedList {
-	private LinkedListNode root = new LinkedListNode(-1);
+	private ListNode head = null;
 	private int size = 0;
 
-	/**
-	 * Initialize your data structure here.
-	 */
+	/** Initialize your data structure here. */
 	public DesignLinkedList() {
-
 	}
 
-	/**
-	 * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
-	 */
+	/** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
 	public int get(int index) {
-		if (index >= size) {
+		if(index < 0 || index >= size){
 			return -1;
 		}
-		LinkedListNode curr = root;
-		int i = 0;
-		while (i <= index) {
-			curr = curr.next;
-			i++;
+		ListNode cur = head;
+		while(index-- > 0){
+			cur = cur.next;
 		}
-		return curr.val;
+		return cur.val;
 	}
 
-	/**
-	 * Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
-	 */
+	/** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
 	public void addAtHead(int val) {
-		LinkedListNode head = new LinkedListNode(val);
-		head.next = root.next;
-		root.next = head;
+		ListNode cur = new ListNode(val);
+		if(size > 0){
+			cur.next = head;
+		}
+		head = cur;
 		size++;
 	}
 
-	/**
-	 * Append a node of value val to the last element of the linked list.
-	 */
+	/** Append a node of value val to the last element of the linked list. */
 	public void addAtTail(int val) {
-		LinkedListNode curr = root;
-		while (curr.next != null) {
-			curr = curr.next;
+		ListNode cur = new ListNode(val);
+		if(size == 0){
+			head = cur;
+			size++;
+			return;
 		}
-		curr.next = new LinkedListNode(val);
+		ListNode tail = head;
+		while(tail.next != null){
+			tail = tail.next;
+		}
+		tail.next = cur;
 		size++;
 	}
 
-	/**
-	 * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
-	 */
+	/** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
 	public void addAtIndex(int index, int val) {
-		if (index > size) {
+		if(index > size){
 			return;
 		}
-		if (index < 0) {
-			index = 0;
+		if(index == size){
+			addAtTail(val);
+		}else if(index == 0){
+			addAtHead(val);
+		}else{
+			int i = 0;
+			ListNode cur = head;
+			while(i++ < index - 1){
+				cur = cur.next;
+			}
+			ListNode next = cur.next;
+			cur.next = new ListNode(val);
+			cur.next.next = next;
+			size++;
 		}
-		LinkedListNode curr = root;
-		int i = 0;
-		while (i < index) {
-			curr = curr.next;
-			i++;
-		}
-		LinkedListNode insert = new LinkedListNode(val);
-		insert.next = curr.next;
-		curr.next = insert;
-		size++;
 	}
 
-	/**
-	 * Delete the index-th node in the linked list, if the index is valid.
-	 */
+	/** Delete the index-th node in the linked list, if the index is valid. */
 	public void deleteAtIndex(int index) {
-		if (index < 0 || index >= size) {
+		if(index < 0 || index >= size){
 			return;
 		}
-		LinkedListNode prev = null;
-		LinkedListNode curr = root;
-		int i = 0;
-		while (i <= index) {
-			prev = curr;
-			curr = curr.next;
-			i++;
+		if(index == 0){
+			head = head.next;
+		}else{
+			int i = 0;
+			ListNode cur = head;
+			while(i++ < index - 1){
+				cur = cur.next;
+			}
+			cur.next = cur.next.next;
 		}
-		prev.next = curr.next;
 		size--;
+	}
+
+	private class ListNode{
+		int val;
+
+		ListNode next;
+
+		public ListNode(int val){
+			this.val = val;
+		}
 	}
 }
 
--- src/org/wshuai/leetcode/ToLowerCase.java
+++ src/org/wshuai/leetcode/ToLowerCase.java
@@ -1,23 +1,22 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 8/7/19.
- * #709 https://leetcode.com/problems/to-lower-case/
+ * Created by Wei on 08/07/2019.
+ * #0709 https://leetcode.com/problems/to-lower-case/
  */
 public class ToLowerCase {
+	// time O(n)
 	public String toLowerCase(String str) {
-		if (str == null) {
+		if(str == null || str.isEmpty()){
 			return "";
 		}
+		char[] arr = str.toCharArray();
 		int diff = 'a' - 'A';
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < str.length(); i++) {
-			char c = str.charAt(i);
-			if (c >= 'A' && c <= 'Z') {
-				c = (char) (c + diff);
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] >= 'A' && arr[i] <= 'Z'){
+				arr[i] = (char)(arr[i] + diff);
 			}
-			sb.append(c);
 		}
-		return sb.toString();
+		return new String(arr);
 	}
 }
