d4db14e7d5803ec03b73c5aac62b8058b946b34d
Wei Shuai <relentless.code@gmail.com>
2020-04-10 23:28:21 -0400

    r1-720

--- src/org/wshuai/leetcode/LongestWordInDictionary.java
+++ src/org/wshuai/leetcode/LongestWordInDictionary.java
@@ -4,26 +4,34 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Created by Wei on 04/10/2020.
+ * #0720 https://leetcode.com/problems/longest-word-in-dictionary/
+ */
 public class LongestWordInDictionary {
 
+	// time O(n*d), space O(n)
 	public String longestWord(String[] words) {
+		Arrays.sort(words, (a, b) -> a.length() == b.length() ?
+			a.compareTo(b) : b.length() - a.length());
 		Set<String> set = new HashSet<>();
-		int max = 0;
-		String res = "";
-		Arrays.sort(words, (x, y) -> x.length() == y.length() ? x.compareTo(y) : x.length() - y.length());
-		for (String word : words) {
-			if (word.length() == 1 || set.contains(word.substring(0, word.length() - 1))) {
-				set.add(word);
-				if (word.length() > max) {
-					max = word.length();
-					res = word;
-				}
+		for(String w : words){
+			set.add(w);
+		}
+		for(int i = 0; i < words.length; i++){
+			String cur = words[i];
+			int j = 1;
+			while(j <= cur.length() && set.contains(cur.substring(0, j))){
+				j++;
+			}
+			if(j == cur.length() + 1){
+				return cur;
 			}
 		}
-
-		return res;
+		return "";
 	}
 
+	// time O(n*d), space O(n*d)
 	public String longestWordTrie(String[] words) {
 		int max = 0;
 		String res = "";
@@ -34,7 +42,7 @@ public class LongestWordInDictionary {
 			TrieNode prev = root;
 			boolean buildable = true;
 			for (int i = 0; i < chars.length - 1; i++) {
-				if (!prev.isEnd() || prev.containsKey(chars[i])) {
+				if (!prev.isEnd() || !prev.containsKey(chars[i])) {
 					buildable = false;
 					break;
 				}
