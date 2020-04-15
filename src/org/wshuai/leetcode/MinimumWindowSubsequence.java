0b1dffe18e80b3809ff6fd626b020c2d7ebfc299
Wei Shuai <relentless.code@gmail.com>
2020-04-14 21:40:11 -0400

    R1-730

--- src/org/wshuai/leetcode/MinimumWindowSubsequence.java
+++ src/org/wshuai/leetcode/MinimumWindowSubsequence.java
@@ -1,65 +1,70 @@
 package org.wshuai.leetcode;
 
+import java.util.Arrays;
+
 /**
- * Created by Wei on 12/2/19.
- * #727 https://leetcode.com/problems/minimum-window-subsequence/
+ * Created by Wei on 12/02/2019.
+ * #0727 https://leetcode.com/problems/minimum-window-subsequence/
  */
 public class MinimumWindowSubsequence {
+	// time O(m*n) worst case
 	public String minWindow(String S, String T) {
 		char[] s = S.toCharArray(), t = T.toCharArray();
 		String res = "";
-		int i = 0, tindex = 0, slen = s.length, tlen = t.length, len = Integer.MAX_VALUE;
-		while(i < slen) {
-			if(s[i] == t[tindex]) {
-				tindex++;
-				if(tindex == tlen) { // all chars in T has been matched
-					int end = i+1; //i is the last match index in S
-					tindex--; // now tindex is the last index in T
-					while(tindex >= 0) {  //both i and tindex move back
-						if(s[i] == t[tindex]){
-							tindex--;
+		int min = Integer.MAX_VALUE, m = s.length, n = t.length;
+		for(int i = 0, j = 0; i < s.length; i++){
+			if(s[i] == t[j]){
+				j++;
+				if(j == n){ // all chars in T has been matched
+					int end = i + 1; //i is the last match index in S
+					j--; // now j is the last index in T
+					while(j >= 0){ // both i and j moves back
+						if(s[i] == t[j]){
+							j--;
 						}
 						i--;
 					}
-					i++;  //we found the first match index in S
-					tindex++;  //now tindex == 0, the first match index in T
-					if(end - i < len) { //optimization ops
-						len = end - i;
+					i++; //we found the first match index in S
+					j++; //now j == 0, the first index in T
+					if(end - i < min){
+						min = end - i;
 						res = S.substring(i, end); // [i, end) is the matching subsequence
 					}
 				}
 			}
-			i++;
 		}
-		return len == Integer.MAX_VALUE ? "" : res;
+		return res;
 	}
 
-	// https://leetcode.com/problems/minimum-window-subsequence/discuss/109362/Java-Super-Easy-DP-Solution-(O(mn))
+	// time O(m*n), space O(m*n)
+	// https://leetcode.com/problems/minimum-window-subsequence/discuss/140193/Logical-Thinking-with-Clean-Java-Code
 	public String minWindowDP(String S, String T) {
-		int m = T.length(), n = S.length();
-		int[][] dp = new int[m + 1][n + 1];
-		for (int j = 0; j <= n; j++) {
-			dp[0][j] = j + 1;
+		String res = "";
+		int m = S.length(), n = T.length(), min = Integer.MAX_VALUE;
+		int[][] dp = new int[m][n];
+		for(int[] temp : dp){
+			Arrays.fill(temp, -1);
 		}
-		for (int i = 1; i <= m; i++) {
-			for (int j = 1; j <= n; j++) {
-				if (T.charAt(i - 1) == S.charAt(j - 1)) {
-					dp[i][j] = dp[i - 1][j - 1];
-				} else {
-					dp[i][j] = dp[i][j - 1];
-				}
+		for(int i = 0; i < m; i++){
+			if(S.charAt(i) == T.charAt(0)){
+				dp[i][0] = i;
 			}
 		}
-
-		int start = 0, len = n + 1;
-		for (int j = 1; j <= n; j++) {
-			if (dp[m][j] != 0) {
-				if (j - dp[m][j] + 1 < len) {
-					start = dp[m][j] - 1;
-					len = j - dp[m][j] + 1;
+		for(int j = 1; j < n; j++){
+			int temp = -1;
+			for(int i = 0; i < m; i++){
+				if(S.charAt(i) == T.charAt(j)){
+					dp[i][j] = temp;
 				}
+				temp = Math.max(temp, dp[i][j - 1]);
 			}
 		}
-		return len == n + 1 ? "" : S.substring(start, start + len);
+		for(int i = 0; i < m; i++){
+			if(dp[i][n - 1] != -1 && i - dp[i][n - 1] + 1 < min){
+				min = i - dp[i][n - 1] + 1;
+				res = S.substring(dp[i][n - 1], i + 1);
+			}
+		}
+		return res;
 	}
 }
