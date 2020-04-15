50fdf90b0abaed1696f736390e2256a76df5dd86
relentlesscoder <relentless.code@gmail.com>
2020-04-15 14:41:40 -0400

    R1-740

--- src/org/wshuai/leetcode/DeleteAndEarn.java
+++ src/org/wshuai/leetcode/DeleteAndEarn.java
@@ -2,18 +2,20 @@ package org.wshuai.leetcode;
 
 /**
  * Created by Wei on 10/10/2019.
- * #740 https://leetcode.com/problems/delete-and-earn/
+ * #0740 https://leetcode.com/problems/delete-and-earn/
  */
 public class DeleteAndEarn {
+	// time O(n)
 	public int deleteAndEarn(int[] nums) {
-		int[] count = new int[10_001];
+		int[] sum = new int[10_001];
 		for(int n: nums){
-			count[n] += n;
+			sum[n] += n;
 		}
 		int[] dp = new int[10_001];
-		dp[1] = count[1];
+		dp[1] = sum[1];
 		for(int i = 2; i <= 10_000; i++){
-			dp[i] = Math.max(count[i] + dp[i - 2], dp[i - 1]);
+			// the max of use num i or not use
+			dp[i] = Math.max(sum[i] + dp[i - 2], dp[i - 1]);
 		}
 		return dp[10_000];
 	}
