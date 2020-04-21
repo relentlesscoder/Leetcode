dca0c9c856f46866e28f75a797f703ef6068cc28
relentlesscoder <relentless.code@gmail.com>
2020-04-21 15:36:06 -0400

    R1-770

--- src/org/wshuai/leetcode/MaxChunksToMakeSorted.java
+++ src/org/wshuai/leetcode/MaxChunksToMakeSorted.java
@@ -2,23 +2,19 @@ package org.wshuai.leetcode;
 
 /**
  * Created by Wei on 10/14/2019.
- * #769 https://leetcode.com/problems/max-chunks-to-make-sorted/
+ * #0769 https://leetcode.com/problems/max-chunks-to-make-sorted/
  */
 public class MaxChunksToMakeSorted {
+	// time O(n)
+	// similar to #763
 	public int maxChunksToSorted(int[] arr) {
-		int res = 0;
-		int[] count = new int[10];
+		int res = 0, min = 0, max = 0;
 		for(int i = 0; i < arr.length; i++){
-			count[arr[i]]++;
-			boolean split = true;
-			for(int j = 0; j <= i; j++){
-				if(count[j] == 0){
-					split = false;
-					break;
-				}
-			}
-			if(split){
+			max = Math.max(arr[i], max);
+			// if current chunk contains all numbers in [min, max], we can split it
+			if(max == i){
 				res++;
+				min = max = i + 1;
 			}
 		}
 		return res;
