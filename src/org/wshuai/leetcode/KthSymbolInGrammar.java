89b0c1bdd3529d7fd636f85790d2b72f7444dd24
Wei Shuai <relentless.code@gmail.com>
2020-04-23 14:41:23 -0400

    R1-780

--- src/org/wshuai/leetcode/KthSymbolInGrammar.java
+++ src/org/wshuai/leetcode/KthSymbolInGrammar.java
@@ -2,20 +2,22 @@ package org.wshuai.leetcode;
 
 /**
  * Created by Wei on 11/11/2019.
- * #779 https://leetcode.com/problems/k-th-symbol-in-grammar/
+ * #0779 https://leetcode.com/problems/k-th-symbol-in-grammar/
  */
 public class KthSymbolInGrammar {
-	// binary tree
-	//  0    1
-	// 0 1  1 0
+
+	// time O(N)
 	public int kthGrammar(int N, int K) {
-		if (N == 1){
+		// draw the binary tree
+		//  0    1
+		// 0 1  1 0
+		if(N == 1){
 			return 0;
 		}
-		if (K % 2 == 0){
-			return (kthGrammar(N - 1, K / 2) == 0) ? 1 : 0;
+		if(K % 2 == 0){
+			return 1 - kthGrammar(N - 1, K >> 1);
 		}else{
-			return (kthGrammar(N - 1, (K + 1) / 2) == 0) ? 0 : 1;
+			return kthGrammar(N - 1, (K + 1) >> 1);
 		}
 	}
 }
--- src/org/wshuai/leetcode/SplitBST.java
+++ src/org/wshuai/leetcode/SplitBST.java
@@ -2,24 +2,32 @@ package org.wshuai.leetcode;
 
 /**
  * Created by Wei on 10/15/2019.
- * #776 https://leetcode.com/problems/split-bst/
+ * #0776 https://leetcode.com/problems/split-bst/
  */
 public class SplitBST {
 
-	// see https://leetcode.com/problems/split-bst/solution/
+	// time O(n)
+	// https://leetcode.com/problems/split-bst/solution/
 	public TreeNode[] splitBST(TreeNode root, int V) {
 		if(root == null){
+			// result[0] saves nodes lte to V
 			return new TreeNode[]{null, null};
 		}else if(root.val <= V){
-			TreeNode[] bns = splitBST(root.right, V);
-			root.right = bns[0];
-			bns[0] = root;
-			return bns;
+			TreeNode[] bst = splitBST(root.right, V);
+			// if the current node is lte V, then it should put into
+			// result[0], since the result[0] returned is from the right
+			// subtree, set the right child of current node to result[0]
+			root.right = bst[0];
+			bst[0] = root;
+			return bst;
 		}else{
-			TreeNode[] bns = splitBST(root.left, V);
-			root.left = bns[1];
-			bns[1] = root;
-			return bns;
+			// if the current node is gt V, then it should put into
+			// result[1], since the result[1] returned is from the left
+			// subtree, set the left child of current node to result[1]
+			TreeNode[] bst = splitBST(root.left, V);
+			root.left = bst[1];
+			bst[1] = root;
+			return bst;
 		}
 	}
 }
