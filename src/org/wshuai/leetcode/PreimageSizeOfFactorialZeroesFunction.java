68568faf4b323a0fb01caa8c907d15096dbd2dd7
relentlesscoder <relentless.code@gmail.com>
2020-04-29 19:32:48 -0400

    R1-800

--- src/org/wshuai/leetcode/PreimageSizeOfFactorialZeroesFunction.java
+++ src/org/wshuai/leetcode/PreimageSizeOfFactorialZeroesFunction.java
@@ -2,34 +2,34 @@ package org.wshuai.leetcode;
 
 /**
  * Created by Wei on 12/31/2019.
- * #793 https://leetcode.com/problems/preimage-size-of-factorial-zeroes-function/
+ * #0793 https://leetcode.com/problems/preimage-size-of-factorial-zeroes-function/
  */
 public class PreimageSizeOfFactorialZeroesFunction {
-	// https://leetcode.com/problems/preimage-size-of-factorial-zeroes-function/discuss/117821/Four-binary-search-solutions-based-on-different-ideas
-	public int preimageSizeFZF(int K) {
-		for (long l = 0, r =  5L * (K + 1); l <= r;) {
-			long m = l + (r - l) / 2;
-			long k = numOfTrailingZeros(m);
+    // time O(log(K))
+    // https://leetcode.com/problems/preimage-size-of-factorial-zeroes-function/discuss/117821/Four-binary-search-solutions-based-on-different-ideas
+    public int preimageSizeFZF(int K) {
+        return (int) (binarySearch(K) - binarySearch(K - 1));
+    }
 
-			if (k < K) {
-				l = m + 1;
-			} else if (k > K) {
-				r = m - 1;
-			} else {
-				return 5;
-			}
-		}
+    private long binarySearch(int K) {
+        long l = 0, r = 5L * (K + 1);
+        while (l <= r) {
+            long m = l + (r - l) / 2;
+            long k = numOfTrailingZeros(m);
+            if (k <= K) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return r;
+    }
 
-		return 0;
-	}
-
-	private long numOfTrailingZeros(long x) {
-		long res = 0;
-
-		for (; x > 0; x /= 5) {
-			res += x/5;
-		}
-
-		return res;
-	}
+    private long numOfTrailingZeros(long x) {
+        long res = 0;
+        for (; x > 0; x /= 5) {
+            res += x / 5;
+        }
+        return res;
+    }
 }
