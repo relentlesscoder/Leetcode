b0d57e2166d8f8756f07643ab888b447f45b4cb7
relentlesscoder <relentless.code@gmail.com>
2020-05-01 11:13:50 -0400

    R1-810

--- src/org/wshuai/leetcode/ExpressiveWords.java
+++ src/org/wshuai/leetcode/ExpressiveWords.java
@@ -2,34 +2,31 @@ package org.wshuai.leetcode;
 
 /**
  * Created by Wei on 10/16/2019.
- * #809 https://leetcode.com/problems/expressive-words/
+ * #0809 https://leetcode.com/problems/expressive-words/
  */
 public class ExpressiveWords {
+
+	// time O(m*d)
 	public int expressiveWords(String S, String[] words) {
 		int res = 0;
-		for(String word: words){
-			if(check(S, word)){
+		for(String cur : words){
+			if(canForm(S, cur)){
 				res++;
 			}
 		}
 		return res;
 	}
 
-	private boolean check(String s, String t){
-		if(t.length() > s.length()){
-			return false;
-		}
-		int i = 0;
-		int j = 0;
-		while(i < s.length() && j < t.length()){
-			char next = s.charAt(i);
-			int c1 = 0;
-			int c2 = 0;
-			while(i < s.length() && s.charAt(i) == next){
+	private boolean canForm(String S, String T){
+		int i = 0, j = 0, m = S.length(), n = T.length();
+		while(i < m && j < n){
+			char cur = S.charAt(i);
+			int c1 = 0, c2 = 0;
+			while(i < m && S.charAt(i) == cur){
 				c1++;
 				i++;
 			}
-			while(j < t.length() && t.charAt(j) == next){
+			while(j < n && T.charAt(j) == cur){
 				c2++;
 				j++;
 			}
@@ -37,6 +34,6 @@ public class ExpressiveWords {
 				return false;
 			}
 		}
-		return i == s.length() && j == t.length();
+		return i == m && j == n;
 	}
 }
