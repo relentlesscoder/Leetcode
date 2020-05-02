0ef0da5673f56678fe4a4c86228f5f3cb5a2c1f1
relentlesscoder <relentless.code@gmail.com>
2020-05-02 07:50:40 -0400

    1310

---
+++ src/org/wshuai/leetcode/LeftmostColumnWithAtLeastAOne.java
@@ -0,0 +1,38 @@
+package org.wshuai.leetcode;
+
+import java.util.List;
+
+/**
+ * Created by Wei on 05/02/2020.
+ * #1428 https://leetcode.com/problems/leftmost-column-with-at-least-a-one/
+ */
+public class LeftmostColumnWithAtLeastAOne {
+    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+        List<Integer> dim = binaryMatrix.dimensions();
+        int res = Integer.MAX_VALUE, m = dim.get(0), n = dim.get(1);
+        for (int i = 0; i < m; i++) {
+            int low = 0, high = n - 1;
+            while (low < high) {
+                int mid = low + (high - low) / 2, val = binaryMatrix.get(i, mid);
+                if (val == 0) {
+                    low = mid + 1;
+                } else {
+                    high = mid;
+                }
+            }
+            if (binaryMatrix.get(i, low) == 1) {
+                res = Math.min(res, low);
+            }
+        }
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+
+    // This is the BinaryMatrix's API interface.
+    // You should not implement it, or speculate about its implementation
+    private interface BinaryMatrix {
+
+        public int get(int row, int col);
+
+        public List<Integer> dimensions();
+    }
+}
