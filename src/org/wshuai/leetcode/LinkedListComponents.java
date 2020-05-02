19ac27aebc08622010d41bcb0bb4cd9a60db54f5
relentlesscoder <relentless.code@gmail.com>
2020-05-02 19:34:32 -0400

    R1-820

--- src/org/wshuai/leetcode/LinkedListComponents.java
+++ src/org/wshuai/leetcode/LinkedListComponents.java
@@ -4,26 +4,29 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Created by Wei on 9/22/19.
- * #817 https://leetcode.com/problems/linked-list-components/
+ * Created by Wei on 09/22/2019.
+ * #0817 https://leetcode.com/problems/linked-list-components/
  */
 public class LinkedListComponents {
+	// time O(n), space O(n)
 	public int numComponents(LinkedListNode head, int[] G) {
-		int count = 0;
-		Set<Integer> set = new HashSet<>();
-		for (int g : G) {
-			set.add(g);
+		int res = 0;
+		Set<Integer> nodes = new HashSet<>();
+		for(int g : G){
+			nodes.add(g);
 		}
-		LinkedListNode node = head;
-		while (node != null) {
-			if (set.contains(node.val)) {
-				count++;
-				while (node.next != null && set.contains(node.next.val)) {
-					node = node.next;
-				}
+		LinkedListNode cur = head;
+		boolean flag = false;
+		while(cur != null){
+			if(nodes.contains(cur.val) && !flag){
+				flag = true;
+				res++;
 			}
-			node = node.next;
+			if(!nodes.contains(cur.val) && flag){
+				flag = false;
+			}
+			cur = cur.next;
 		}
-		return count;
+		return res;
 	}
 }
