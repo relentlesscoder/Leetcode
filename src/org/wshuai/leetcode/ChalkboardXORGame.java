c689c2c26f7407185e5464b2081f7412ca1efb3a
relentlesscoder <relentless.code@gmail.com>
2020-05-01 21:12:03 -0400

    R1-810

--- src/org/wshuai/leetcode/ChalkboardXORGame.java
+++ src/org/wshuai/leetcode/ChalkboardXORGame.java
@@ -2,15 +2,23 @@ package org.wshuai.leetcode;
 
 /**
  * Created by Wei on 12/24/2019.
- * #810 https://leetcode.com/problems/chalkboard-xor-game/
+ * #0810 https://leetcode.com/problems/chalkboard-xor-game/
  */
 public class ChalkboardXORGame {
-	// https://leetcode.com/problems/chalkboard-xor-game/discuss/165396/Detailed-math-explanation-Easy-to-understand
-	public boolean xorGame(int[] nums) {
-		int xor = 0;
-		for(int n : nums){
-			xor ^= n;
-		}
-		return xor == 0 || nums.length % 2 == 0;
-	}
+    // https://leetcode.com/problems/chalkboard-xor-game/discuss/165396/Detailed-math-explanation-Easy-to-understand
+    public boolean xorGame(int[] nums) {
+		/*
+		Short explanation:
+		If on Alice's first turn, there are even amount of numbers left, then on any Alice's turn, there will be even amount of numbers left, nothing can change that.
+				The only way to make your opponent lose on the immediate next turn, is to let the them face ODD amount of the SAME number. Because
+		2.1 If the numbers are not all the same, then the opponent can always choose between two numbers the one that won't lose immediately.
+		2.2 If the number are all the same but the amount is even, then the opponent already wins.
+		Combining 1 and 2, we know that whatever Bob does, Alice can always do something on the next turn and not lose. => Alice wins.
+		*/
+        int xor = 0;
+        for (int n : nums) {
+            xor ^= n;
+        }
+        return xor == 0 || nums.length % 2 == 0;
+    }
 }
