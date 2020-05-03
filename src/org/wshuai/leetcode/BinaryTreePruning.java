674bc98f73ad58256f1cacad0786059b10b9944b
relentlesscoder <relentless.code@gmail.com>
2020-05-02 20:13:57 -0400

    R1-820

--- src/org/wshuai/leetcode/BinaryTreePruning.java
+++ src/org/wshuai/leetcode/BinaryTreePruning.java
@@ -1,19 +1,31 @@
 package org.wshuai.leetcode;
 
 /**
- * Created by Wei on 8/31/2019.
- * #814 https://leetcode.com/problems/binary-tree-pruning/
+ * Created by Wei on 08/31/2019.
+ * #0814 https://leetcode.com/problems/binary-tree-pruning/
  */
 public class BinaryTreePruning {
+
+	// time O(n)
 	public TreeNode pruneTree(TreeNode root) {
-		if (root == null) {
-			return root;
+		if(root == null){
+			return null;
 		}
-		root.left = pruneTree(root.left);
-		root.right = pruneTree(root.right);
-		if (root.val == 0 && root.left == null && root.right == null) {
-			root = null;
+		return dfs(root) ? root : null;
+	}
+
+	private boolean dfs(TreeNode root){
+		if(root == null){
+			return false;
 		}
-		return root;
+		boolean left = dfs(root.left);
+		boolean right = dfs(root.right);
+		if(!left){
+			root.left = null;
+		}
+		if(!right){
+			root.right = null;
+		}
+		return left || right || root.val == 1;
 	}
 }
