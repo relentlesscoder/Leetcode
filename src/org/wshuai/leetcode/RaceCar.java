bf2933968a2fc20de06d0f5535d0aa59d4a914ff
relentlesscoder <relentless.code@gmail.com>
2020-05-04 22:39:48 -0400

    R1-820

--- src/org/wshuai/leetcode/RaceCar.java
+++ src/org/wshuai/leetcode/RaceCar.java
@@ -5,34 +5,34 @@ import java.util.LinkedList;
 import java.util.Set;
 
 /**
- * Created by Wei on 12/2/19.
- * #818 https://leetcode.com/problems/race-car/
+ * Created by Wei on 12/02/2019.
+ * #0818 https://leetcode.com/problems/race-car/
  */
 public class RaceCar {
 
-	public int racecarBFS(int target) {
+	public int racecar(int target) {
+		int seqence = 0;
 		LinkedList<int[]> queue = new LinkedList<>();
-		Set<String> visited = new HashSet<>();
 		queue.offerLast(new int[]{0, 1});
-		visited.add(0 + "," + 1);
-		int res = 0;
+		Set<String> visited = new HashSet<>();
+		visited.add("0,1");
 		while(!queue.isEmpty()){
 			int size = queue.size();
 			while(size-- > 0){
 				int[] cur = queue.pollFirst();
-				int[] n1 = new int[]{cur[0] + cur[1], cur[1] * 2};
-				int[] n2 = new int[]{cur[0], cur[1] > 0 ? -1 : 1};
-				if(n1[0] == target || n2[0] == target){
-					return res + 1;
+				int[] a = new int[]{cur[0] + cur[1], cur[1] << 1};
+				int[] r = new int[]{cur[0], cur[1] > 0 ? -1 : 1};
+				if(a[0] == target){
+					return seqence + 1;
 				}
-				if(visited.add(n1[0] + "," + n1[1]) && 0 < n1[0] && n1[0] < (target << 1)){
-					queue.offerLast(n1);
+				if(visited.add(a[0] + "," + a[1]) && a[0] > 0 && a[0] < (target << 1)){
+					queue.offerLast(a);
 				}
-				if(visited.add(n2[0] + "," + n2[1]) && 0 < n2[0] && n2[0] < (target << 1)){
-					queue.offerLast(n2);
+				if(visited.add(r[0] + "," + r[1]) && r[0] > 0 && r[0] < (target << 1)){
+					queue.offerLast(r);
 				}
 			}
-			res++;
+			seqence++;
 		}
 		return -1;
 	}
