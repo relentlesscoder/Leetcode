1746aa166d67e3db262325b2466b5bb02cf3d840
relentlesscoder <relentless.code@gmail.com>
2020-05-07 21:14:39 -0400

    R1-850

--- src/org/wshuai/leetcode/LongestMountainInArray.java
+++ src/org/wshuai/leetcode/LongestMountainInArray.java
@@ -1,37 +1,37 @@
 package org.wshuai.leetcode;
 
+import java.util.LinkedList;
+
 /**
- * Created by Wei on 9/25/19.
- * #845 https://leetcode.com/problems/longest-mountain-in-array/
+ * Created by Wei on 09/25/2019.
+ * #0845 https://leetcode.com/problems/longest-mountain-in-array/
  */
 public class LongestMountainInArray {
+
+	// time O(n), space O(m)
 	public int longestMountain(int[] A) {
-		int res = 0;
-		int len = A.length;
-		if(len < 3){
+		if(A == null || A.length == 0){
 			return 0;
 		}
-		int i = 0;
-		int j = 0;
-		int k = 0;
-		while(j < len){
-			// find the next (possible) starting point of a mountain
-			if(j < len - 1 && A[j + 1] > A[j]){
-				i = j;
-				while(j < len - 1 && A[j + 1] > A[j]){
-					j++;
-				}
-				// record the (possible) mountain peak
-				k = j;
-				while(j < len - 1 && A[j + 1] < A[j]){
-					j++;
-				}
-				// check if the (possible) mountain is valid
-				if(i != k && k != j){
-					res = Math.max(res, j - i + 1);
-				}
-			}else{
+		int res = 0, n = A.length;
+		LinkedList<Integer> queue = new LinkedList<>();
+		for(int i = 1; i < n - 1; i++){
+			if(A[i] > A[i - 1] && A[i] > A[i + 1]){
+				queue.offerLast(i);
+			}
+		}
+		while(!queue.isEmpty()){
+			int i = queue.pollFirst(), j = i, cur = 1, left = 0, right = 0;
+			while(i > 0 && A[i] > A[i - 1]){
+				left++;
+				i--;
+			}
+			while(j < n - 1 && A[j] > A[j + 1]){
+				right++;
 				j++;
+			}
+			if(left > 0 && right > 0){
+				res = Math.max(res, cur + left + right);
 			}
 		}
 		return res;
--- src/org/wshuai/leetcode/SplitArrayIntoFibonacciSequence.java
+++ src/org/wshuai/leetcode/SplitArrayIntoFibonacciSequence.java
@@ -4,63 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Wei on 11/5/2019.
- * #842 https://leetcode.com/problems/split-array-into-fibonacci-sequence/
+ * Created by Wei on 11/05/2019.
+ * #0842 https://leetcode.com/problems/split-array-into-fibonacci-sequence/
  */
 public class SplitArrayIntoFibonacciSequence {
+
 	public List<Integer> splitIntoFibonacci(String S) {
-		for(int i = 1; i < S.length(); i++){
-			List<Long> temp = new ArrayList<>();
-			String val1 = S.substring(0, i);
-			if(val1.startsWith("0") && val1.length() > 1){
-				break;
-			}
-			long first = Long.parseLong(val1);
-			if(first > Integer.MAX_VALUE){
-				break;
-			}
-			temp.add(first);
-			String T = S.substring(i);
-			for(int j = 1; j < T.length(); j++){
-				String val2 = T.substring(0, j);
-				if(val2.startsWith("0") && val2.length() > 1){
-					break;
-				}
-				long second = Long.parseLong(val2);
-				if(second > Integer.MAX_VALUE){
-					break;
-				}
-				temp.add(second);
-				if(dfs(T.substring(j), temp)){
-					List<Integer> res = new ArrayList<>();
-					for(long l : temp){
-						res.add((int)l);
-					}
-					return res;
-				}else{
-					temp = new ArrayList<>();
-					temp.add(first);
-				}
-			}
-		}
-		return new ArrayList<>();
+		List<Integer> res = new ArrayList<>();
+		dfs(0, S.length(), S.toCharArray(), res);
+		return res;
 	}
 
-	private boolean dfs(String S, List<Long> list){
-		if(S.equals("")){
-			return true;
+	private boolean dfs(int start, int n, char[] digits, List<Integer> res){
+		if(start == n){
+			return res.size() >= 3;
 		}
-		long first = list.get(list.size() - 1);
-		long second = list.get(list.size() - 2);
-		long next = first + second;
-		if(next > Integer.MAX_VALUE){
-			return false;
+		long cur = 0;
+		int size = res.size();
+		if(digits[start] == '0'){
+			if(size < 2 || res.get(size - 1) + res.get(size - 2) == 0){
+				res.add(0);
+				if(dfs(start + 1, n, digits, res)){
+					return true;
+				}
+				// only remove when all attempt failed
+				res.remove(res.size() - 1);
+				return false;
+			}else{
+				return false;
+			}
 		}
-		String val = Long.toString(next);
-		if(S.startsWith(val)){
-			list.add(next);
-			if(dfs(S.substring(val.length()), list)){
-				return true;
+		for(int i = start; i < n; i++){
+			cur = cur * 10 + (digits[i] - '0');
+			if(cur > Integer.MAX_VALUE){
+				break;
+			}
+			int next = (int)cur;
+			if(size < 2 || res.get(size - 1) + res.get(size - 2) == next){
+				res.add(next);
+				if(dfs(i + 1, n, digits, res)){
+					return true;
+				}
+				// only remove when all attempt failed
+				res.remove(res.size() - 1);
 			}
 		}
 		return false;
