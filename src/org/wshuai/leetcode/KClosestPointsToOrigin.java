f396e67778b1e3c97c3bbd0c67e950255c61aab5
Wei Shuai <relentless.code@gmail.com>
2020-09-11 07:46:50 -0400

    re-02

--- src/org/wshuai/leetcode/KClosestPointsToOrigin.java
+++ src/org/wshuai/leetcode/KClosestPointsToOrigin.java
@@ -3,63 +3,51 @@ package org.wshuai.leetcode;
 import java.util.*;
 
 /**
- * Created by Wei on 8/4/19.
- * #973 https://leetcode.com/problems/k-closest-points-to-origin/
+ * Created by Wei on 08/04/2019.
+ * #0973 https://leetcode.com/problems/k-closest-points-to-origin/
  */
 public class KClosestPointsToOrigin {
 
+	// time O(n)
 	public int[][] kClosest(int[][] points, int K) {
-		int len = points.length;
-		int[] dists = new int[len];
-		for (int i = 0; i < len; i++) {
-			dists[i] = points[i][0] * points[i][0] + points[i][1] * points[i][1];
-		}
-
-		Arrays.sort(dists);
-		int kDist = dists[K - 1];
-		int[][] res = new int[K][2];
-		int j = 0;
-		for (int i = 0; i < len; i++) {
-			int dist = points[i][0] * points[i][0] + points[i][1] * points[i][1];
-			if (dist <= kDist) {
-				res[j][0] = points[i][0];
-				res[j][1] = points[i][1];
-				j++;
+		int n = points.length, left = 0, right = n - 1;
+		while(left <= right){
+			int mid = findPivot(points, left, right);
+			if(mid == K){
+				break;
+			}
+			if(mid < K){
+				left = mid + 1;
+			}else{
+				right = mid - 1;
 			}
 		}
-		return res;
+		return Arrays.copyOfRange(points, 0, K);
 	}
 
-	public int[][] kClosestPriorityQueue(int[][] points, int K) {
-		PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
-		Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
-		for (int i = 0; i < points.length; i++) {
-			int sum = points[i][0] * points[i][0];
-			sum += points[i][1] * points[i][1];
-			queue.offer(sum);
-			if (map.containsKey(sum)) {
-				List<Integer> lst = map.get(sum);
-				lst.add(i);
-			} else {
-				List<Integer> lst = new ArrayList<Integer>();
-				lst.add(i);
-				map.put(sum, lst);
+	private int findPivot(int[][] points, int left, int right){
+		int pivot = distance(points[right]);
+		int p = left;
+		for(int i = left; i < right; i++){
+			if(distance(points[i]) < pivot){
+				int[] temp = points[p];
+				points[p++] = points[i];
+				points[i] = temp;
 			}
 		}
-		int[][] result = new int[K][2];
-		int j = 0;
-		while (K > 0) {
-			int sum = queue.poll();
-			List<Integer> lst = map.get(sum);
-			int len = lst.size();
-			for (int i = 0; i < len; i++) {
-				int pos = lst.get(i);
-				result[j][0] = points[pos][0];
-				result[j][1] = points[pos][1];
-				j++;
-				K--;
-			}
-		}
-		return result;
+		int[] swap = points[p];
+		points[p] = points[right];
+		points[right] = swap;
+		return p;
+	}
+
+	private int distance(int[] p){
+		return p[0]*p[0] + p[1]*p[1];
+	}
+
+	// time O(n*log(n))
+	public int[][] kClosestSorting(int[][] points, int K) {
+		Arrays.sort(points, (a, b) -> (a[0]*a[0] + a[1]*a[1] - b[0]*b[0] - b[1]*b[1]));
+		return Arrays.copyOfRange(points, 0, K);
 	}
 }
