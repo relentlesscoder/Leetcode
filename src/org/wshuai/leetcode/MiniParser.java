cce72ff9aeba2053742a5f7cb097307a1f73decc
Wei Shuai <relentless.code@gmail.com>
2020-09-18 07:49:15 -0400

    re-13

--- src/org/wshuai/leetcode/MiniParser.java
+++ src/org/wshuai/leetcode/MiniParser.java
@@ -1,5 +1,6 @@
 package org.wshuai.leetcode;
 
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -36,5 +37,42 @@ public class MiniParser {
 			j++;
 		}
 		return res;
+	}
+
+	// This is the interface that allows for creating nested lists.
+	// You should not implement it, or speculate about its implementation
+	private class NestedInteger {
+		// Constructor initializes an empty nested list.
+		public NestedInteger() {
+		}
+
+		// Constructor initializes a single integer.
+		public NestedInteger(int value) {
+		}
+
+		// @return true if this NestedInteger holds a single integer, rather than a nested list.
+		public boolean isInteger() {
+			return false;
+		}
+
+		// @return the single integer that this NestedInteger holds, if it holds a single integer
+		// Return null if this NestedInteger holds a nested list
+		public Integer getInteger() {
+			return 0;
+		}
+
+		// Set this NestedInteger to hold a single integer.
+		public void setInteger(int value) {
+		}
+
+		// Set this NestedInteger to hold a nested list and adds a nested integer to it.
+		public void add(NestedInteger ni) {
+		}
+
+		// @return the nested list that this NestedInteger holds, if it holds a nested list
+		// Return null if this NestedInteger holds a single integer
+		public List<NestedInteger> getList() {
+			return null;
+		}
 	}
 }
--- src/org/wshuai/leetcode/NestedListWeightSum.java
+++ src/org/wshuai/leetcode/NestedListWeightSum.java
@@ -7,16 +7,58 @@ import java.util.List;
  * #0339 https://leetcode.com/problems/nested-list-weight-sum/
  */
 public class NestedListWeightSum {
+
 	// time O(n)
 	public int depthSum(List<NestedInteger> nestedList) {
 		return dfs(nestedList, 1);
 	}
 
-	private int dfs(List<NestedInteger> nestedList, int depth){
+	private int dfs(List<NestedInteger> nestedList, int depth) {
 		int res = 0;
-		for(NestedInteger cur : nestedList){
-			res += cur.isInteger() ? cur.getInteger() * depth : dfs(cur.getList(), depth + 1);
+		for (NestedInteger cur : nestedList) {
+			if (cur.isInteger()) {
+				res += depth * cur.getInteger();
+			} else {
+				res += dfs(cur.getList(), depth + 1);
+			}
 		}
 		return res;
+	}
+
+	// This is the interface that allows for creating nested lists.
+	// You should not implement it, or speculate about its implementation
+	private class NestedInteger {
+		// Constructor initializes an empty nested list.
+		public NestedInteger() {
+		}
+
+		// Constructor initializes a single integer.
+		public NestedInteger(int value) {
+		}
+
+		// @return true if this NestedInteger holds a single integer, rather than a nested list.
+		public boolean isInteger() {
+			return false;
+		}
+
+		// @return the single integer that this NestedInteger holds, if it holds a single integer
+		// Return null if this NestedInteger holds a nested list
+		public Integer getInteger() {
+			return 0;
+		}
+
+		// Set this NestedInteger to hold a single integer.
+		public void setInteger(int value) {
+		}
+
+		// Set this NestedInteger to hold a nested list and adds a nested integer to it.
+		public void add(NestedInteger ni) {
+		}
+
+		// @return the nested list that this NestedInteger holds, if it holds a nested list
+		// Return null if this NestedInteger holds a single integer
+		public List<NestedInteger> getList() {
+			return null;
+		}
 	}
 }
