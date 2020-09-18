3399856b08c8600c73ba8a57a9e19d5187f238ca
relentlesscoder <relentless.code@gmail.com>
2020-09-17 22:48:46 -0400

    re-11

--- src/org/wshuai/leetcode/AccountsMerge.java
+++ src/org/wshuai/leetcode/AccountsMerge.java
@@ -7,47 +7,102 @@ import java.util.*;
  * #0721 https://leetcode.com/problems/accounts-merge/
  */
 public class AccountsMerge {
-	// time O(V + E), space O(V^2)
+
+	// complexity analysis - https://leetcode.com/problems/accounts-merge/solution/
 	public List<List<String>> accountsMerge(List<List<String>> accounts) {
 		List<List<String>> res = new ArrayList<>();
 		int n = accounts.size();
 		boolean[][] adj = new boolean[n][n];
 		Map<String, Integer> map = new HashMap<>();
-		boolean[] visited = new boolean[n];
 		for(int i = 0; i < n; i++){
-			List<String> account = accounts.get(i);
-			for(int j = 1; j < account.size(); j++){
-				String email = account.get(j);
+			List<String> vals = accounts.get(i);
+			for(int j = 1; j < vals.size(); j++){
+				String email = vals.get(j);
 				if(map.containsKey(email)){
-					adj[i][map.get(email)] = adj[map.get(email)][i] = true;
+					adj[map.get(email)][i] = adj[i][map.get(email)] = true;
 				}
 				map.put(email, i);
 			}
 		}
+		boolean[] visited = new boolean[n];
 		for(int i = 0; i < n; i++){
-			if(!visited[i]){
-				TreeSet<String> emails = new TreeSet<>();
-				dfs(i, n, adj, visited, emails, accounts);
-				List<String> cur = new ArrayList<>();
-				cur.add(accounts.get(i).get(0));
-				cur.addAll(emails);
-				res.add(cur);
+			if(visited[i]){
+				continue;
 			}
+			TreeSet<String> emails = new TreeSet<>();
+			dfs(i, n, accounts, adj, emails, visited);
+			List<String> cur = new ArrayList<>();
+			cur.add(accounts.get(i).get(0));
+			cur.addAll(emails);
+			res.add(cur);
 		}
 		return res;
 	}
 
-	private void dfs(int i, int n, boolean[][] adj, boolean[] visited,
-	                 TreeSet<String> emails, List<List<String>> accounts){
+	private void dfs(int i, int n, List<List<String>> accounts,
+					 boolean[][] adj, TreeSet<String> emails, boolean[] visited){
 		visited[i] = true;
 		List<String> account = accounts.get(i);
-		for(int j = 1; j < account.size(); j++){
-			emails.add(account.get(j));
+		for(int k = 1; k < account.size(); k++){
+			emails.add(account.get(k));
 		}
 		for(int j = 0; j < n; j++){
-			if(!visited[j] && adj[i][j]){
-				dfs(j, n, adj, visited, emails, accounts);
+			if(adj[i][j] && !visited[j]){
+				dfs(j, n, accounts, adj, emails, visited);
 			}
 		}
 	}
+
+	public List<List<String>> accountsMergeUnionFind(List<List<String>> accounts) {
+		List<List<String>> res = new ArrayList<>();
+		Map<String, String> root = new HashMap<>();
+		Map<String, String> users = new HashMap<>();
+		Map<String, TreeSet<String>> union = new HashMap<>();
+		int n = accounts.size();
+		for(List<String> account : accounts){
+			String username = account.get(0);
+			for(int i = 1; i < account.size(); i++){
+				root.put(account.get(i), account.get(i));
+				users.put(account.get(i), username);
+			}
+		}
+		for(List<String> account : accounts){
+			String p = find(account.get(1), root);
+			for(int i = 2; i < account.size(); i++){
+				root.put(find(account.get(i), root), p);
+			}
+		}
+		for(List<String> account : accounts){
+			String p = find(account.get(1), root);
+			union.putIfAbsent(p, new TreeSet<>());
+			for(int i = 1; i < account.size(); i++){
+				union.get(p).add(account.get(i));
+			}
+		}
+		for(String p : union.keySet()){
+			List<String> emails = new ArrayList<>();
+			emails.add(users.get(p));
+			emails.addAll(union.get(p));
+			res.add(emails);
+		}
+		return res;
+	}
+
+	private String find(String r, Map<String, String> root){
+		if(!r.equals(root.get(r))){
+			root.put(r, find(root.get(r), root));
+		}
+		return root.get(r);
+	}
+
+    /*
+    private String find(String r, Map<String, String> root){
+        while(!r.equals(root.get(r))) {
+            //path compression
+            root.put(r, root.get(root.get(r)));
+            r = root.get(r);
+        }
+
+        return r;
+    }*/
 }
--- src/org/wshuai/leetcode/MaximumSwap.java
+++ src/org/wshuai/leetcode/MaximumSwap.java
@@ -5,22 +5,21 @@ package org.wshuai.leetcode;
  * #0670 https://leetcode.com/problems/maximum-swap/
  */
 public class MaximumSwap {
+
 	// time O(n)
 	public int maximumSwap(int num) {
+		int[] map = new int[10];
 		char[] arr = Integer.toString(num).toCharArray();
-		int n = arr.length;
-		int[] index = new int[10];
-		for(int i = 0; i < n; i++){
-			// try to use the leftmost value to swap to get the maximum value
-			// for 189269: 989261 > 981269
-			index[arr[i] - '0'] = i;
+		for(int i = 0; i < arr.length; i++){
+			map[arr[i] - '0'] = i;
 		}
-		for(int i = 0; i < n; i++){
-			for(char j = '9'; j > arr[i]; j--){
-				if(index[j - '0'] > i){
-					arr[index[j - '0']] = arr[i];
-					arr[i] = j;
-					return Integer.parseInt(new String(arr));
+		for(int i = 0; i < arr.length; i++){
+			int cur = arr[i] - '0';
+			for(int j = 9; j > cur; j--){
+				if(map[j] > i){
+					arr[map[j]] = arr[i];
+					arr[i] = (char)('0' + j);
+					return Integer.parseInt(String.valueOf(arr));
 				}
 			}
 		}
