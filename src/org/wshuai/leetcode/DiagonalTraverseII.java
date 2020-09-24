fe0414989454a50cc0fb61a715e70a87356191ab
Wei Shuai <relentless.code@gmail.com>
2020-09-23 23:06:21 -0400

    re-17

--- src/org/wshuai/leetcode/DiagonalTraverseII.java
+++ src/org/wshuai/leetcode/DiagonalTraverseII.java
@@ -8,32 +8,28 @@ import java.util.*;
  */
 public class DiagonalTraverseII {
 
-	// time O(m*n), space O(m*n)
+	// time O(d), space O(d)
 	public int[] findDiagonalOrderHashMap(List<List<Integer>> nums) {
+		int count = 0, k = 0;
 		Map<Integer, LinkedList<Integer>> map = new HashMap<>();
-		int m = nums.size(), count = 0;
-		for(int i = 0; i < m; i++){
+		for(int i = 0; i < nums.size(); i++){
 			List<Integer> cur = nums.get(i);
 			for(int j = 0; j < cur.size(); j++){
-				if(j >= nums.get(i).size()){
-					continue;
-				}
 				map.putIfAbsent(i + j, new LinkedList<>());
 				map.get(i + j).offerFirst(cur.get(j));
 				count++;
 			}
 		}
 		int[] res = new int[count];
-		int k = 0;
 		for(int i = 0; i < map.size(); i++){
-			for(int j : map.get(i)){
-				res[k++] = j;
+			for(int num : map.get(i)){
+				res[k++] = num;
 			}
 		}
 		return res;
 	}
 
-	// time O(m*n*log(m*n)), space O(m*n)
+	// time O(d*log(d)), space O(d)
 	public int[] findDiagonalOrderPriorityQueue(List<List<Integer>> nums) {
 		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) ->
 			a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
@@ -51,33 +47,30 @@ public class DiagonalTraverseII {
 		return res;
 	}
 
-	// time O(m*n), space O(1)
+	// time O(m*n)
 	// TLE
-	public int[] findDiagonalOrderMatrixPositioning(List<List<Integer>> nums) {
-		int m = nums.size(), n = 0, count = 0;
-		for(int i = 0; i < m; i++){
-			n = Math.max(n, nums.get(i).size());
-			count += nums.get(i).size();
+	public int[] findDiagonalOrder(List<List<Integer>> nums) {
+		int count = 0, r = nums.size(), c = 0;
+		for(List<Integer> list : nums){
+			count += list.size();
+			c = Math.max(c, list.size());
 		}
 		int[] res = new int[count];
-		int k = 0;
-		for(int s = 0; s < m; s++){
-			for(int i = s, j = 0; i >= 0; i--, j++){
-				if(j >= nums.get(i).size()){
-					continue;
+		int index = 0;
+		for(int k = 0; k < r; k++){
+			for(int i = k, j = 0; i >= 0 && j < c; i--, j++){
+				if(j < nums.get(i).size()){
+					res[index++] = nums.get(i).get(j);
 				}
-				res[k++] = nums.get(i).get(j);
 			}
 		}
-		for(int s = 1; s < n; s++){
-			for(int i = m - 1, j = s; i >= 0; i--, j++){
-				if(j >= nums.get(i).size()){
-					continue;
+		for(int k = 1; k < c; k++){
+			for(int i = r - 1, j = k; i >= 0 && j < c; i--, j++){
+				if(j < nums.get(i).size()){
+					res[index++] = nums.get(i).get(j);
 				}
-				res[k++] = nums.get(i).get(j);
 			}
 		}
-
 		return res;
 	}
 }
--- src/org/wshuai/leetcode/NextPermutation.java
+++ src/org/wshuai/leetcode/NextPermutation.java
@@ -5,15 +5,16 @@ package org.wshuai.leetcode;
  * #0031 https://leetcode.com/problems/next-permutation/
  */
 public class NextPermutation {
-	// time O(n), https://www.geeksforgeeks.org/lexicographic-permutations-of-string/
+
+	// time O(n)
+	// https://www.geeksforgeeks.org/lexicographic-permutations-of-string/
 	public void nextPermutation(int[] nums) {
 		if(nums.length < 2){
 			return;
 		}
-		int n = nums.length;
+		int n = nums.length, left = n - 2;
 		// Take the previously printed permutation and find the rightmost character in it,
 		// which is smaller than its next character. Let us call this character as ‘left character’.
-		int left = n - 2;
 		while(left >= 0 && nums[left] >= nums[left + 1]){
 			left--;
 		}
@@ -21,9 +22,8 @@ public class NextPermutation {
 			// Now find the ceiling of the ‘left character’. Ceiling is the smallest character on right
 			// of ‘left character’, which is greater than ‘left character’. Let us call the ceil character
 			// as ‘right character’.
-			int val = nums[left];
-			int right = n - 1;
-			while(right > left && nums[right] <= val){
+			int right = n - 1, val = nums[left];
+			while(left < right && nums[right] <= val){
 				right--;
 			}
 			// Swap the two characters.
@@ -31,14 +31,11 @@ public class NextPermutation {
 			nums[right] = val;
 		}
 		// Sort the substring (in non-decreasing order) after the original index of ‘left character’.
-		int start = left + 1;
-		int end = n - 1;
+		int start = left + 1, end = n - 1;
 		while(start < end){
 			int temp = nums[start];
-			nums[start] = nums[end];
-			nums[end] = temp;
-			start++;
-			end--;
+			nums[start++] = nums[end];
+			nums[end--] = temp;
 		}
 	}
 }
