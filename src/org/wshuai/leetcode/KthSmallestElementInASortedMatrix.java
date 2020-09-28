a4701c03987996549b90c9908832f3023fa358a6
Wei Shuai <relentless.code@gmail.com>
2020-09-27 20:24:29 -0400

    re-20

--- src/org/wshuai/leetcode/KthSmallestElementInASortedMatrix.java
+++ src/org/wshuai/leetcode/KthSmallestElementInASortedMatrix.java
@@ -1,33 +1,34 @@
 package org.wshuai.leetcode;
 
+import java.util.PriorityQueue;
+
 /**
  * Created by Wei on 09/25/2016.
  * #0378 https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
  */
 public class KthSmallestElementInASortedMatrix {
-	// time O(log(n)*log(n))
+
+	// time O(log(d)*n*log(n)), d = max - min
 	public int kthSmallest(int[][] matrix, int k) {
-		int n = matrix.length, low = matrix[0][0], high = matrix[n - 1][n - 1];
-		while(low < high){
-			int mid = low + (high - low) / 2;
-			int count = 0;
+		int n = matrix.length, left = matrix[0][0], right = matrix[n - 1][n - 1];
+		while(left < right){
+			int mid = left + (right - left) / 2, count = 0;
 			for(int i = 0; i < n; i++){
-				count += binarySearch(matrix[i], mid);
+				count += countLessOrEqual(matrix[i], mid);
 			}
 			if(count < k){
-				low = mid + 1;
+				left = mid + 1;
 			}else{
-				high = mid;
+				right = mid;
 			}
 		}
-		return low;
+		return left;
 	}
 
-	private int binarySearch(int[] nums, int target){
+	private int countLessOrEqual(int[] nums, int target){
 		int left = 0, right = nums.length;
 		while(left < right){
 			int mid = left + (right - left) / 2;
-			// need to count the target itself in
 			if(nums[mid] <= target){
 				left = mid + 1;
 			}else{
@@ -35,5 +36,23 @@ public class KthSmallestElementInASortedMatrix {
 			}
 		}
 		return left;
+	}
+
+	// time O(k*log(n)), space O(n)
+	public int kthSmallestPriorityQueue(int[][] matrix, int k) {
+		int n = matrix.length;
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+		for(int i = 0; i < n; i++){
+			pq.offer(new int[]{matrix[i][0], i, 0});
+		}
+		int[] cur = new int[0];
+		for(int i = 0; i < k; i++){
+			cur = pq.poll();
+			int row = cur[1], col = cur[2];
+			if(col != n - 1){
+				pq.offer(new int[]{matrix[row][col + 1], row, col + 1});
+			}
+		}
+		return cur[0];
 	}
 }
