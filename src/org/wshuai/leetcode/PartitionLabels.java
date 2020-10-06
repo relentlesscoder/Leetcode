c47d02014d3f0096a97bb2f20026768f674857dd
Wei Shuai <relentless.code@gmail.com>
2020-10-06 16:14:18 -0400

    re-30

--- src/org/wshuai/leetcode/PartitionLabels.java
+++ src/org/wshuai/leetcode/PartitionLabels.java
@@ -8,20 +8,20 @@ import java.util.List;
  * #0763 https://leetcode.com/problems/partition-labels/
  */
 public class PartitionLabels {
+
 	// time O(n)
 	public List<Integer> partitionLabels(String S) {
 		List<Integer> res = new ArrayList<>();
 		int n = S.length();
-		int[] map = new int[26];
+		int[] rightBounds = new int[26];
 		for(int i = 0; i < n; i++){
-			map[S.charAt(i) - 'a'] = i;
+			rightBounds[S.charAt(i) - 'a'] = i;
 		}
-		for(int i = 0, j = 0, k = 0; i < n; i++){
-			// find the last index of the current character to extend the substring as long as possible
-			k = Math.max(k, map[S.charAt(i) - 'a']);
-			if(k == i){
-				res.add(i - j + 1);
-				j = i + 1;
+		for(int i = 0, j = 0, k = 0; j < n; j++){
+			k = Math.max(k, rightBounds[S.charAt(j) - 'a']);
+			if(k == j){
+				res.add(j - i + 1);
+				i = j + 1;
 			}
 		}
 		return res;
--- src/org/wshuai/leetcode/ReorderLogFiles.java
+++ src/org/wshuai/leetcode/ReorderLogFiles.java
@@ -6,59 +6,43 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * Created by Wei on 8/5/19.
- * #937 https://leetcode.com/problems/reorder-log-files/
+ * Created by Wei on 08/05/2019.
+ * #0937 https://leetcode.com/problems/reorder-log-files/
  */
 public class ReorderLogFiles {
+
+	// time O(d*n*log(n)), space O(n)
 	public String[] reorderLogFiles(String[] logs) {
-		if (logs == null || logs.length == 0) {
-			return null;
+		if(logs == null || logs.length == 0){
+			return new String[0];
 		}
-		int len = logs.length;
-		List<String> dLogs = new ArrayList<String>();
-		List<String> lLogs = new ArrayList<String>();
-		for (int i = 0; i < len; i++) {
-			String str = logs[i];
-			char last = str.charAt(str.length() - 1);
-			if (last >= '0' && last <= '9') {
-				dLogs.add(logs[i]);
-			} else {
-				lLogs.add(logs[i]);
+		int n = logs.length;
+		String[] res = new String[n];
+		List<String> letterLogs = new ArrayList<>(),
+			digitLogs = new ArrayList<>();
+		for(String log : logs){
+			if(Character.isDigit(log.charAt(log.length() - 1))){
+				digitLogs.add(log);
+			}else{
+				letterLogs.add(log);
 			}
 		}
-		Collections.sort(lLogs, new LogComparator());
-
-		// using Lambda
-        /*Collections.sort(lLogs, (log1, log2) -> {
-            String[] arr1 = log1.split(" ", 2);
-            String[] arr2 = log2.split(" ", 2);
-            if(arr1[1].compareTo(arr2[1]) == 0){
-                return arr1[0].compareTo(arr2[0]);
-            }else{
-                return arr1[1].compareTo(arr2[1]);
-            }
-        });*/
-
-		for (int i = 0; i < len; i++) {
-			if (i < lLogs.size()) {
-				logs[i] = lLogs.get(i);
-			} else {
-				logs[i] = dLogs.get(i - lLogs.size());
+		Collections.sort(letterLogs, (String a, String b) -> {
+			String[] arr1 = a.split(" ", 2),
+				arr2 = b.split(" ", 2);
+			if(arr1[1].equals(arr2[1])){
+				return arr1[0].compareTo(arr2[0]);
+			}else{
+				return arr1[1].compareTo(arr2[1]);
+			}
+		});
+		for(int i = 0; i < n; i++){
+			if(i < letterLogs.size()){
+				res[i] = letterLogs.get(i);
+			}else{
+				res[i] = digitLogs.get(i - letterLogs.size());
 			}
 		}
-		return logs;
-	}
-}
-
-class LogComparator implements Comparator<String> {
-	@Override
-	public int compare(String log1, String log2) {
-		String[] arr1 = log1.split(" ", 2);
-		String[] arr2 = log2.split(" ", 2);
-		if (arr1[1].compareTo(arr2[1]) == 0) {
-			return arr1[0].compareTo(arr2[0]);
-		} else {
-			return arr1[1].compareTo(arr2[1]);
-		}
+		return res;
 	}
 }
