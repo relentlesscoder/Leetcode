bdf93046d6679ceaba2d0eb8cca734a6e471c3ca
relentlesscoder <relentless.code@gmail.com>
2023-10-03 15:40:58 -0700

    1799

--- src/org/wshuai/leetcode/CountSortedVowelStrings.java
+++ src/org/wshuai/leetcode/CountSortedVowelStrings.java
@@ -8,19 +8,19 @@ import java.util.Arrays;
  */
 public class CountSortedVowelStrings {
 
-    // time O(n), space O(n)
-    public int countVowelStrings(int n) {
-        // dp[i][j] denotes number of strings has length i + 1 which
-        // ends with mapping[j]. mapping is ["a","e","i","o","u"].
-        int[][] dp = new int[n][5];
-        Arrays.fill(dp[0], 1);
-        for(int i = 1; i < n; i++){
-            int sum = 0;
-            for(int j = 0; j < 5; j++){
-                sum += dp[i - 1][j];
-                dp[i][j] = sum;
-            }
-        }
-        return dp[n - 1][0] + dp[n - 1][1] + dp[n - 1][2] + dp[n - 1][3] + dp[n - 1][4];
-    }
+	// time O(n), space O(n)
+	public int countVowelStrings(int n) {
+		// dp[i][j] denotes number of strings has length i + 1 which
+		// ends with mapping[j]. mapping is ["a","e","i","o","u"].
+		int[][] dp = new int[n][5];
+		Arrays.fill(dp[0], 1);
+		for (int i = 1; i < n; i++) {
+			int sum = 0;
+			for (int j = 0; j < 5; j++) {
+				sum += dp[i - 1][j];
+				dp[i][j] = sum;
+			}
+		}
+		return dp[n - 1][0] + dp[n - 1][1] + dp[n - 1][2] + dp[n - 1][3] + dp[n - 1][4];
+	}
 }
