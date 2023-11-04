e63dbcf04843d0b35adf7de320cce6cb7b8c9391
relentlesscoder <relentless.code@gmail.com>
2023-11-03 19:01:50 -0700

    1842

--- src/org/wshuai/leetcode/AmountOfNewAreaPaintedEachDay.java
+++ src/org/wshuai/leetcode/AmountOfNewAreaPaintedEachDay.java
@@ -1,4 +1,55 @@
 package org.wshuai.leetcode;
 
+import java.util.*;
+
+/**
+ * Created by Wei on 11/03/2023.
+ * #2158 https://leetcode.com/problems/amount-of-new-area-painted-each-day/
+ */
 public class AmountOfNewAreaPaintedEachDay {
+
+	// time O(m + n), space O(m + n)
+	public int[] amountPainted(int[][] paint) {
+		int[] res = new int[paint.length], lines = new int[50_001];
+		for (int i = 0; i < paint.length; i++) {
+			int start = paint[i][0], end = paint[i][1];
+			while (start < end) {
+				int jump = Math.max(start + 1, lines[start]); // determine the next step
+				res[i] += lines[start] == 0 ? 1 : 0; // count the spot only if it is not painted yet
+				lines[start] = Math.max(lines[start], end); // mark the spot using end time
+				start = jump;
+			}
+		}
+		return res;
+	}
+
+	// time O(n * log(n)), space O(n)
+	public int[] amountPaintedTreeMap(int[][] paint) {
+		int[] res = new int[paint.length];
+		TreeMap<Integer, Integer> painted = new TreeMap<>();
+		int i = 0;
+		for (int[] p : paint) {
+			int start = p[0], end = p[1];
+			Integer startKey = painted.floorKey(p[0]), endKey = painted.floorKey(p[1]); // find the boundary of the overlap
+			if (startKey != null && painted.get(startKey) >= p[0]) {
+				start = startKey;
+			}
+			if (endKey != null && painted.get(endKey) >= p[1]) {
+				end = painted.get(endKey);
+			}
+			int length = end - start;
+			SortedMap<Integer, Integer> subMap = painted.subMap(start, end); // get all area that already painted
+			List<Integer> keysToRemove = new ArrayList<>();
+			for (Map.Entry<Integer, Integer> entry : subMap.entrySet()) {
+				length -= entry.getValue() - entry.getKey(); // do not paint the same area twice
+				keysToRemove.add(entry.getKey());
+			}
+			for (int key : keysToRemove) {
+				painted.remove(key);
+			}
+			painted.put(start, end);
+			res[i++] = length;
+		}
+		return res;
+	}
 }
--- src/org/wshuai/leetcode/MaximumSplitOfPositiveEvenIntegers.java
+++ src/org/wshuai/leetcode/MaximumSplitOfPositiveEvenIntegers.java
@@ -1,4 +1,42 @@
 package org.wshuai.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Wei on 11/03/2023.
+ * #2178 https://leetcode.com/problems/maximum-split-of-positive-even-integers/
+ */
 public class MaximumSplitOfPositiveEvenIntegers {
+
+	// time O(sqrt(n)), space O(1)
+	public List<Long> maximumEvenSplit(long finalSum) {
+		List<Long> res = new ArrayList<>();
+		if (finalSum % 2 != 0) {
+			return res;
+		}
+		/**
+		Take n =14
+		i = 2 , current = 0 , list = [] (current + 2 <= 14 , so push it) , current + i = 2 , list = [2]
+		i = 4 , current = 2 , list = [2] (current + 4 <= 14 , so push it) , current + i = 6 , list = [2,4]
+		i = 6 , current = 6 , list = [2,4] (current + 6 <= 14 , so push it) , current + i = 12 , list = [2,4,6]
+		i = 8 , current = 12 , list = [2,4,6] (current + 8 > 14 , so don't push it , break the loop)
+		Now we have current = 12 , and we want 14 , so simply add difference (which is 14 - 12 = 2 ) in the last element of list
+		so list = [2,4,6 + (14 - 12)] = [2,4,8]
+		 */
+		long curr = 2;
+		while (finalSum > 0) {
+			if (finalSum - curr < 0) {
+				break;
+			}
+			res.add(curr);
+			finalSum -= curr;
+			curr += 2;
+		}
+		if (finalSum > 0) {
+			long tail = res.get(res.size() - 1);
+			res.set(res.size() - 1, tail + finalSum);
+		}
+		return res;
+	}
 }
