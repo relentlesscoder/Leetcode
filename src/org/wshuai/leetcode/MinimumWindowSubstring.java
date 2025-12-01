a8c3ec6eb36a583c08129179455150eab5a0eb4a
Wei Shuai <relentless.code@gmail.com>
2025-12-01 23:24:41 +0800

    2447

--- src/org/wshuai/leetcode/MinimumWindowSubstring.java
+++ src/org/wshuai/leetcode/MinimumWindowSubstring.java
@@ -6,28 +6,32 @@ package org.wshuai.leetcode;
  */
 public class MinimumWindowSubstring {
 
-	// time O(n)
-	// https://leetcode.com/problems/minimum-window-substring/discuss/26808/Here-is-a-10-line-template-that-can-solve-most-
-	public String minWindow(String s, String t) {
-		int[] target = new int[128];
-		for(char c : t.toCharArray()){
-			target[c]++;
-		}
-		int unmatched = t.length(), min = Integer.MAX_VALUE, start = -1;
-		for(int i = 0, j = 0; j < s.length(); j++){
-			if(target[s.charAt(j)]-- > 0){
-				unmatched--;
-			}
-			while(unmatched == 0){ // try shortening the sliding window from left
-				if(j - i + 1 < min){
-					min = j - i + 1;
-					start = i;
-				}
-				if(target[s.charAt(i++)]++ == 0){ // find first character makes the current sliding window invalid
-					unmatched++;
-				}
-			}
-		}
-		return min == Integer.MAX_VALUE ? "" : s.substring(start, start + min);
-	}
+    // time O(m + n), space O(1)
+    public String minWindow(String s, String t) {
+        int m = s.length(), index = -1, len = m + 1, unmatched = 0;
+        int[] freq = new int[128];
+        boolean[] seen = new boolean[128];
+        for (char c : t.toCharArray()) {
+            if (freq[c]++ == 0) {
+                unmatched++;
+            }
+            seen[c] = true;
+        }
+        for (int i = 0, j = 0; i < m; i++) {
+            if (seen[s.charAt(i)] && --freq[s.charAt(i)] == 0) {
+                unmatched--;
+            }
+            while (unmatched == 0) {
+                if (i - j + 1 < len) {
+                    len = Math.min(len, i - j + 1);
+                    index = j;
+                }
+                if (seen[s.charAt(j)] && freq[s.charAt(j)]++ == 0) {
+                    unmatched++;
+                }
+                j++;
+            }
+        }
+        return index == -1 ? "" : s.substring(index, index + len);
+    }
 }
