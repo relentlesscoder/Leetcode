2852c706c422381d08e8249448a05279e24e36e5
Wei Shuai <relentless.code@gmail.com>
2025-12-06 19:40:07 +0800

    2464

--- src/org/wshuai/leetcode/CheckIfThereIsAValidPathInAGrid.java
+++ src/org/wshuai/leetcode/CheckIfThereIsAValidPathInAGrid.java
@@ -1,70 +1,124 @@
 package org.wshuai.leetcode;
 
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * Created by Wei on 03/23/2020.
  * #1391 https://leetcode.com/problems/check-if-there-is-a-valid-path-in-a-grid/
  */
 public class CheckIfThereIsAValidPathInAGrid {
-	// time O(m*n), space O(m*n)
-	public boolean hasValidPath(int[][] grid) {
-		int m = grid.length, n = grid[0].length;
-		LinkedList<int[]> queue = new LinkedList<>();
-		boolean[][] visited = new boolean[m][n];
-		queue.offerLast(new int[]{0, 0, grid[0][0]});
-		visited[0][0] = true;
-		while(!queue.isEmpty()){
-			int[] cur = queue.pollFirst();
-			if(cur[0] == m - 1 && cur[1] == n - 1){
-				return true;
-			}
-			int i = cur[0], j = cur[1], v = cur[2];
-			// left
-			if(v == 1 || v == 3 || v == 5){
-				int x = i, y = j - 1;
-				if(x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]){
-					int z = grid[x][y];
-					if(z == 1 || z == 4 || z == 6){
-						visited[x][y] = true;
-						queue.offerLast(new int[]{x, y, z});
-					}
-				}
-			}
-			// right
-			if(v == 1 || v == 4 || v == 6){
-				int x = i, y = j + 1;
-				if(x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]){
-					int z = grid[x][y];
-					if(z == 1 || z == 3 || z == 5){
-						visited[x][y] = true;
-						queue.offerLast(new int[]{x, y, z});
-					}
-				}
-			}
-			// upper
-			if(v == 2 || v == 5 || v == 6){
-				int x = i - 1, y = j;
-				if(x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]){
-					int z = grid[x][y];
-					if(z == 2 || z == 3 || z == 4){
-						visited[x][y] = true;
-						queue.offerLast(new int[]{x, y, z});
-					}
-				}
-			}
-			//lower
-			if(v == 2 || v == 3 || v == 4){
-				int x = i + 1, y = j;
-				if(x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]){
-					int z = grid[x][y];
-					if(z == 2 || z == 5 || z == 6){
-						visited[x][y] = true;
-						queue.offerLast(new int[]{x, y, z});
-					}
-				}
-			}
-		}
-		return false;
-	}
+
+    // time O(m * n), space O(m * n)
+    public boolean hasValidPath(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        return dfs(grid, 0, 0, visited);
+    }
+
+    private boolean dfs(int[][] grid, int i, int j, boolean[][] visited) {
+        int m = grid.length, n = grid[0].length;
+        if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j]) {
+            return false;
+        }
+        if (i == m - 1 && j == n - 1) {
+            return true;
+        }
+        visited[i][j] = true;
+        int v = grid[i][j];
+        // Move left
+        if (v == 1 || v == 3 || v == 5) {
+            int x = i, y = j - 1;
+            if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]
+                    && (grid[x][y] == 1 || grid[x][y] == 4 || grid[x][y] == 6)) {
+                if (dfs(grid, x, y, visited)) {
+                    return true;
+                }
+            }
+        }
+        // Move right
+        if (v == 1 || v == 4 || v == 6) {
+            int x = i, y = j + 1;
+            if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]
+                    && (grid[x][y] == 1 || grid[x][y] == 3 || grid[x][y] == 5)) {
+                if (dfs(grid, x, y, visited)) {
+                    return true;
+                }
+            }
+        }
+        // Move up
+        if (v == 2 || v == 5 || v == 6) {
+            int x = i - 1, y = j;
+            if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]
+                    && (grid[x][y] == 2 || grid[x][y] == 3 || grid[x][y] == 4)) {
+                if (dfs(grid, x, y, visited)) {
+                    return true;
+                }
+            }
+        }
+        // Move down
+        if (v == 2 || v == 3 || v == 4) {
+            int x = i + 1, y = j;
+            if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]
+                    && (grid[x][y] == 2 || grid[x][y] == 5 || grid[x][y] == 6)) {
+                if (dfs(grid, x, y, visited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // time O(m * n), space O(min(m, n))
+    public boolean hasValidPathBFS(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        Deque<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{0, 0, grid[0][0]});
+        visited[0][0] = true;
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int i = curr[0], j = curr[1], v = curr[2];
+            if (i == m - 1 && j == n - 1) {
+                return true;
+            }
+            // Move left
+            if (v == 1 || v == 3 || v == 5) {
+                int x = i, y = j - 1;
+                if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]
+                        && (grid[x][y] == 1 || grid[x][y] == 4 || grid[x][y] == 6)) {
+                    visited[x][y] = true;
+                    queue.offer(new int[]{x, y, grid[x][y]});
+                }
+            }
+            // Move right
+            if (v == 1 || v == 4 || v == 6) {
+                int x = i, y = j + 1;
+                if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]
+                        && (grid[x][y] == 1 || grid[x][y] == 3 || grid[x][y] == 5)) {
+                    visited[x][y] = true;
+                    queue.offer(new int[]{x, y, grid[x][y]});
+                }
+            }
+            // Move up
+            if (v == 2 || v == 5 || v == 6) {
+                int x = i - 1, y = j;
+                if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]
+                        && (grid[x][y] == 2 || grid[x][y] == 3 || grid[x][y] == 4)) {
+                    visited[x][y] = true;
+                    queue.offer(new int[]{x, y, grid[x][y]});
+                }
+            }
+            // Move down
+            if (v == 2 || v == 3 || v == 4) {
+                int x = i + 1, y = j;
+                if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]
+                        && (grid[x][y] == 2 || grid[x][y] == 5 || grid[x][y] == 6)) {
+                    visited[x][y] = true;
+                    queue.offer(new int[]{x, y, grid[x][y]});
+                }
+            }
+        }
+        return false;
+    }
 }
--- src/org/wshuai/leetcode/CountSubIslands.java
+++ src/org/wshuai/leetcode/CountSubIslands.java
@@ -1,40 +1,82 @@
 package org.wshuai.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * Created by Wei on 09/20/2023.
  * #1905 https://leetcode.com/problems/count-sub-islands/
  */
 public class CountSubIslands {
 
-	private static final int[] DIRS = new int[] {0, 1, 0, -1, 0};
+    private static final int[] DIRS = new int[]{0, -1, 0, 1, 0};
+    private boolean child = true;
 
-	// time O(m * n), space O(m * n)
-	public int countSubIslands(int[][] grid1, int[][] grid2) {
-		int res = 0, m = grid1.length, n = grid1[0].length;
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (grid2[i][j] == 1) {
-					if (findIsland(grid1, grid2, i, j, m, n)) {
-						res++;
-					}
-				}
-			}
-		}
-		return res;
-	}
+    // time O(m * n), space O(m * n)
+    public int countSubIslandsDFS(int[][] grid1, int[][] grid2) {
+        int res = 0, m = grid1.length, n = grid1[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid2[i][j] == 1) {
+                    child = true;
+                    dfs(grid1, grid2, i, j);
+                    if (child) {
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
 
-	private boolean findIsland(int[][] grid1, int[][] grid2, int i, int j, int m, int n) {
-		boolean res = grid1[i][j] == 1; // just need to check if [i,j] in grid1 is also land
-		grid2[i][j] = 0;
-		for (int k = 0; k < 4; k++) {
-			int x = i + DIRS[k], y = j + DIRS[k + 1];
-			if (x < 0 || x >= m || y < 0 || y >= n || grid2[x][y] != 1) {
-				continue;
-			}
-			if (!findIsland(grid1, grid2, x, y, m, n)) {
-				res = false;
-			}
-		}
-		return res;
-	}
+    private void dfs(int[][] grid1, int[][] grid2, int i, int j) {
+        // 判断边界合法性以及当前格子是否已经遍历过
+        if (i < 0 || i >= grid2.length || j < 0 || j >= grid2[0].length || grid2[i][j] != 1) {
+            return;
+        }
+		// 如果在当前DFS过程中所有grid2中为1的格子在grid1中也为1，那当前格子所在的
+		// grid2中的岛屿一定是grid1中某个岛屿的子岛屿。
+        if (grid1[i][j] != 1) {
+            child = false;
+        }
+        // 标记当前格子为遍历过
+        grid2[i][j] = 0;
+        for (int d = 0; d < 4; d++) {
+            dfs(grid1, grid2, i + DIRS[d], j + DIRS[d + 1]);
+        }
+    }
+
+    // time O(m * n), space O(min(m, n))
+    public int countSubIslandsBFS(int[][] grid1, int[][] grid2) {
+        int res = 0, m = grid1.length, n = grid1[0].length;
+        Deque<int[]> queue = new ArrayDeque<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid2[i][j] == 1) {
+                    boolean child = true;
+                    queue.offer(new int[]{i, j});
+                    grid2[i][j] = 0;
+                    while (!queue.isEmpty()) {
+                        int[] curr = queue.poll();
+                        if (grid1[curr[0]][curr[1]] != 1) {
+                            child = false;
+                        }
+                        for (int d = 0; d < 4; d++) {
+                            int x = curr[0] + DIRS[d], y = curr[1] + DIRS[d + 1];
+                            // 判断边界合法性以及当前格子是否已经遍历过
+                            if (x >= 0 && x < m && y >= 0 && y < n && grid2[x][y] == 1) {
+                                // 标记当前格子为遍历过
+                                grid2[x][y] = 0;
+                                queue.offer(new int[]{x, y});
+                            }
+                        }
+                    }
+                    if (child) {
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
 }
