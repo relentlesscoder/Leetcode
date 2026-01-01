be46898e0e2c372aa234cfa00ddaa5caea46750b
Wei Shuai <relentless.code@gmail.com>
2025-12-31 18:41:11 -0800

    2504

--- src/org/wshuai/leetcode/MaxSumOfRectangleNoLargerThanK.java
+++ src/org/wshuai/leetcode/MaxSumOfRectangleNoLargerThanK.java
@@ -7,44 +7,40 @@ import java.util.TreeSet;
  * #0363 https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/
  */
 public class MaxSumOfRectangleNoLargerThanK {
-	// time O(c^2*r*log(r))
-	public int maxSumSubmatrix(int[][] matrix, int k) {
-		if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
-			return 0;
-		}
-		int res = Integer.MIN_VALUE, r = matrix.length, c = matrix[0].length;
-		int[][] prefix = new int[r][c + 1];
-		// prefix sum of columns
-		for(int i = 0; i < r; i++){
-			for(int j = 1; j <= c; j++){
-				prefix[i][j] = prefix[i][j - 1] + matrix[i][j - 1];
-			}
-		}
-		for(int i = 0; i < c; i++){
-			for(int j = i; j < c; j++){
-				int[] sum = new int[r];
-				for(int x = 0; x < r; x++){
-					sum[x] = prefix[x][j + 1] - prefix[x][i];
-				}
-				res = Math.max(res, findMax(sum, k));
-			}
-		}
-		return res;
-	}
 
-	private int findMax(int[] arr, int k){
-		int res = Integer.MIN_VALUE, sum = 0;
-		TreeSet<Integer> treeset = new TreeSet<>();
-		treeset.add(0);
-		for(int i = 0; i < arr.length; i++){
-			sum += arr[i];
-			// find the closest sum to target
-			Integer min = treeset.ceiling(sum - k);
-			if(min != null){
-				res = Math.max(res, sum - min);
-			}
-			treeset.add(sum);
-		}
-		return res;
-	}
+    // time O(m^2 * n * log(n)), space O(n)
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        // #1074 相似题
+        // 固定起始行 s ，遍历每个结束行 r。维护一个数组 nums 计算将多行压缩到
+        // 1 行的列的和。
+        // 题目转化为求一位数组中和不超过 k 的子数组的最大值。
+        //   s[i] - s[j] <= k
+        //   s[j] >= s[i] - k
+        // 因为我们需要最大值，所以满足上式的 s[j] 越小越好。
+        int res = Integer.MIN_VALUE, m = matrix.length, n = matrix[0].length;
+        for (int s = 0; s < m; s++) { // O(m)
+            int[] nums = new int[n];
+            for (int r = s; r < m; r++) { // O(m)
+                for (int c = 0; c < n; c++) { // O(n)
+                    nums[c] += matrix[r][c];
+                }
+                // 维护一个有序集合存出现过的前缀和
+                TreeSet<Integer> set = new TreeSet<>();
+                set.add(0);
+                for (int i = 0, sum = 0; i < n; i++) { // O(n)
+                    sum += nums[i];
+                    // 利用有序集合找到满足 s[j] >= s[i] - k 的 s[j] 的最小值
+                    Integer ceil = set.ceiling(sum - k); // O(log(n))
+                    if (ceil != null) {
+                        if (sum - ceil == k) { // 优化: k 是可能的最大值
+                            return k;
+                        }
+                        res = Math.max(res, sum - ceil);
+                    }
+                    set.add(sum); // O(log(n))
+                }
+            }
+        }
+        return res;
+    }
 }
