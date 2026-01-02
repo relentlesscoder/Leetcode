c7332848df22117e2dd08496aa5e68d979adf9db
Wei Shuai <relentless.code@gmail.com>
2026-01-01 21:17:09 -0800

    2508

---
+++ src/org/wshuai/leetcode/CountPathsThatCanFormAPalindromeInATree.java
@@ -0,0 +1,47 @@
+package org.wshuai.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Wei on 01/01/2026.
+ * #2791 https://leetcode.com/problems/count-paths-that-can-form-a-palindrome-in-a-tree/
+ */
+public class CountPathsThatCanFormAPalindromeInATree {
+
+    // time O(n), space O(n)
+    public long countPalindromePaths(List<Integer> parent, String s) {
+        // #1542相似题, 对树做 DFS 然后根据前缀奇偶性异或和来统计回文的数量。
+        // DFS 的遍历顺序保证处理到一个点时，另一侧到达该点的路径的前缀和已经
+        // 在哈希表中。
+        // 示例1: parent = [-1,0,0,1,1,2], s = "acaabc"
+        // 哈希表的遍历顺序为 [0,1,3,4,2,5] , 当处理到点 2 时另一侧路径 [3,1,0]
+        //  和 [4,1,0] 前缀和已经在哈希表中。
+        int n = parent.size();
+        List<Integer>[] adj = new ArrayList[n];
+        Arrays.setAll(adj, i -> new ArrayList<>());
+        for (int i = 1; i < n; i++) {
+            adj[parent.get(i)].add(i);
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        return dfs(0, 0, adj, s.toCharArray(), map);
+    }
+
+    private long dfs(int i, int prev, List<Integer>[] adj, char[] s, HashMap<Integer, Integer> map) {
+        long res = 0;
+        for (int x : adj[i]) {
+            int mask = prev ^ (1 << (s[x] - 'a'));
+            res += map.getOrDefault(mask, 0);
+            for (int j = 0; j < 26; j++) {
+                int curr = mask ^ (1 << j);
+                res += map.getOrDefault(curr, 0);
+            }
+            map.merge(mask, 1, Integer::sum);
+            res += dfs(x, mask, adj, s, map);
+        }
+        return res;
+    }
+}
--- src/org/wshuai/leetcode/NumberOfWonderfulSubstrings.java
+++ src/org/wshuai/leetcode/NumberOfWonderfulSubstrings.java
@@ -6,21 +6,20 @@ package org.wshuai.leetcode;
  */
 public class NumberOfWonderfulSubstrings {
 
-	// time O(n), space O(1)
-	public long wonderfulSubstrings(String word) {
-		long[] count = new long[1 << 10];
-		count[0] = 1;
-		int mask = 0;
-		long res = 0;
-		for (char c : word.toCharArray()) {
-			mask ^= 1 << (c - 'a');
-			res += count[mask];
-			for (int i = 0; i < 10; i++) {
-				int nextOdd = mask ^ (1 << i);
-				res += count[nextOdd];
-			}
-			count[mask]++;
-		}
-		return res;
-	}
+    // time O(n), space O(2^10)
+    public long wonderfulSubstrings(String word) {
+		// 同 #1542
+        long res = 0, n = word.length();
+        int[] map = new int[1_024];
+        map[0] = 1;
+        for (int i = 0, mask = 0; i < n; i++) {
+            mask ^= 1 << (word.charAt(i) - 'a');
+            for (int j = 0; j < 10; j++) {
+                res += map[mask ^ (1 << j)];
+            }
+            res += map[mask];
+            map[mask]++;
+        }
+        return res;
+    }
 }
