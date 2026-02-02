8e1c170d4e285a5c97cab9c25266665572dbc975
Wei Shuai <relentless.code@gmail.com>
2026-02-02 09:34:45 -0800

    2536

--- src/org/wshuai/leetcode/FrogPositionAfterTSeconds.java
+++ src/org/wshuai/leetcode/FrogPositionAfterTSeconds.java
@@ -1,6 +1,8 @@
 package org.wshuai.leetcode;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Wei on 03/09/2020.
@@ -8,62 +10,46 @@ import java.util.*;
  */
 public class FrogPositionAfterTSeconds {
 
-	// time O(n), space O(n)
-	public double frogPosition(int n, int[][] edges, int t, int target) {
-		int time = 0;
-		Map<Integer, List<Integer>> adj = new HashMap<>();
-		for (int[] e : edges) {
-			adj.putIfAbsent(e[0], new ArrayList<>());
-			adj.putIfAbsent(e[1], new ArrayList<>());
-			adj.get(e[0]).add(e[1]);
-			adj.get(e[1]).add(e[0]);
-		}
-		boolean[] visited = new boolean[n + 1];
-		LinkedList<NodeState> queue = new LinkedList<>();
-		queue.offerLast(new NodeState(1, 1.0));
-		visited[1] = true;
-		while (!queue.isEmpty() && time <= t) {
-			int size = queue.size();
-			while (size-- > 0) {
-				NodeState cur = queue.pollFirst();
-				if (time == t && cur.id == target) {
-					return cur.prob;
-				}
-				int count = 0;
-				List<Integer> temp = new ArrayList<>();
-				if (adj.containsKey(cur.id)) {
-					List<Integer> child = adj.get(cur.id);
-					for (int next : child) {
-						if (visited[next]) {
-							continue;
-						}
-						count++;
-						visited[next] = true;
-						temp.add(next);
-					}
-				}
-				if (count == 0) {
-					// if cannot move, stay at the same node with 100% probability
-					queue.offerLast(cur);
-				} else {
-					double prob = cur.prob / count;
-					for (int next : temp) {
-						queue.offerLast(new NodeState(next, prob));
-					}
-				}
-			}
-			time++;
-		}
-		return 0;
-	}
+    private double res = 0.0;
 
-	private class NodeState {
-		int id;
-		double prob;
+    // time O(n), space O(n)
+    public double frogPosition(int n, int[][] edges, int t, int target) {
+        res = 0.0;
+        List<Integer>[] adj = new ArrayList[n + 1];
+        Arrays.setAll(adj, i -> new ArrayList<>());
+        adj[1].add(0); // 避免单独判断根结点的小技巧
+        for (int[] e : edges) {
+            adj[e[0]].add(e[1]);
+            adj[e[1]].add(e[0]);
+        }
+		// 因为选择每条路都是同样的几率所以可以计算路径上的乘积 prod 然后将答案设为 1 / prod。
+        dfs(1, 0, 1L, t, adj, target);
+        return res;
+    }
 
-		public NodeState(int id, double prob) {
-			this.id = id;
-			this.prob = prob;
-		}
-	}
+    private boolean dfs(int node, int parent, long prod, int time,
+                        List<Integer>[] adj, int target) {
+		// 找到目标节点的条件:
+		//   1. 当前节点为 target
+		//   2a. 时间为 0
+		//   2b. 时间不为 0 但是已经无路可走
+        if (node == target && (time == 0 || adj[node].size() == 1)) {
+            res = 1.0 / prod; // 计算结果
+            return true;
+        }
+		// 不满足条件的情况:
+		//   1. 找到 target 但时间没用完且有路可走
+		//   2. 没找到 target 但是时间已经用完
+        if (node == target || time == 0) {
+            return false;
+        }
+        for (int next : adj[node]) {
+			// 树的 DFS 只需要保证下一个节点不是父节点即可
+            if (next != parent && dfs(next, node, prod * (adj[node].size() - 1),
+                    time - 1, adj, target)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/org/wshuai/leetcode/MaximizeTheNumberOfTargetNodesAfterConnectingTreesI.java
+++ src/org/wshuai/leetcode/MaximizeTheNumberOfTargetNodesAfterConnectingTreesI.java
@@ -12,57 +12,48 @@ public class MaximizeTheNumberOfTargetNodesAfterConnectingTreesI {
 
     // time O(n^2 + m^2), space O(n + m)
     public int[] maxTargetNodes(int[][] edges1, int[][] edges2, int k) {
+        // 思路: 从 edges2 中选一个节点与 edges1 中的每个节点相连，题目要求最大化目标节点数。
+        // 因为连接需要花费一条边，所以我们总是可以从 edges2 里选择那个距离为 k - 1 的目标节
+        // 点数最大的节点与edges1 中的每一个节点相连。
         int n = edges1.length + 1, m = edges2.length + 1;
-        List<Integer>[] adj1 = new ArrayList[n];
-        Arrays.setAll(adj1, i -> new ArrayList<>());
-        for (int[] edge : edges1) {
-            adj1[edge[0]].add(edge[1]);
-            adj1[edge[1]].add(edge[0]);
-        }
-        List<Integer>[] adj2 = new ArrayList[m];
-        Arrays.setAll(adj2, i -> new ArrayList<>());
-        for (int[] edge : edges2) {
-            adj2[edge[0]].add(edge[1]);
-            adj2[edge[1]].add(edge[0]);
-        }
         int[] res = new int[n];
-        int[] count1 = findAllTargets(adj1, k);
-        int count2 = findMaxTargets(adj2, k - 1);
-        for (int i = 0; i < n; i++) {
-            res[i] = count1[i] + count2;
+        // 构造邻接表
+        List<Integer>[] adj1 = buildTree(edges1), adj2 = buildTree(edges2);
+        // 对 edges2 中的每一个节点用 DFS 找出距离为 k - 1 (节点数 k) 的目标节点数的最大值
+        int max = 0;
+        for (int i = 0; i < m; i++) { // O(m^2)
+            max = Math.max(max, dfs(i, -1, k, adj2));
+        }
+        // // 对 edges1 中的每一个节点用 DFS 找出距离为 k (节点数 k + 1) 的目标节点数并将
+        // 最大值 max 加到 edges1 的每个答案中
+        for (int i = 0; i < n; i++) { // O(n^2)
+            res[i] = dfs(i, -1, k + 1, adj1) + max;
         }
         return res;
     }
 
-    private int[] findAllTargets(List<Integer>[] adj, int k) {
-        int n = adj.length;
-        int[] res = new int[n];
-        for (int i = 0; i < n; i++) {
-            res[i] = dfs(i, -1, adj, k);
-        }
-        return res;
-    }
-
-    private int dfs(int node, int parent, List<Integer>[] adj, int k) {
-        if (k < 0) {
+    private int dfs(int node, int parent, int k, List<Integer>[] adj) {
+        if (k == 0) {
             return 0;
         }
         int res = 1;
         for (int next : adj[node]) {
+            // 树的 DFS 只需要保证下一个节点不是父节点即可
             if (next == parent) {
                 continue;
             }
-            res += dfs(next, node, adj, k - 1);
+            res += dfs(next, node, k - 1, adj);
         }
         return res;
     }
 
-    private int findMaxTargets(List<Integer>[] adj, int k) {
-        int[] counts = findAllTargets(adj, k);
-        int res = 0;
-        for (int count : counts) {
-            res = Math.max(res, count);
+    private List<Integer>[] buildTree(int[][] edges) {
+        List<Integer>[] tree = new ArrayList[edges.length + 1];
+        Arrays.setAll(tree, i -> new ArrayList<>());
+        for (int[] e : edges) {
+            tree[e[0]].add(e[1]);
+            tree[e[1]].add(e[0]);
         }
-        return res;
+        return tree;
     }
 }
