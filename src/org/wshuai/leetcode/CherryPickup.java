bbcc649d85b0578bd0fa3e35805579433d66a503
Wei Shuai <relentless.code@gmail.com>
2026-02-28 08:32:33 -0800

    2559

--- src/org/wshuai/leetcode/CherryPickup.java
+++ src/org/wshuai/leetcode/CherryPickup.java
@@ -7,38 +7,108 @@ import java.util.Arrays;
  * #0741 https://leetcode.com/problems/cherry-pickup/
  */
 public class CherryPickup {
-	// time O(n^3), space O(n^3)
-	// https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-741-cherry-pickup/
-	// https://leetcode.com/problems/cherry-pickup/discuss/165218/Java-O(N3)-DP-solution-w-specific-explanation
+
+	// time O(n^3), space O(n^2)
 	public int cherryPickup(int[][] grid) {
-		int n = grid.length;
-		int[][][] dp = new int[n + 1][n + 1][n + 1];
-		for (int i = 0; i <= n; i++) {
-			for (int j = 0; j <= n; j++) {
-				Arrays.fill(dp[i][j], Integer.MIN_VALUE);
-			}
+		// 空间优化版 DP
+		int n = grid.length, m = 2 * n - 1;
+		int[][] pre = new int[n + 1][n + 1];
+		for (int[] row : pre) {
+			Arrays.fill(row, -1_000);
 		}
-		dp[1][1][1] = grid[0][0];
-		for (int x1 = 1; x1 <= n; x1++) {
-			for (int y1 = 1; y1 <= n; y1++) {
-				for (int x2 = 1; x2 <= n; x2++) {
-					int y2 = x1 + y1 - x2;
-					if (dp[x1][y1][x2] > 0 || y2 < 1 || y2 > n || grid[x1 - 1][y1 - 1] == -1 || grid[x2 - 1][y2 - 1] == -1) {
+		pre[1][1] = grid[0][0];
+		for (int s = 1; s < m; s++) {
+			int[][] dp = new int[n + 1][n + 1];
+			for (int[] row : dp) {
+				Arrays.fill(row, -1_000);
+			}
+			for (int j1 = Math.max(s - n + 1, 0); j1 <= Math.min(s, n - 1); j1++) {
+				if (grid[s - j1][j1] < 0) {
+					continue;
+				}
+				for (int j2 = Math.max(s - n + 1, 0); j2 <= Math.min(s, n - 1); j2++) {
+					if (grid[s - j2][j2] < 0) {
 						continue;
-						// have already detected || out of boundary || cannot access
 					}
-					int cur = Math.max(Math.max(dp[x1 - 1][y1][x2], dp[x1 - 1][y1][x2 - 1]),
-							Math.max(dp[x1][y1 - 1][x2], dp[x1][y1 - 1][x2 - 1]));
-					if (cur < 0) {
-						continue;
-					}
-					dp[x1][y1][x2] = cur + grid[x1 - 1][y1 - 1];
-					if (x1 != x2) {
-						dp[x1][y1][x2] += grid[x2 - 1][y2 - 1];
-					}
+					int max = Math.max(Math.max(pre[j1 + 1][j2 + 1], pre[j1][j2]),
+							Math.max(pre[j1][j2 + 1], pre[j1 + 1][j2]));
+					dp[j1 + 1][j2 + 1] = max + (j1 == j2 ? grid[s - j1][j1] : grid[s - j1][j1] + grid[s - j2][j2]);
 				}
 			}
+			pre = dp;
 		}
-		return dp[n][n][n] < 0 ? 0 : dp[n][n][n];
+		return Math.max(pre[n][n], 0);
 	}
+
+    // time O(n^3), space O(n^3)
+    public int cherryPickupDPWithGrid(int[][] grid) {
+        // 把记忆化搜索翻译成 DP
+        int n = grid.length, m = 2 * n - 1;
+        int[][][] dp = new int[m][n + 1][n + 1];
+        for (int[][] matrix : dp) {
+            for (int[] row : matrix) {
+                Arrays.fill(row, -1_000);
+            }
+        }
+        dp[0][1][1] = grid[0][0];
+        // 需要注意 j1 和 j2 的循环范围。
+        // 由于 i1 + j1 = s 且 0 <= i1 <= n − 1 且 0 <= j1 <= n − 1
+        //   s - j1 >= 0       -> j1 <= s
+        //   s - j1 <= n - 1   -> j1 >= s - n - 1
+        //   j1 >= 0           -> j1 >= 0
+        //   j1 <= n - 1       -> j1 <= n - 1
+        // 联立得 max(s − n + 1, 0) <= j1 <= min(s, n − 1), 对于 j2 也同理。
+        for (int s = 1; s < m; s++) {
+            for (int j1 = Math.max(s - n + 1, 0); j1 <= Math.min(s, n - 1); j1++) {
+                if (grid[s - j1][j1] < 0) {
+                    continue;
+                }
+                for (int j2 = Math.max(s - n + 1, 0); j2 <= Math.min(s, n - 1); j2++) {
+                    if (grid[s - j2][j2] < 0) {
+                        continue;
+                    }
+                    int max = Math.max(Math.max(dp[s - 1][j1 + 1][j2 + 1], dp[s - 1][j1][j2]),
+                            Math.max(dp[s - 1][j1][j2 + 1], dp[s - 1][j1 + 1][j2]));
+                    dp[s][j1 + 1][j2 + 1] = max + (j1 == j2 ? grid[s - j1][j1] : grid[s - j1][j1] + grid[s - j2][j2]);
+                }
+            }
+        }
+        return Math.max(dp[m - 1][n][n], 0);
+    }
+
+    // time O(n^3), space O(n^3)
+    public int cherryPickupDFSWithMemorization(int[][] grid) {
+        // 记忆化搜索 - 将问题转化为两个人从出发点一起出发最后同时到达右下角格子所能摘到
+        // 的樱桃的最大数量。
+        int n = grid.length;
+        // memo[i][j][k] i: 当前步数， j: 第一个人的列坐标， k: 第二个人的列坐标
+        int[][][] memo = new int[2 * n - 1][n][n];
+        for (int i = 0; i < memo.length; i++) {
+            for (int j = 0; j < memo[0].length; j++) {
+                Arrays.fill(memo[i][j], -1);
+            }
+        }
+        return Math.max(dfs(2 * n - 2, n - 1, n - 1, grid, memo), 0);
+    }
+
+    private int dfs(int s, int j1, int j2, int[][] grid, int[][][] memo) {
+        if (s == 0) { // 回到出发点
+            // j1 == 0 && j2 == 0 && s - j1 == 0 && s - j2 == 0
+            return grid[0][0];
+        }
+        // 越界或者遇到荆棘
+        if (j1 < 0 || j2 < 0 || s - j1 < 0 || s - j2 < 0
+                || grid[s - j1][j1] == -1 || grid[s - j2][j2] == -1) {
+            return -1_000;
+        }
+        if (memo[s][j1][j2] != -1) {
+            return memo[s][j1][j2];
+        }
+        int max = Math.max(Math.max(dfs(s - 1, j1, j2, grid, memo), // 两个人都是从上面格子过来
+                        dfs(s - 1, j1 - 1, j2 - 1, grid, memo)), // 两个人都是从左边格子过来
+                Math.max(dfs(s - 1, j1, j2 - 1, grid, memo), // 上和左
+                        dfs(s - 1, j1 - 1, j2, grid, memo))); // 左和上
+        //注意如果恰好走到同一个格子则最多只能摘到一个樱桃
+        return memo[s][j1][j2] = max + (j1 == j2 ? grid[s - j1][j1] : grid[s - j1][j1] + grid[s - j2][j2]);
+    }
 }
--- src/org/wshuai/leetcode/TargetSum.java
+++ src/org/wshuai/leetcode/TargetSum.java
@@ -1,5 +1,6 @@
 package org.wshuai.leetcode;
 
+import java.util.Arrays;
 
 /**
  * Created by Wei on 01/25/2017.
@@ -7,50 +8,90 @@ package org.wshuai.leetcode;
  */
 public class TargetSum {
 
-	// time O(2*m*n), space O(2*m*n)
-	public int findTargetSumWaysDP(int[] nums, int S) {
-		int sum = 0, n = nums.length;
-		for(int num : nums){
-			sum += num;
-		}
-		if(S < -sum || S > sum){
-			return 0;
-		}
-		int m = sum << 1;
-		int[][] dp = new int[n + 1][m + 1];
-		dp[0][sum] = 1;
-		for(int i = 1; i <= n; i++){
-			for(int j = 0; j <= m; j++){
-				if(j >= nums[i - 1]){
-					dp[i][j] += dp[i - 1][j - nums[i - 1]];
-				}
-				if(j + nums[i - 1] <= m){
-					dp[i][j] += dp[i - 1][j + nums[i - 1]];
-				}
-			}
-		}
-		return dp[n][S + sum];
-	}
+    // time O(n * t), space O(t)
+    public int findTargetSumWays(int[] nums, int target) {
+        // 空间优化版 DP
+        int n = nums.length, sum = 0;
+        for (int x : nums) {
+            sum += x;
+        }
+        int t = target + sum;
+        if (t < 0 || t % 2 == 1) {
+            return 0;
+        }
+        t /= 2;
+        int[] pre = new int[t + 1];
+        pre[0] = 1;
+        for (int i = 0; i < n; i++) {
+            int[] dp = new int[t + 1];
+            for (int j = 0; j <= t; j++) {
+                dp[j] = j < nums[i] ? pre[j] : pre[j - nums[i]] + pre[j];
+            }
+            pre = dp;
+        }
+        return pre[t];
+    }
 
-	// time O(2*m*n), space O(2*m*n)
-	public int findTargetSumWays(int[] nums, int S) {
-		int n = nums.length, sum = 0;
-		for(int num : nums){
-			sum += num;
-		}
-		int m = (sum << 1);
-		return dfs(0, nums, sum + S, sum, new Integer[n + 1][m + 1]);
-	}
+    // time O(n * t), space O(n * t)
+    public int findTargetSumWaysDPWithGrid(int[] nums, int target) {
+        // 把记忆化搜索翻译成 DP
+        int n = nums.length, sum = 0;
+        for (int x : nums) {
+            sum += x;
+        }
+        int t = target + sum;
+        if (t < 0 || t % 2 == 1) {
+            return 0;
+        }
+        t /= 2;
+        int[][] dp = new int[n + 1][t + 1];
+        dp[0][0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= t; j++) {
+                dp[i + 1][j] = j < nums[i] ? dp[i][j] : dp[i][j - nums[i]] + dp[i][j];
+            }
+        }
+        return dp[n][t];
+    }
 
-	private int dfs(int start, int[] nums, int target, int cur, Integer[][] dp){
-		if(start == nums.length){
-			return target == cur ? 1 : 0;
-		}
-		if(dp[start][cur] != null){
-			return dp[start][cur];
-		}
-		dp[start][cur] = dfs(start + 1, nums, target, cur + nums[start], dp)
-			+ dfs(start + 1, nums, target, cur - nums[start], dp);
-		return dp[start][cur];
-	}
+    // time O(n * t), space O(n * t)
+    public int findTargetSumWaysDFSWithMemorization(int[] nums, int target) {
+        // 记忆化搜索
+        // 设数组和为 s， 目标和为 t，加加号的数字和为 p，则加减号的数字和为 s - p 。
+        //   p - (s - p) = t
+        //   2p = s + t
+        //   p = (s + t) / 2;
+        // 题目转化成从数组中选出一些数使的它们的和为 p ，此为背包问题。
+        int n = nums.length, sum = 0;
+        for (int x : nums) {
+            sum += x;
+        }
+        target += sum;
+        // 由推导出的公式可知 s + t 非负且能为偶数
+        if (target < 0 || target % 2 == 1) {
+            return 0;
+        }
+        target /= 2;
+        int[][] memo = new int[n][target + 1];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+        return dfs(n - 1, target, nums, memo);
+    }
+
+    private int dfs(int i, int t, int[] nums, int[][] memo) {
+        if (i == -1) {
+            return t == 0 ? 1 : 0;
+        }
+        if (memo[i][t] != -1) {
+            return memo[i][t];
+        }
+        // 只能不选当前数
+        if (t < nums[i]) {
+            return memo[i][t] = dfs(i - 1, t, nums, memo);
+        }
+        // 选当前数或者不选当前数
+        return memo[i][t] = dfs(i - 1, t - nums[i], nums, memo)
+                + dfs(i - 1, t, nums, memo);
+    }
 }
